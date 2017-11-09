dfd6acea9bf299bb092aff241769c73e28ae8f88
Terence Nero <terence.nero@cuelogic.com>
2017-11-09 21:38:58 +0530

    Replaced HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 import promiseFinally from 'promise.prototype.finally';
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { useStrict } from 'mobx';
 import { Provider } from 'mobx-react';
 import { Switch, Route, withRouter } from 'react-router-dom';
@@ -63,8 +63,8 @@ useStrict(true);
 
 ReactDOM.render((
   <Provider {...stores}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 ), document.getElementById('root'));
