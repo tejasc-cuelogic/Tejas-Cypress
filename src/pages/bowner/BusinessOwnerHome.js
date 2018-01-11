56781eab0992ea71792cf18b646648494b811437
sudeept-cuelogic <sudeep.tarlekar@cuelogic.com>
2018-01-11 16:33:54 +0530

    Added error pages.

---
+++ src/pages/bowner/BusinessOwnerHome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class BusinessOwnerHome extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>This is Business Owner Home page</p>
+      </div>
+    );
+  }
+}
---
+++ src/pages/investor/InvestorHome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class InvestorHome extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>this is investor home page</p>
+      </div>
+    );
+  }
+}
