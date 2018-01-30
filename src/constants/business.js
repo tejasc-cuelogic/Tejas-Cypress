4a7bc083447ded7bcdda5bbfb24cf3aac73447ea
sudeept-cuelogic <sudeep.tarlekar@cuelogic.com>
2018-01-30 11:35:59 +0530

    corrected label.

--- src/constants/business.js
+++ src/constants/business.js
@@ -6,7 +6,7 @@ export const FORM_VALUES = [
   { name: 'minimum_offering_amount', placeholder: 'Minimum offering amount' },
   { name: 'offering_amount', placeholder: 'Offering Amount' },
   { name: 'maturity_date', placeholder: 'Maturity Date' },
-  { name: 'interest_rate', placeholder: 'Interest Date' },
+  { name: 'interest_rate', placeholder: 'Interest Rate' },
   { name: 'offer_date', placeholder: 'Offer Date' },
   { name: 'state_of_formation', placeholder: 'State of Formation' },
   { name: 'type_of_business', placeholder: 'Business Type' },
