841b0813fb73a20daefce137f77d5301294b61c9
pratik-tayade <pratik.tayade@cuelogic.com>
2018-02-01 15:35:16 +0530

    introduced investers page

--- src/modules/home/containers/Home.js
+++ src/modules/home/containers/Home.js
@@ -5,7 +5,10 @@ class Home extends Component {
     return (
       <div className="ui one column grid">
         <div className="column" style={{ fontSize: '30px', color: '#666', top: '25px' }} >
-          NextSeed Home Page
+          <span className="title">NextSeed Home</span>
+          <span className="infotext">
+            NextSeed is giving local business owners a platform to access flexible debt financing
+          </span>
         </div>
       </div>
     );
