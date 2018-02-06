6e28a03154585f173b50afc4ae1d63034cacadda
sudeep.cuelogic <sudeep.cuelogic@nextseed.com>
2018-02-06 17:23:20 +0530

    adding layout changes

--- src/theme/layout/Footer.js
+++ src/theme/layout/Footer.js
@@ -16,32 +16,30 @@ import {
 
 const footer = () => (
   <Segment inverted vertical style={{ padding: '2em 0em' }}>
-    <Container>
+    <Container fluid>
       <Grid divided inverted stackable>
-        <Grid.Row>
-          <Grid.Column className="footerLinks" width={3}>
-            <Header inverted as={Link} to="/about/team" content="Team" />
-            <Header inverted as={Link} to="/blog" content="Blog" />
-            <Header inverted as={Link} to="/about/faq" content="FAQ" />
-            <Header inverted as={Link} to="/offerings" content="Browse" />
-          </Grid.Column>
-          <Grid.Column className="footerLinks" width={3}>
-            <Header inverted as="h4" content="info@nextseed.com" />
-            <Header inverted as="h4" content="800-705-4220" />
-            <Header inverted as={Link} to="/agreements/terms-of-use" content="Terms of Use" />
-            <Header inverted as={Link} to="/agreements/privacy-policy" content="Privacy Policy" />
-          </Grid.Column>
-          <Grid.Column width={10}>
-            <p>
-              This site is operated by NextSeed US LLC, a Funding Portal
-              registered with the U.S. Securities and Exchange Commission (the &quot;SEC&quot;),
-              for the purpose of offering and selling securities in accordance
-              with the exemption from securities registration requirements
-              contained in Section 4(a)(6) of the Securities Act of 1933 and
-              the regulations promulgated by the SEC
-            </p>
-          </Grid.Column>
-        </Grid.Row>
+        <Grid.Column className="footerLinks" width={3}>
+          <Header inverted as={Link} to="/about/team" content="Team" />
+          <Header inverted as={Link} to="/blog" content="Blog" />
+          <Header inverted as={Link} to="/about/faq" content="FAQ" />
+          <Header inverted as={Link} to="/offerings" content="Browse" />
+        </Grid.Column>
+        <Grid.Column className="footerLinks" width={3}>
+          <Header inverted as="h4" content="info@nextseed.com" />
+          <Header inverted as="h4" content="800-705-4220" />
+          <Header inverted as={Link} to="/agreements/terms-of-use" content="Terms of Use" />
+          <Header inverted as={Link} to="/agreements/privacy-policy" content="Privacy Policy" />
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <p>
+            This site is operated by NextSeed US LLC, a Funding Portal
+            registered with the U.S. Securities and Exchange Commission (the &quot;SEC&quot;),
+            for the purpose of offering and selling securities in accordance
+            with the exemption from securities registration requirements
+            contained in Section 4(a)(6) of the Securities Act of 1933 and
+            the regulations promulgated by the SEC
+          </p>
+        </Grid.Column>
       </Grid>
     </Container>
     <Divider inverted section />
