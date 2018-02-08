76505a04e48b00d66a7462a70ba4e5778b6e657f
sagar.cuelogic <sagar.cuelogic@nextseed.com>
2018-02-08 18:00:18 +0530

    including sass

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { withRouter, Switch, Route } from 'react-router-dom'; // Redirect
 import { inject, observer } from 'mobx-react';
 import 'semantic-ui-css/semantic.min.css';
+import './assets/app.css';
 import Layout from './theme/layout/Layout';
 import { publicRoutes } from './modules/routes';
 import SessionCheckContainer from './modules/SessionCheckContainer';
