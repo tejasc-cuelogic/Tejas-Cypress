aca6f93e2e9e03dde76b351ac59dec49c380991b
sagar.cuelogic <sagar.cuelogic@nextseed.com>
2018-03-15 20:00:02 +0530

    updating form error message css

--- src/components/common/FieldError.js
+++ src/components/common/FieldError.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FieldError = (props) => {
   if (props.error) {
-    return (<div>{props.error}</div>);
+    return (<div className="error">{props.error}</div>);
   }
   return null;
 };
