cc77dc7f9af1505f408e15060d46cd8ea9ae397c
sagar.cuelogic <sagar.cuelogic@nextseed.com>
2018-03-15 15:32:20 +0530

    css changes

--- src/modules/business/components/XmlSubmission.js
+++ src/modules/business/components/XmlSubmission.js
@@ -10,12 +10,12 @@ const XmlSubmission = observer((props) => {
       <Accordion.Content active={props.active} key={props.filingId}>
         {
           props.xmlSubmissions.map(xmlSubmission => (
-            <div>
+            <p>
               <Link to={`/app/business/xml/${xmlSubmission.xmlSubmissionId}`}>
                 {`XML Submission | ${xmlSubmission.created}`}
               </Link>
               <Button xmlid={xmlSubmission.xmlSubmissionId}><Icon name="trash" /></Button>
-            </div>
+            </p>
           ))
         }
       </Accordion.Content>
