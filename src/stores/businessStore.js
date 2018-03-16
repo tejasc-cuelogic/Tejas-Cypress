1bcb38882e6f06fcbf15f604ceabb8343a17fd4b
Mayura Thorat <mayura.cuelogic@nextseed.com>
2018-03-16 12:05:38 +0530

    Done changes for enable/disbale button based on validation

--- src/stores/businessStore.js
+++ src/stores/businessStore.js
@@ -86,7 +86,7 @@ export class BusinessStore {
   }
 
   @computed get canSubmitEditBusinessForm() {
-    return (this.business.name.value !== '' && this.business.desc.value !== null);
+    return (this.business.name.value !== '' && this.business.desc.value !== '');
   }
 
   @computed get getSummary() {
