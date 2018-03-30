8db8a7ba0525fe2c13ab54c1aa3615fc31a4ee98
sagarwd31 <sagarwd31@gmail.com>
2018-03-30 18:25:01 +0530

    updating account initial setup UI

--- src/modules/dashboard/containers/Dashboard.js
+++ src/modules/dashboard/containers/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Grid, Card, List, Header, Icon, Statistic, Button, Responsive } from 'semantic-ui-react';
-// import Logo from '../../../assets/images/ns-logo-small.svg';
+import { Grid, Card, Header, Item, Icon, Statistic, Button, Responsive } from 'semantic-ui-react';
+import Thumb from '../../../assets/images/ns-logo-small.svg';
 
 class Invest extends Component {
   render() {
@@ -37,32 +37,65 @@ class Invest extends Component {
               <Grid.Column mobile={16} tablet={16} computer={8}>
                 <Card fluid raised className="welcome-card">
                   <Card.Content>
-                    {/* <Item.Image size="tiny" floated="left" src={Logo} /> */}
-                    <Icon size="huge" name="thin circle" className="pull-left" />
+                    <Item.Image size="mini" floated="left" src={Thumb} />
+                    {/* <Icon size="huge" name="thin circle" className="pull-left" /> */}
                     <Card.Header>Thank you for signing up!</Card.Header>
                     <Card.Meta>Please complete the following steps in order
                       to unlock all features:
                     </Card.Meta>
                   </Card.Content>
-                  <Card.Content>
+                  {/* <Card.Content>
                     <Card.Description>
                       <List relaxed className="check-list">
                         <List.Item>
                           <List.Icon name="check circle large" />
-                          <List.Content verticalAlign="middle">Complete all required information about you <Link as="a" to=""><strong>here</strong></Link></List.Content>
+                          <List.Content verticalAlign="middle">Complete all required information
+                          about you <Link as="a" to=""><strong>here</strong></Link></List.Content>
                         </List.Item>
                         <List.Item>
                           <List.Icon name="circle thin large" />
-                          <List.Content verticalAlign="middle">Confirm your email address. <Link as="a" to=""><strong>Resend email?</strong></Link></List.Content>
+                          <List.Content verticalAlign="middle">Confirm your email address. <Link
+                          as="a" to=""><strong>Resend email?</strong></Link></List.Content>
                         </List.Item>
                         <List.Item>
                           <List.Icon name="circle thin large" />
-                          <List.Content verticalAlign="middle">Create your first NextSeed Account <Link as="a" to=""><strong>here</strong></Link></List.Content>
+                          <List.Content verticalAlign="middle">Create your first NextSeed Account
+                          <Link as="a" to=""><strong>here</strong></Link></List.Content>
                         </List.Item>
                       </List>
                     </Card.Description>
-                  </Card.Content>
+                  </Card.Content> */}
                 </Card>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column mobile={16} tablet={16} computer={6}>
+                <Card.Group className="steps-card">
+                  <Card fluid raised>
+                    <Card.Content>
+                      <Card.Description>
+                        <Icon name="numbar" data-icon="1" />
+                        Complete all required information about you <Link as="a" to=""><strong>here</strong></Link>
+                      </Card.Description>
+                    </Card.Content>
+                  </Card>
+                  <Card fluid raised>
+                    <Card.Content>
+                      <Card.Description>
+                        <Icon name="numbar" data-icon="2" />
+                        Confirm your e-mail address. <Link as="a" to=""><strong>Resend email</strong></Link>
+                      </Card.Description>
+                    </Card.Content>
+                  </Card>
+                  <Card fluid raised>
+                    <Card.Content>
+                      <Card.Description className="disabled">
+                        <Icon name="numbar" data-icon="3" />
+                        Open your first NextSeeed Account
+                      </Card.Description>
+                    </Card.Content>
+                  </Card>
+                </Card.Group>
               </Grid.Column>
             </Grid.Row>
           </Grid>
