f5dddbf38d011ba5af73b21a5bc6adf01418405c
Mayura Thorat <mayura.cuelogic@nextseed.com>
2018-04-10 12:51:30 +0530

    done changes to remove unused code

--- src/modules/dashboard/containers/individual/LinkBankAccount.js
+++ src/modules/dashboard/containers/individual/LinkBankAccount.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { Modal } from 'semantic-ui-react';
 
 import MuliStep from '../../../../helper/MultiStep';
 import LinkBankAccountPlaid from './LinkBankAccountPlaid';
