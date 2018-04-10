caeca76b46446cc45736e70575be8687441d1f84
sagar.cuelogic <sagar.cuelogic@nextseed.com>
2018-04-10 17:02:42 +0530

    adding IRA account creation styles

--- src/modules/dashboard/containers/ira/FinancialInformation.js
+++ src/modules/dashboard/containers/ira/FinancialInformation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
-import { Header, Form, Input, Popup, Icon } from 'semantic-ui-react';
+import { Header, Form, Input, Popup, Icon, Label } from 'semantic-ui-react';
 
 import validationActions from '../../../../actions/validation';
 import FieldError from '../../../../components/common/FieldError';
@@ -17,49 +17,57 @@ export default class FinancialInformation extends React.Component {
     return (
       <div>
         <div>
-          <Header as="h2">Complete your financial information</Header>
+          <Header as="h2" textAlign="center">Complete your financial information</Header>
+          <p className="center-align">Lorem psum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           <Form error>
-            <Form.Field>
-              { /*  eslint-disable jsx-a11y/label-has-for */ }
-              <label>
-                {iraAccount.networth.label}
-                <Popup
-                  trigger={<Icon name="help circle outline" />}
-                  content="Put your first name as listed on your driver license"
-                  position="top center"
-                  className="center-align"
+            <div className="field-wrap">
+              <Form.Field>
+                { /*  eslint-disable jsx-a11y/label-has-for */ }
+                <label>
+                  {iraAccount.networth.label}
+                  <Popup
+                    trigger={<Icon name="help circle outline" />}
+                    content="Tell us your net worth in $"
+                    position="top center"
+                    className="center-align"
+                  />
+                </label>
+                <Input type="text" placeholder="Amount">
+                  <Label basic>$</Label>
+                  <input />
+                  <Label>.00</Label>
+                </Input>
+                <Input
+                  name={iraAccount.networth.key}
+                  placeholder={iraAccount.networth.placeHolder}
+                  value={iraAccount.networth.value}
+                  error={!!iraAccount.networth.error}
+                  onChange={this.handleInputChange}
+                  maxLength={10}
                 />
-              </label>
-              <Input
-                name={iraAccount.networth.key}
-                placeholder={iraAccount.networth.placeHolder}
-                value={iraAccount.networth.value}
-                error={!!iraAccount.networth.error}
-                onChange={this.handleInputChange}
-                maxLength={10}
-              />
-              <FieldError error={iraAccount.networth.error} />
-            </Form.Field>
-            <Form.Field>
-              <label>
-                {iraAccount.annualIncome.label}
-                <Popup
-                  trigger={<Icon name="help circle outline" />}
-                  content="Put your first name as listed on your driver license"
-                  position="top center"
-                  className="center-align"
+                <FieldError error={iraAccount.networth.error} />
+              </Form.Field>
+              <Form.Field>
+                <label>
+                  {iraAccount.annualIncome.label}
+                  <Popup
+                    trigger={<Icon name="help circle outline" />}
+                    content="Tell us your annual income in $"
+                    position="top center"
+                    className="center-align"
+                  />
+                </label>
+                <Input
+                  name={iraAccount.annualIncome.key}
+                  placeholder={iraAccount.annualIncome.placeHolder}
+                  value={iraAccount.annualIncome.value}
+                  error={!!iraAccount.annualIncome.error}
+                  onChange={this.handleInputChange}
+                  maxLength={12}
                 />
-              </label>
-              <Input
-                name={iraAccount.annualIncome.key}
-                placeholder={iraAccount.annualIncome.placeHolder}
-                value={iraAccount.annualIncome.value}
-                error={!!iraAccount.annualIncome.error}
-                onChange={this.handleInputChange}
-                maxLength={12}
-              />
-              <FieldError error={iraAccount.annualIncome.error} />
-            </Form.Field>
+                <FieldError error={iraAccount.annualIncome.error} />
+              </Form.Field>
+            </div>
           </Form>
         </div>
       </div>
