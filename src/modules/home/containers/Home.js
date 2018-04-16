750308244aecabcf459248e27cff631bd7bdf6e1
Terence Nero <terence.cuelogic@nextseed.com>
2018-04-16 10:08:18 +0000

    Update Home.js

--- src/modules/home/containers/Home.js
+++ src/modules/home/containers/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Home extends Component {
   render() {
-      console.log(process.env);
+    console.log(process.env);
     return (
       <div className="ui vertical segment content">
         <div className="ui container">
