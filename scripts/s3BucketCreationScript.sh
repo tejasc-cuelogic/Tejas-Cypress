4e0be3764ee98534c5248da7781240f76baf68a7
Ranjit Singh <ranjit.singh@cuelogic.com>
2018-04-17 16:45:02 +0530

    specific access for issues* branches

--- scripts/s3BucketCreationScript.sh
+++ scripts/s3BucketCreationScript.sh
@@ -8,9 +8,8 @@ if [ "$isBucketExist" != "" ]; then
 else
 	echo "bucket ${bucketName} don't exist."
 	aws s3api create-bucket --bucket ${bucketName} --region ${regionName} && echo "Bucket ${bucketName} successfully created in region ${regionName}."
-	sed -i "s/Bucket-Name/${bucketName}/" scripts/S3PublicReadAccessPolicy.json && echo "policy created"
+	sed -i "s/Bucket-Name/${bucketName}/" scripts/S3PublicReadAccessPolicy.json && echo "Policy created."
 	aws s3api put-bucket-policy --bucket ${bucketName} --policy file://scripts/S3PublicReadAccessPolicy.json && echo "Policy attached."
 	aws s3 website s3://${bucketName}/ --index-document index.html --error-document index.html && echo "Static hosting configured."
-	aws s3api put-bucket-versioning --bucket ${bucketName} --versioning-configuration Status=Enabled && echo "Versioning enabled" && echo "Script executed successfully"
-	
+	aws s3api put-bucket-versioning --bucket ${bucketName} --versioning-configuration Status=Enabled && echo "Versioning enabled" && echo "Script executed successfully."
 fi
