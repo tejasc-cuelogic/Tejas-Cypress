cd0fa574ac90e1e39f98b5a441dd250a246ac53b
Terence Nero <terence.cuelogic@nextseed.com>
2018-04-17 10:16:48 +0000

    Update s3BucketCreationScript.sh

--- scripts/s3BucketCreationScript.sh
+++ scripts/s3BucketCreationScript.sh
@@ -9,7 +9,7 @@ else
 	echo "bucket ${bucketName} don't exist."
 	aws s3api create-bucket --bucket ${bucketName} --region ${regionName} && echo "Bucket ${bucketName} successfully created in region ${regionName}."
 	sed -i "s/Bucket-Name/${bucketName}/" scripts/s3PublicReadPolicy.json && echo "policy created"
-	aws s3api put-bucket-policy --bucket ${bucketName} --policy file://scripts/s3PublicReadPolicy.json && echo "Policy attached."
+	aws s3api put-bucket-policy --bucket ${bucketName} --policy file://scripts/S3PublicReadPolicy.json && echo "Policy attached."
 	aws s3 website s3://${bucketName}/ --index-document index.html --error-document index.html && echo "Static hosting configured."
 	aws s3api put-bucket-versioning --bucket ${bucketName} --versioning-configuration Status=Enabled && echo "Versioning enabled" && echo "Script executed successfully"
 	
