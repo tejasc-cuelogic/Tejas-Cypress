46343b050ea6af1a497814808c0b1fa7ecf7e584
Mayura Thorat <mayura.cuelogic@nextseed.com>
2018-04-24 13:51:46 +0530

    done changes for form components

--- src/components/form/FormElements.js
+++ src/components/form/FormElements.js
@@ -1,5 +1,7 @@
 export { default as FormInput } from './FormInput';
+export { default as FormLabelInput } from './FormLabelInput';
 export { default as FormSelect } from './FormSelect';
+export { default as FormDropDown } from './FormDropDown';
 export { default as FormRadioGroup } from './FormRadioGroup';
 export { default as FileUploaderVertical } from './FileUploaderVertical';
 export { default as FileUploaderInline } from './FileUploaderInline';
