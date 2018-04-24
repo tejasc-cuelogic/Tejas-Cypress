da195c93107c0f1b7e2e19d9b81e65fd915d9577
Mayura Thorat <mayura.cuelogic@nextseed.com>
2018-04-24 13:45:11 +0530

    done changes for file uoloader inline common component

--- src/components/form/FormElements.js
+++ src/components/form/FormElements.js
@@ -2,5 +2,6 @@ export { default as FormInput } from './FormInput';
 export { default as FormSelect } from './FormSelect';
 export { default as FormRadioGroup } from './FormRadioGroup';
 export { default as FileUploaderVertical } from './FileUploaderVertical';
+export { default as FileUploaderInline } from './FileUploaderInline';
 export { default as FormDatePicker } from './FormDatePicker';
 export { default as MaskedInput } from './MaskedInput';
--- src/modules/dashboard/containers/ira/Identity.js
+++ src/modules/dashboard/containers/ira/Identity.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import { Header, Form, Icon, Divider } from 'semantic-ui-react';
+import { Header, Form, Divider } from 'semantic-ui-react';
 import { inject, observer } from 'mobx-react';
+import { FileUploaderInline } from '../../../../components/form/FormElements';
 
 @inject('accountStore')
 @observer
 export default class Identity extends Component {
-  uploadDocument = (e) => {
-    if (e.target.files.length) {
-      const uploadFile = e.target.files[0];
-      this.props.accountStore.setIraAccountDetails(e.target.name, uploadFile.name);
+  uploadDocument = (name, files) => {
+    if (files.length) {
+      const uploadFile = files[0];
+      this.props.accountStore.setIraAccountDetails(name, uploadFile.name);
     }
   }
   removeUploadedDriversLicence = () => {
@@ -22,24 +23,12 @@ export default class Identity extends Component {
         <Header as="h4" textAlign="center">Lorem psum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</Header>
         <Divider section hidden />
         <Form className="file-uploader-inline">
-          <Form.Field>
-            {driversLicence.value === '' &&
-              <div className="file-uploader">
-                <Icon name="upload" /> Choose a file <span>or drag it here</span>
-                <input
-                  name={driversLicence.key}
-                  type="file"
-                  onChange={this.uploadDocument}
-                />
-              </div>
-            }
-            {driversLicence.value !== '' &&
-              <div className="file-uploader attached">
-                <span title={driversLicence.value}>{driversLicence.value}</span>
-                <Icon name="remove" onClick={this.removeUploadedDriversLicence} />
-              </div>
-            }
-          </Form.Field>
+          <FileUploaderInline
+            name={driversLicence.key}
+            fielddata={driversLicence}
+            uploadDocument={this.uploadDocument}
+            removeUploadedDocument={this.removeUploadedDriversLicence}
+          />
         </Form>
         <Divider section hidden />
         <p className="center-align">As a regulated financial service company operating in the US we are periodically
