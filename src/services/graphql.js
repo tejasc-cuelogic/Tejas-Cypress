92b5e4d7957c0715472b60d9f4fd1112e57bea68
Terence Nero <terence.nero@cuelogic.com>
2018-04-26 17:43:54 +0530

    Added types for unions

--- src/services/graphql.js
+++ src/services/graphql.js
@@ -1,10 +1,11 @@
 /* eslint-disable arrow-body-style */
 import fetch from 'isomorphic-fetch';
-import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset';
+import { ApolloClient, HttpLink, InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-client-preset';
 import { setContext } from 'apollo-link-context';
 import commonStore from './../stores/commonStore';
 import { API_ROOT } from '../constants/common';
 import { GRAPHQL } from '../constants/business';
+import introspectionQueryResultData from '../constants/graphQLFragmentTypes.json';
 
 global.fetch = fetch;
 
@@ -19,7 +20,12 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+// For unions and interfaces
+const fragmentMatcher = new IntrospectionFragmentMatcher({
+  introspectionQueryResultData,
+});
+
 export const GqlClient = new ApolloClient({
   link: authLink.concat(new HttpLink({ uri })),
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({ fragmentMatcher }),
 });
