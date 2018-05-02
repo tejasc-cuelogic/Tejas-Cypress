8f82a60344ccdc95fb4d90d03f67a0e2a77b8c61
Terence <terence.cuelogic@nextseed.com>
2018-05-02 21:10:56 +0530

    s3 creation script updated

--- scripts/cicd/s3BucketCreationScript.sh
+++ scripts/cicd/s3BucketCreationScript.sh
@@ -1,7 +1,6 @@
 #/bin/bash
 bucketName=$1
 regionName=$2
-
 	aws s3api create-bucket --bucket ${bucketName} --region ${regionName} && echo "Bucket ${bucketName} successfully created in region ${regionName}."
 	sed -i "s/Bucket-Name/${bucketName}/" scripts/cicd/s3PublicReadAccessPolicy.json && echo "Policy created."
 	aws s3api put-bucket-policy --bucket ${bucketName} --policy file://scripts/cicd/s3PublicReadAccessPolicy.json && echo "Policy attached."
