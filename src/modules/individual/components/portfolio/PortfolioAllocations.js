1377b17c3c842af932d20057cdc2e69d87df474f
sagar.cuelogic <sagar.cuelogic@nextseed.com>
2018-05-11 20:42:44 +0530

    updating UI

--- src/modules/individual/components/portfolio/PortfolioAllocations.js
+++ src/modules/individual/components/portfolio/PortfolioAllocations.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Aux from 'react-aux';
-import { Header } from 'semantic-ui-react';
+import { Header, Grid } from 'semantic-ui-react';
 
 const PortfolioAllocations = () => (
   <Aux>
     <Header as="h3">Portfolio Allocations</Header>
-    <div>
-      graph !
-    </div>
+    <Grid columns="equal" celled="internally">
+      <Grid.Column>
+        graph !
+      </Grid.Column>
+      <Grid.Column>
+        graph !
+      </Grid.Column>
+      <Grid.Column>
+        graph !
+      </Grid.Column>
+    </Grid>
   </Aux>
 );
 
