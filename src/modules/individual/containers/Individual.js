5d1bcc7baced4852d6db58b25676d9bf9707f801
sagar.cuelogic <sagar.cuelogic@nextseed.com>
2018-05-11 14:12:39 +0530

    adding transfer funds page UI

--- src/modules/individual/containers/Individual.js
+++ src/modules/individual/containers/Individual.js
@@ -5,6 +5,7 @@ import { Route } from 'react-router-dom';
 import PrivateLayout from '../../../containers/common/PrivateHOC';
 import Portfolio from './Portfolio';
 import BankAccount from './BankAccount';
+import TransferFunds from './TransferFunds';
 
 // @withRouter
 export default class Individual extends Component {
@@ -12,8 +13,9 @@ export default class Individual extends Component {
     const { match } = this.props;
     return (
       <PrivateLayout {...this.props}>
-        <Route exact path={match.url} component={Portfolio} />
+        <Route exact path={`${match.url}/portfolio`} component={Portfolio} />
         <Route exact path={`${match.url}/bank-accounts`} component={BankAccount} />
+        <Route exact path={`${match.url}/transfer-funds`} component={TransferFunds} />
       </PrivateLayout>
     );
   }
--- src/modules/individual/containers/TransferFunds.js
+++ src/modules/individual/containers/TransferFunds.js
@@ -1,10 +1,66 @@
 import React, { Component } from 'react';
+import { Header, Grid, Card, Button, Statistic, Popup, Accordion, Icon } from 'semantic-ui-react';
 
 export default class TransferFunds extends Component {
   render() {
     return (
       <div>
-        Transfer Funds!
+        <Header as="h3">Transfer funds</Header>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column widescreen={8} largeScreen={12} computer={13} tablet={16} mobile={16}>
+              <Card fluid>
+                <Card.Content>
+                  <Grid columns="equal">
+                    <Grid.Row>
+                      <Grid.Column>
+                        <Statistic size="tiny">
+                          <Statistic.Label>
+                            Available cash
+                            <Popup
+                              trigger={<Icon className="ns-help-circle" />}
+                              content="Available cash in your Nextseed Account"
+                              position="top center"
+                              className="center-align"
+                            />
+                          </Statistic.Label>
+                          <Statistic.Value>$15.405,23</Statistic.Value>
+                        </Statistic>
+                      </Grid.Column>
+                      <Grid.Column textAlign="right" verticalAlign="middle">
+                        <Button primary content="Add funds" />
+                        <Button inverted color="green" content="Withdraw funds" />
+                      </Grid.Column>
+                    </Grid.Row>
+                  </Grid>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column widescreen={6} largeScreen={10} computer={10} tablet={13} mobile={16}>
+              <Card fluid>
+                <Card.Content>
+                  <Header as="h3">Bank Account FAQs</Header>
+                  <Accordion>
+                    <Accordion.Title active>
+                      <Icon name="dropdown" />
+                      Lorem ipsum dolor sit amet enim ullamcorper?
+                    </Accordion.Title>
+                    <Accordion.Content active>
+                      <p>
+                        Pellentesque facilisis. Nulla imperdiet sit amet magna. Vestibulum
+                        dapibus, mauris nec malesuada fames ac turpis Pellentesque facilisis.
+                        Nulla imperdiet sit amet magna. Vestibulum dapibus, mauris nec
+                        malesuada fames ac turpis
+                      </p>
+                    </Accordion.Content>
+                  </Accordion>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     );
   }
