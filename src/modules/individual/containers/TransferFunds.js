49f2e337f3016004dcb392651070067277a18fe2
pratik-tayade <pratik.tayade@cuelogic.com>
2018-05-11 16:20:25 +0530

    generic faq comp and transfer funds

--- src/modules/individual/containers/TransferFunds.js
+++ src/modules/individual/containers/TransferFunds.js
@@ -1,5 +1,27 @@
 import React, { Component } from 'react';
-import { Header, Grid, Card, Button, Statistic, Popup, Accordion, Icon } from 'semantic-ui-react';
+import { Header, Grid } from 'semantic-ui-react';
+import { FaqWidget } from '../../../theme/common/ImportCommon';
+import AvailableCashTransfer from '../components/transferFunds/AvailableCashTransfer';
+
+const data = {
+  availableCash: 9743.33,
+  faqs: [
+    {
+      id: 1,
+      title: 'Are these one-time investments or monthly investments?',
+      description: `Funds for your investment will first be taken from the available cash sitting in
+      your NextSeed account. If you have insufficient funds in your NextSeed account,
+      you will be prompted to request an immediate transfer of funds from your external banking account
+      to your NextSeed account.`,
+    },
+    {
+      id: 2,
+      title: 'Can I cancel my investment after the closing?',
+      description: `Unlike investing in in companies listed on a stock exchange where you can quickly
+      and easily trade securities, there is no public market for crowdfunded securities.`,
+    },
+  ],
+};
 
 export default class TransferFunds extends Component {
   render() {
@@ -9,55 +31,12 @@ export default class TransferFunds extends Component {
         <Grid>
           <Grid.Row>
             <Grid.Column widescreen={8} largeScreen={12} computer={13} tablet={16} mobile={16}>
-              <Card fluid>
-                <Card.Content>
-                  <Grid columns="equal">
-                    <Grid.Row>
-                      <Grid.Column>
-                        <Statistic size="tiny">
-                          <Statistic.Label>
-                            Available cash
-                            <Popup
-                              trigger={<Icon className="ns-help-circle" />}
-                              content="Available cash in your Nextseed Account"
-                              position="top center"
-                              className="center-align"
-                            />
-                          </Statistic.Label>
-                          <Statistic.Value>$15.405,23</Statistic.Value>
-                        </Statistic>
-                      </Grid.Column>
-                      <Grid.Column textAlign="right" verticalAlign="middle">
-                        <Button primary content="Add funds" />
-                        <Button inverted color="green" content="Withdraw funds" />
-                      </Grid.Column>
-                    </Grid.Row>
-                  </Grid>
-                </Card.Content>
-              </Card>
+              <AvailableCashTransfer match={this.props.match} cash={data.availableCash} />
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
             <Grid.Column widescreen={6} largeScreen={10} computer={10} tablet={13} mobile={16}>
-              <Card fluid>
-                <Card.Content>
-                  <Header as="h3">Bank Account FAQs</Header>
-                  <Accordion>
-                    <Accordion.Title active>
-                      <Icon name="dropdown" />
-                      Lorem ipsum dolor sit amet enim ullamcorper?
-                    </Accordion.Title>
-                    <Accordion.Content active>
-                      <p>
-                        Pellentesque facilisis. Nulla imperdiet sit amet magna. Vestibulum
-                        dapibus, mauris nec malesuada fames ac turpis Pellentesque facilisis.
-                        Nulla imperdiet sit amet magna. Vestibulum dapibus, mauris nec
-                        malesuada fames ac turpis
-                      </p>
-                    </Accordion.Content>
-                  </Accordion>
-                </Card.Content>
-              </Card>
+              <FaqWidget heading="Transfer Funds" faqs={data.faqs} />
             </Grid.Column>
           </Grid.Row>
         </Grid>
