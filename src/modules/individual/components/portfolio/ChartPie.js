fb5cbd863f1b78be49273944d1fc7d83965f3ac9
pratik-tayade <pratik.tayade@cuelogic.com>
2018-05-13 22:11:50 +0530

    introduced ReCharts and portfolio allocation graphs

---
+++ src/modules/individual/components/portfolio/ChartPie.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import { PieChart, Pie, Cell, ResponsiveContainer, Label, Tooltip, Legend, Sector } from 'recharts';
+import Helper from '../../../../helper/utility';
+
+/*
+  Reference:
+  http://recharts.org/en-US/examples/CustomActiveShapePieChart
+*/
+const renderActiveShape = (props) => {
+  const RADIAN = Math.PI / 180;
+  const {
+    cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value,
+  } = props;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + ((outerRadius + 10) * cos);
+  const sy = cy + ((outerRadius + 10) * sin);
+  const mx = cx + ((outerRadius + 30) * cos);
+  const my = cy + ((outerRadius + 30) * sin);
+  const ex = mx + ((cos >= 0 ? 1 : -1) * 22);
+  const ey = my;
+  const textAnchor = cos >= 0 ? 'start' : 'end';
+
+  return (
+    <g>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius - 1}
+        outerRadius={outerRadius + 9}
+        fill={fill}
+      />
+      <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+      <text x={ex + ((cos >= 0 ? 1 : -1) * 12)} y={ey} textAnchor={textAnchor} fill="#333">{payload.name}</text>
+      <text x={ex + ((cos >= 0 ? 1 : -1) * 12)} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
+        {`${Helper.CurrencyFormat(value)} (${(percent * 100).toFixed(2)}%)`}
+      </text>
+    </g>
+  );
+};
+
+export default class ChartPie extends Component {
+  state = {
+    activeIndex: 0,
+  }
+
+  onPieEnter = (data, index) => {
+    this.setState({
+      activeIndex: index,
+    });
+  }
+
+  renderLegend = (props) => {
+    const { payload } = props;
+    return (
+      <ul className="chartLegends">
+        {
+          payload.map(entry => (
+            <li style={{ color: entry.color }} key={`item-${entry.value}`}><span>{entry.value}</span></li>
+          ))
+        }
+      </ul>
+    );
+  };
+
+  render() {
+    const { data, title, colors } = this.props;
+    return (
+      <ResponsiveContainer width="100%" height={280}>
+        <PieChart>
+          <Legend layout="vertical" verticalAlign="middle" align="right" content={this.renderLegend} />
+          <Pie
+            activeIndex={this.state.activeIndex}
+            activeShape={renderActiveShape}
+            data={data}
+            cx={114}
+            cy={140}
+            innerRadius={82}
+            outerRadius={105}
+            type="circle"
+            fill="#8884d8"
+            paddingAngle={0}
+            onMouseEnter={this.onPieEnter}
+          >
+            {
+              data.map((entry, index) => <Cell fill={colors[index % colors.length]} />)
+            }
+            <Label value={title} offset={0} position="center" />
+            <Tooltip />
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    );
+  }
+}
--- src/modules/individual/components/portfolio/PortfolioAllocations.js
+++ src/modules/individual/components/portfolio/PortfolioAllocations.js
@@ -1,19 +1,44 @@
 import React from 'react';
 import Aux from 'react-aux';
 import { Header, Grid } from 'semantic-ui-react';
+import ChartPie from './ChartPie';
+
+const INVESTMENT_TYPE = [
+  { name: 'Term Note', value: 7514 },
+  { name: 'Rev Share', value: 1299 },
+];
+
+const INDUSTRY = [
+  { name: 'Food & Breverage', value: 3514 },
+  { name: 'Fitness', value: 1580 },
+  { name: 'Bar & Brewery', value: 7430 },
+  { name: 'Hospitality', value: 1299 },
+  { name: 'Other', value: 3200 },
+];
+
+const REGION = [
+  { name: 'North East', value: 7514 },
+  { name: 'South East', value: 1299 },
+  { name: 'Midwest', value: 5399 },
+  { name: 'South Central', value: 1100 },
+  { name: 'Mountain', value: 532 },
+  { name: 'West', value: 1730 },
+];
+
+const COLORS = ['#C782FF', '#28DAC9', '#0681A1', '#86D200', '#D2FF85'];
 
 const PortfolioAllocations = () => (
   <Aux>
     <Header as="h3">Portfolio Allocations</Header>
     <Grid columns="equal" celled="internally">
-      <Grid.Column>
-        graph !
+      <Grid.Column style={{ paddingLeft: '0px' }}>
+        <ChartPie title="Investment Type" data={INVESTMENT_TYPE} colors={COLORS} />
       </Grid.Column>
-      <Grid.Column>
-        graph !
+      <Grid.Column style={{ paddingLeft: '0px' }}>
+        <ChartPie title="Industry" data={INDUSTRY} colors={COLORS} />
       </Grid.Column>
-      <Grid.Column>
-        graph !
+      <Grid.Column style={{ paddingLeft: '0px' }}>
+        <ChartPie title="Boston 25%" data={REGION} colors={COLORS} />
       </Grid.Column>
     </Grid>
   </Aux>
