f4a8fc117ce50285ce0659c72e3496a8862cbb8b
prasanna.cuelogic <prasanna.cuelogic@nextseed.com>
2018-05-16 13:39:20 +0530

    Resolved codacy issue

--- src/theme/layout/SidebarNav.js
+++ src/theme/layout/SidebarNav.js
@@ -40,14 +40,11 @@ export const SidebarNav = (props) => {
 
 export const GetNavItem = (item, roles) => {
   const result = _.find(ALL_NAV_ITEMS, i => i.to === item);
+  const link = <h3><Link to={`/app/page/${result.to}`}>{result.title}</Link></h3>;
   return (
     result && (
       result.accessibleTo.length === 0 ||
-      _.intersection(result.accessibleTo, roles).length > 0)) ? (
-        <h3>
-          <Link to={`/app/page/${result.to}`}>{result.title}</Link>
-        </h3>
-    ) : false;
+      _.intersection(result.accessibleTo, roles).length > 0)) ? link : false;
 };
 
 export const GetNavMeta = (item) => {
