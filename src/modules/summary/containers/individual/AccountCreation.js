04f02f9f1c16470613ec0d75c13b27b8f07fe1b5
pratik-tayade <pratik.tayade@cuelogic.com>
2018-05-17 16:30:51 +0530

    fixed stepwise highlight and reopen issue

--- src/modules/summary/containers/individual/AccountCreation.js
+++ src/modules/summary/containers/individual/AccountCreation.js
@@ -16,9 +16,6 @@ const steps =
 @inject('individualAccountStore')
 @observer
 export default class AccountCreation extends React.Component {
-  componentWillUnmount() {
-    this.props.individualAccountStore.setStepToBeRendered(0);
-  }
   render() {
     return (
       <div className="step-progress">
