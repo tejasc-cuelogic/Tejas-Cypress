8daa30a1e5986d81ffecba9da117707e1b73d66c
pratik-tayade <pratik.tayade@cuelogic.com>
2018-05-22 15:24:26 +0530

    sidebar - subnav

--- src/theme/layout/SidebarNav.js
+++ src/theme/layout/SidebarNav.js
@@ -1,42 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import Aux from 'react-aux';
-import { Menu, Icon, Label } from 'semantic-ui-react';
+import { Menu, Icon, Label, Dropdown } from 'semantic-ui-react';
 import _ from 'lodash';
 import { ALL_NAV_ITEMS } from '../../constants/privateNavigationMeta';
 
-export const SidebarNav = (props) => {
-  const { roles } = props;
-  const navItems = _.filter(
-    ALL_NAV_ITEMS,
-    n => n.to !== 'profile-settings' && (n.accessibleTo.length === 0 || _.intersection(n.accessibleTo, roles).length > 0),
-  );
-  const actuals = ['account-details', 'summary', 'users', 'profile-settings', 'edgar'];
-  return (
-    <Aux>
-      {
-        navItems.map(item => (
-          <Menu.Item
-            key={item.to}
-            name={item.to}
-            as={NavLink}
-            to={(actuals.includes(item.to.split('/')[0])) ? `/app/${item.to}` : `/app/page/${item.to}`}
-          >
-            <Icon className={item.icon} />
-            {item.to === 'messages' &&
-              <Label circular color="red" size="mini" horizontal>3</Label>
-            }
-            <span>{item.title}</span>
-          </Menu.Item>
-        ))
-      }
-      <Menu.Item key="logout" name="logout" onClick={props.handleLogOut}>
-        <Icon name="sign out" />
-        <span>Logout</span>
-      </Menu.Item>
-    </Aux>
-  );
-};
+export class SidebarNav extends Component {
+  state = { active: '' };
+  navClick = (e, { name }) => this.setState({ active: name });
+  render() {
+    const { roles } = this.props;
+    const { active } = this.state;
+    const navItems = _.filter(
+      ALL_NAV_ITEMS,
+      n => n.to !== 'profile-settings' && (n.accessibleTo.length === 0 || _.intersection(n.accessibleTo, roles).length > 0),
+    );
+    const actuals = ['account-details', 'summary', 'users', 'profile-settings', 'edgar'];
+    return (
+      <Aux>
+        {
+          navItems.map(item => (
+            <Aux>
+              {(item.subNavigations && item.subNavigations.length > 0) ? (
+                <Dropdown
+                  item
+                  active={active === item.to}
+                  name={item.to}
+                  text={<span><Icon className={item.icon} />{item.title}</span>}
+                  onClick={this.navClick}
+                >
+                  <Dropdown.Menu>
+                    {item.subNavigations.map(sn => (
+                      <Dropdown.Item as={NavLink} to={sn.to}>{sn.title}</Dropdown.Item>
+                    ))}
+                  </Dropdown.Menu>
+                </Dropdown>
+              ) : (
+                <Menu.Item
+                  key={item.to}
+                  name={item.to}
+                  as={NavLink}
+                  to={(actuals.includes(item.to.split('/')[0])) ? `/app/${item.to}` : `/app/page/${item.to}`}
+                >
+                  <Icon className={item.icon} />
+                  {item.to === 'messages' &&
+                    <Label circular color="red" size="mini" horizontal>3</Label>
+                  }
+                  <span>{item.title}</span>
+                </Menu.Item>
+              )}
+            </Aux>
+          ))
+        }
+        <Menu.Item key="logout" name="logout" onClick={this.props.handleLogOut}>
+          <Icon name="sign out" />
+          <span>Logout</span>
+        </Menu.Item>
+      </Aux>
+    );
+  }
+}
 
 export const GetNavItem = (item, roles) => {
   const result = _.find(ALL_NAV_ITEMS, i => i.to === item);
