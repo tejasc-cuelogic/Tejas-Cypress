7240a9dae1f7a8850bdcd33fb18f8347be3d96a5
sagar.cuelogic <sagar.cuelogic@nextseed.com>
2018-05-28 17:24:09 +0530

    adding investor signup UI

--- src/modules/public/businessSignup/containers/Signup.js
+++ src/modules/public/businessSignup/containers/Signup.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Segment, Grid, Icon, Header, Divider } from 'semantic-ui-react';
+import { Segment, Grid, Icon, Header, Divider, Form } from 'semantic-ui-react';
+import { inject, observer } from 'mobx-react';
 
+import { FormRadioGroup } from '../../../../theme/form/FormElements';
+
+@inject('accountStore', 'entityAccountStore')
+@observer
 class Signup extends Component {
   render() {
     return (
       <Segment vertical className="content issuer-signup">
         <Grid container>
           <Grid.Column>
-            <Icon className="ns-paper-plane" size="huge" color="green" />
-            <Header as="h1">
-              Pre-Qualification Application Process
-              <Header.Subheader>
-                Welcome to NextSeed! Run through this quick form to get pre-qualified.{' '}
-                <Link to="/" className="link">Need help or have questions?</Link>
-              </Header.Subheader>
-            </Header>
-            <Divider section />
+            <Form className="issuer-signup">
+              <Icon className="ns-paper-plane" size="huge" color="green" />
+              <Header as="h1">
+                Pre-Qualification Application Process
+                <Header.Subheader>
+                  Welcome to NextSeed! Run through this quick form to get pre-qualified.{' '}
+                  <Link to="/" className="link">Need help or have questions?</Link>
+                </Header.Subheader>
+              </Header>
+              <Divider section />
+              <Header as="h1">
+                What is your Business Model?
+                <Header.Subheader>
+                  Only Business to Consumer models are accepted at this time
+                </Header.Subheader>
+              </Header>
+              <FormRadioGroup
+                fielddata={formEntityInfo.fields.isEntityTrust}
+                name="isEntityTrust"
+                changed={entityInfoChange}
+                containerclassname="button-radio center-align"
+              />
+            </Form>
           </Grid.Column>
         </Grid>
       </Segment>
