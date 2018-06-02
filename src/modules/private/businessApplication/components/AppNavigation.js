c7c045370123aca9a2234d9955616ef0cf7b818b
pratik-tayade <pratik.tayade@cuelogic.com>
2018-06-02 13:08:33 +0530

    improved business app bottom nav

--- src/modules/private/businessApplication/components/AppNavigation.js
+++ src/modules/private/businessApplication/components/AppNavigation.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { Button, Icon } from 'semantic-ui-react';
+import Aux from 'react-aux';
 import { GetNavMeta } from '../../../../theme/layout/SidebarNav';
 
 @withRouter
@@ -29,14 +30,17 @@ export default class AppNavigation extends Component {
             {this.state.navItems[this.state.step - 1].title}
           </div>
         }
-        {this.state.step < (this.state.navItems.length - 1) &&
-          <div className="pull-right">
-            {this.state.navItems[this.state.step + 1].title}
-            <Button circular icon primary className="multistep__btn next active" onClick={() => this.actualSubmit(1)}>
-              <Icon className="ns-arrow-right" />
-            </Button>
-          </div>
-        }
+        <div className="pull-right">
+          {this.state.step < (this.state.navItems.length - 1) ? (
+            <Aux>
+              {this.state.navItems[this.state.step + 1].title}
+              <Button circular icon primary className="multistep__btn next active" onClick={() => this.actualSubmit(1)}>
+                <Icon className="ns-arrow-right" />
+              </Button>
+            </Aux>
+          ) : <Button primary className="very relaxed" content="Submit" />
+          }
+        </div>
       </div>
     );
   }
