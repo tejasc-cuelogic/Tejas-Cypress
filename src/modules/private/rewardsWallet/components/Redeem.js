db9bb2c0af19aea1e23db3a050a1c26d70aa78fb
sagar.cuelogic <sagar.cuelogic@nextseed.com>
2018-06-14 17:55:40 +0530

    updating redeem reward modal styles

--- src/modules/private/rewardsWallet/components/Redeem.js
+++ src/modules/private/rewardsWallet/components/Redeem.js
@@ -34,18 +34,19 @@ const Redeem = props => (
       </Grid>
     </Modal.Content>
     <Modal.Content className="center-align">
-      <div>
+      <div className="reward-description">
         <Header as="h5">Description Header</Header>
         <p>
           The prime wagyu briskets from Akaushi Beef will go into the pit between midnight and 2am
           in preparation for lunch and dinner service that day.
         </p>
       </div>
-      <p>
+      <p className="note">
         Please present this digital voucher (on your phone or printed up) to California 88 to
         finish the redemption process
       </p>
       <Button primary size="large" onClose={props.history.goBack} className="very relaxed">Redeem Reward</Button>
+      <p className="note"><b>Serial #  326 5646 126</b></p>
     </Modal.Content>
   </Modal>
 );
