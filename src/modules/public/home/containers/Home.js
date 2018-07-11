f4f75560ef830b94c049ecdb6ee71b08a8ae1eba
sagar.cuelogic <vishakha.cuelogic@nextseed.com>
2018-07-11 20:23:36 +0530

    home page featured and learn more section UI

--- src/modules/public/home/containers/Home.js
+++ src/modules/public/home/containers/Home.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import Aux from 'react-aux';
 import { Link } from 'react-router-dom';
-import { Header, Container, Grid, Image, Button, Divider, Embed, Card, Label, Icon, List } from 'semantic-ui-react';
+import { Header, Form, Container, Grid, Image, Button, Divider, Embed, Card, Label, Icon, List } from 'semantic-ui-react';
 import UserOne from '../../../../assets/images/owner-1.jpg';
 import UserTwo from '../../../../assets/images/owner-2.jpg';
 import videoPoster from '../../../../assets/images/636206632.webp';
 import campaign1 from '../../../../assets/images/campaign-1.jpg';
 import campaign2 from '../../../../assets/images/campaign-2.jpg';
 import campaign3 from '../../../../assets/images/campaign-3.jpg';
+import bloomberg from '../../../../assets/images/bloomberg.png';
+import houstonCronical from '../../../../assets/images/houston-chronicle.png';
+import forbes from '../../../../assets/images/forbes.png';
+import newYorkTimes from '../../../../assets/images/new-york-times.png';
+import austinAmericanStatesman from '../../../../assets/images/austin-american-statesman.png';
+import wallstreetJournal from '../../../../assets/images/wallstreet-journal.png';
+import npr from '../../../../assets/images/npr.png';
+import time from '../../../../assets/images/time.png';
+import msnMoney from '../../../../assets/images/msn-money.png';
+import crowdfundInsider from '../../../../assets/images/crowdfund-insider.png';
+import pc from '../../../../assets/images/pc.png';
+import mashable from '../../../../assets/images/mashable.png';
+import usNews from '../../../../assets/images/us-news.png';
 
 const nsvideos = {
   embed: '218642510',
@@ -43,7 +56,7 @@ class Home extends Component {
             <Button secondary content="Secondary Button" /> */}
           </Container>
           <Container>
-            <Grid centered relaxed="very">
+            <Grid centered relaxed="very" stackable>
               <Grid.Column textAlign="center" width={6} className="info-card">
                 <Image src={UserOne} size="small" circular centered />
                 <Header as="h4">Raise funds without giving up ownership.</Header>
@@ -52,7 +65,7 @@ class Home extends Component {
                   fundraising. No need to sell ownership of your business. Simply create a
                   debt offering and invite your community to invest in your growth.
                 </p>
-                <Button primary content="How Investing Works" />
+                <Button primary content="How Fundraising Works" />
               </Grid.Column>
               <Grid.Column textAlign="center" width={6} className="info-card">
                 <Image src={UserTwo} size="small" circular centered />
@@ -71,7 +84,7 @@ class Home extends Component {
         <section>
           <Container>
             <Header as="h2" textAlign="center">How does NextSeed work?</Header>
-            <Grid centered relaxed="very">
+            <Grid centered>
               <Grid.Column width={12}>
                 <Embed
                   className="centered-video"
@@ -87,7 +100,7 @@ class Home extends Component {
         <section>
           <Container>
             <Header as="h2" textAlign="center">Latest Campaigns</Header>
-            <p className="caption text-center">Browse the newest investment opportunities on NextSeed. The next big thing may be inviting you to participate.</p>
+            <p className="caption center-align">Browse the newest investment opportunities on NextSeed. The next big thing may be inviting you to participate.</p>
             <Card.Group itemsPerRow={3} stackable>
               <Card className="campaign">
                 <Image src={campaign1} />
@@ -158,6 +171,79 @@ class Home extends Component {
                 </Card.Content>
               </Card>
             </Card.Group>
+            <div className="explore-campaign-button">
+              <Button secondary compact as={Link} to="/offerings/">Explore Campaigns</Button>
+            </div>
+          </Container>
+        </section>
+        <section className="featured-section">
+          <Container>
+            <Header as="h2" textAlign="center" className="caption">As seen on…</Header>
+            <Grid columns="equal" doubling>
+              <Grid.Row>
+                <Grid.Column>
+                  <Image src={bloomberg} />
+                </Grid.Column>
+                <Grid.Column>
+                  <Image src={houstonCronical} />
+                </Grid.Column>
+                <Grid.Column>
+                  <Image src={forbes} />
+                </Grid.Column>
+                <Grid.Column>
+                  <Image src={newYorkTimes} />
+                </Grid.Column>
+                <Grid.Column>
+                  <Image src={austinAmericanStatesman} />
+                </Grid.Column>
+                <Grid.Column>
+                  <Image src={wallstreetJournal} />
+                </Grid.Column>
+                <Grid.Column>
+                  <Image src={npr} />
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  <Image src={time} />
+                </Grid.Column>
+                <Grid.Column>
+                  <Image src={msnMoney} />
+                </Grid.Column>
+                <Grid.Column>
+                  <Image src={crowdfundInsider} />
+                </Grid.Column>
+                <Grid.Column>
+                  <Image src={pc} />
+                </Grid.Column>
+                <Grid.Column>
+                  <Image src={mashable} />
+                </Grid.Column>
+                <Grid.Column>
+                  <Image src={usNews} />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </section>
+        <section className="learn-more">
+          <Container textAlign="center">
+            <Header as="h2">Want to learn more about NextSeed?</Header>
+            <p className="caption">
+              Leave us your contact information and we’ll keep you posted
+              with the latest news and updates.
+            </p>
+            <Form>
+              <Form.Group>
+                <Form.Input inverted placeholder="Name" name="name" width={4} />
+                <Form.Input
+                  placeholder="Email"
+                  name="email"
+                  width={4}
+                />
+                <Form.Button primary content="Submit" compact width={2} />
+              </Form.Group>
+            </Form>
           </Container>
         </section>
       </Aux>
