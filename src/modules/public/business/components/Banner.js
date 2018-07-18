3211541ec563c19e7da0e73def351765f68ce7eb
pratik-tayade <pratik.tayade@cuelogic.com>
2018-07-18 20:13:22 +0530

    business subnavigation components

--- src/modules/public/business/components/Banner.js
+++ src/modules/public/business/components/Banner.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { Header, Container } from 'semantic-ui-react';
+import { Header, Container, Button } from 'semantic-ui-react';
 
 const Banner = () => (
-  <Container fluid className="banner home-banner">
+  <Container fluid className="banner business-banner">
     <Container>
       <div className="banner-caption">
         <Header as="h1">
-          Raise capital.<br />
-          Invest local.<br />
-          <span className="highlight-text">Thrive as a<br />
-            community.
-          </span>
+        Accelerate your<br />
+        growth with the<br />
+        power of the crowd
         </Header>
+        <Button secondary className="mt-50">Apply Now</Button>
       </div>
     </Container>
   </Container>
