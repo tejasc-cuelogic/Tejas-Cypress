7498c97d7f78dd24913d41dcd1b684494fa956ff
pratik-tayade <pratik.tayade@cuelogic.com>
2018-07-19 13:20:05 +0530

    business- compare, all-inclusive

--- src/modules/public/business/components/AllInclusive.js
+++ src/modules/public/business/components/AllInclusive.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Header, Container } from 'semantic-ui-react';
 
 const AllInclusive = () => (
-  <div>
-    AllInclusive
-  </div>
+  <section>
+    <Container>
+      <Header as="h2" className="mb-80" textAlign="center">
+        AllInclusive ...
+      </Header>
+    </Container>
+  </section>
 );
 
 export default AllInclusive;
--- src/modules/public/business/components/Compare.js
+++ src/modules/public/business/components/Compare.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Header, Container } from 'semantic-ui-react';
 
 const Compare = () => (
-  <div>
-    Compare
-  </div>
+  <section>
+    <Container>
+      <Header as="h2" className="mb-80" textAlign="center">
+        Compare ...
+      </Header>
+    </Container>
+  </section>
 );
 
 export default Compare;
