a200e5260df314d13203228cfaa4b2c0810422b1
sagar.cuelogic <sagar.cuelogic@nextseed.com>
2018-07-20 15:05:57 +0530

    updating logo svg files

--- src/theme/shared/src/Logo.js
+++ src/theme/shared/src/Logo.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import { Image } from 'semantic-ui-react';
-import LogoWhite from '../../../assets/images/logo.svg';
-import LogoColor from '../../../assets/images/nextseed_logo_white.svg';
-import LogoSmall from '../../../assets/images/ns-logo-small.svg';
+import LogoColor from '../../../assets/images/logo-color.svg';
+import LogoWhite from '../../../assets/images/logo-white.svg';
+import LogoWhiteGreen from '../../../assets/images/logo-white-green.svg';
+import LogoSmall from '../../../assets/images/logo-icon.svg';
+import LogoSmallWhite from '../../../assets/images/logo-icon-white.svg';
 import LogoLendio from '../../../assets/images/lendio_logo.svg';
 import LogoNsAndLendio from '../../../assets/images/nextseed_and_lendio.svg';
 
 const getSrc = (src) => {
   switch (src) {
-    case 'LogoWhite':
-      return LogoWhite;
     case 'LogoColor':
       return LogoColor;
+    case 'LogoWhite':
+      return LogoWhite;
+    case 'LogoWhiteGreen':
+      return LogoWhiteGreen;
     case 'LogoSmall':
       return LogoSmall;
+    case 'LogoSmallWhite':
+      return LogoSmallWhite;
     case 'LogoLendio':
       return LogoLendio;
     case 'LogoNsAndLendio':
       return LogoNsAndLendio;
     default:
-      return LogoWhite;
+      return LogoColor;
   }
 };
 
