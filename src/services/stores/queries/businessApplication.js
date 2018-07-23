33cd3ee50f8304c213c93ddffc7a0c5ba4e78e09
sagar.cuelogic <sagar@cuelogic.nextseed.com>
2018-07-23 20:44:45 +0530

    Submit with lendio changes are done

--- src/services/stores/queries/businessApplication.js
+++ src/services/stores/queries/businessApplication.js
@@ -233,3 +233,24 @@ query _getBusinessApplicationById ($id: String!) {
   }
 }
 `;
+
+export const submitPartneredWithLendio = gql`
+mutation submitPartneredWithLendio(
+  $applicationId: String!,
+  $preQualificationQuestions: PreQualificationQuestionsInput!,
+  $customerInformation: CustomerInformationInput!,
+  $agreeConditions: Boolean,
+  $sendDataToLendio: Boolean
+) {
+  submitPartneredWithLendio(
+    applicationId: $applicationId,
+    preQualificationQuestions: $preQualificationQuestions,
+    customerInformation: $customerInformation,
+    agreeConditions: $agreeConditions,
+    sendDataToLendio: $sendDataToLendio
+  ) {
+    status
+    url
+  }
+}
+`;
