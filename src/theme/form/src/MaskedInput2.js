2c617c5d14c6121854bece5dee96a874968073ef
Mayura Thorat <mayura.cuelogic@nextseed.com>
2018-07-31 12:06:49 +0530

    done changes for bank account manual form

--- src/theme/form/src/MaskedInput2.js
+++ src/theme/form/src/MaskedInput2.js
@@ -61,7 +61,21 @@ const MaskedInput2 = observer((props) => {
       ) : props.taxId ? (
         <NumberFormat type="text" format="##-#######" placeholder={placeHolder} {...props} value={value} onValueChange={values => props.changed(values, props.name)} error={!!error} />
       ) : props.accountNumber ? (
-        <NumberFormat type="text" placeholder={placeHolder} {...props} value={value} onValueChange={values => props.changed(values, props.name)} />
+        <NumberFormat
+          isAllowed={(values) => {
+              const accountNumber = values.value;
+              if (accountNumber.toString().length <= 17) {
+                return true;
+              }
+              return false;
+            }
+          }
+          type="text"
+          placeholder={placeHolder}
+          {...props}
+          value={value}
+          onValueChange={values => props.changed(values, props.name)}
+        />
       ) : props.routingNumber ? (
         <NumberFormat format="#########" type="text" placeholder={placeHolder} {...props} value={value} onValueChange={values => props.changed(values, props.name)} />
       ) : <NumberFormat placeholder={placeHolder} format="(###)-###-####" {...props} value={value} onChange={props.changed} error={!!error} mask="_" />
