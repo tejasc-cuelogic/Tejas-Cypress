4bf78b8e83920e81bf212117d499e5ba76adeb28
Chetan Bura <chetan.cuelogic@nextseed.com>
2018-08-02 15:13:47 +0530

    Added currency symbol

--- src/modules/private/investor/settings/components/investmentLimits/UpdateInvestmentLimits.js
+++ src/modules/private/investor/settings/components/investmentLimits/UpdateInvestmentLimits.js
@@ -61,6 +61,7 @@ export default class UpdateInvestmentLimits extends Component {
                     key={field}
                     name={field}
                     currency
+                    prefix="$ "
                     value={fields[field].value}
                     fielddata={fields[field]}
                     changed={maskingFieldChange}
