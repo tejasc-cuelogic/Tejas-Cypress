c7f42e00840e772c7e1a4cba255ed2c37f4dd002
Chetan Bura <chetan.cuelogic@nextseed.com>
2018-08-02 18:09:17 +0530

    API integration and feedback changes done

--- src/services/constants/user.js
+++ src/services/constants/user.js
@@ -24,17 +24,17 @@ export const securitySections = [
     action: ['change-password', 'Change Password'],
   },
   {
-    title: 'Multi-factor autentitaction',
-    description: 'You can choose your Active MFA Factor.',
-    descriptionNotAvailable: `Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellente sque dui, non felis.
-    Etiam ullamcorper. Suspendisse a pellente sque dui, non felis.`,
-    action: ['mfa', 'Manage multi-factor autentication'],
-  },
-  {
     title: 'Social Connect',
     description: `Your NextSeed account is not connected to any social media.
     If you wish to connect it now, click one of the button below.`,
     action: ['social-connect'],
+  },
+  {
+    title: 'Multi-factor Authentication',
+    description: 'You can choose your Active MFA Factor.',
+    descriptionNotAvailable: `Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellente sque dui, non felis.
+    Etiam ullamcorper. Suspendisse a pellente sque dui, non felis.`,
+    action: ['mfa', 'Manage multi-factor autentication'],
   },
 ];
 
