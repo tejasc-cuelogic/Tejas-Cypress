c8b6b0566b4015f0706b2c0915b1957cbf4cc783
Mayura Thorat <mayura.cuelogic@nextseed.com>
2018-08-03 17:46:01 +0530

    icon added for thank you note

--- src/modules/private/investor/settings/components/investmentLimits/accreditation/ThanksNote.js
+++ src/modules/private/investor/settings/components/investmentLimits/accreditation/ThanksNote.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Modal, Header, Divider } from 'semantic-ui-react';
+import { Modal, Header, Divider, Icon } from 'semantic-ui-react';
 
 const ThanksNote = props => (
   <Modal open closeIcon onClose={props.closeModal} size="tiny" closeOnDimmerClick={false}>
     <Modal.Header className="center-align signup-header">
+      <Icon.Group size="huge">
+        <Icon className="ns-document" />
+        <Icon corner color="orange" className="ns-reload-circle-line" />
+      </Icon.Group>
       <Header as="h3">Thank you! We are verifying your request</Header>
       <Divider />
     </Modal.Header>
