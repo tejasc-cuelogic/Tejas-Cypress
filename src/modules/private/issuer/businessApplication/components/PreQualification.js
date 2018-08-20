e43aedb6bb31c863478737a721c703250ad5f56e
Chetan Bura <chetan.cuelogic@nextseed.com>
2018-08-20 20:02:35 +0530

    Need Help added on public prequal page

--- src/modules/private/issuer/businessApplication/components/PreQualification.js
+++ src/modules/private/issuer/businessApplication/components/PreQualification.js
@@ -62,7 +62,7 @@ export default class PreQualification extends Component {
               subHeader={
                 <Aux>
                   Welcome to NextSeed! Run through this quick form to get pre-qualified.
-                  <Link target="_blank" to="/about/faq" className="link">Need help or have questions?</Link>
+                  <Link to={this.props.isPublic ? 'business-application/need-help' : 'need-help'} className="link">Need help or have questions?</Link>
                 </Aux>
               }
             />
