33ff654d76ec60ce5d1e952487b2945a0c1626e5
sagar.cuelogic <sagar.cuelogic@nextseed.com>
2018-08-21 16:05:21 +0530

    updating overview UI

--- src/modules/private/admin/applications/components/details/review/Overview.js
+++ src/modules/private/admin/applications/components/details/review/Overview.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/label-has-for */
 import React, { Component } from 'react';
-import Aux from 'react-aux';
+import { Link } from 'react-router-dom';
 import { inject, observer } from 'mobx-react';
 import { Form, Header, Button } from 'semantic-ui-react';
 import { FormInput } from '../../../../../../../theme/form';
@@ -16,29 +16,26 @@ export default class Overview extends Component {
     } = this.props.businessAppReviewStore;
     return (
       <div className="inner-content-spacer">
-        <Header as="h5">
+        <Header as="h4">
           Overview
-          <Button color="blue" className="ghost-button" onClick={() => addMore('OVERVIEW_FRM')}>+ Add Critical Point</Button>
+          <Link to={this.props.match.url} className="link" onClick={() => addMore('OVERVIEW_FRM')}><small>+ Add Critical Point</small></Link>
         </Header>
         <Form>
           {
-              OVERVIEW_FRM.fields.data.length ?
-              OVERVIEW_FRM.fields.data.map((overview, index) => (
-                <Aux>
-                  <label>{`Critical Point ${index + 1}`}</label>
-                  <FormInput
-                    type="text"
-                    name="criticalPoint"
-                    fielddata={overview.criticalPoint}
-                    changed={(e, result) => overviewEleChange(e, result, index)}
-                    ishidelabel
-                  />
-                </Aux>
-              )) : null
+            OVERVIEW_FRM.fields.data.length ?
+            OVERVIEW_FRM.fields.data.map((overview, index) => (
+              <FormInput
+                type="text"
+                name="criticalPoint"
+                label={`Critical Point ${index + 1}`}
+                fielddata={overview.criticalPoint}
+                changed={(e, result) => overviewEleChange(e, result, index)}
+              />
+            )) : null
           }
           <Button.Group className="pull-right">
             <Button disabled={!OVERVIEW_FRM.meta.isValid} secondary className="relaxed">Save</Button>
-            <Button disabled={!OVERVIEW_FRM.meta.isValid} primary className="relaxed" type="button">Submit for Approval</Button>
+            <Button disabled={!OVERVIEW_FRM.meta.isValid} primary type="button">Submit for Approval</Button>
           </Button.Group>
         </Form>
       </div>
