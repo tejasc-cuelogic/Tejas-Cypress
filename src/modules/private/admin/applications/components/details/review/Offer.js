6ea28f31e05943399ad5282fc97745021eace50b
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2018-08-22 18:40:44 +0530

    changed modal dimmer color and adjusted grid size

--- src/modules/private/admin/applications/components/details/review/Offer.js
+++ src/modules/private/admin/applications/components/details/review/Offer.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { Table } from 'semantic-ui-react';
 
 export default class Offer extends Component {
   render() {
     return (
       <div>
-        Offer - page!!!!
+        <Table basic compact className="form-table">
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>&nbsp;</Table.HeaderCell>
+              <Table.HeaderCell>Offer A</Table.HeaderCell>
+              <Table.HeaderCell />
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>
+              jghjgjhghj
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
       </div>
     );
   }
