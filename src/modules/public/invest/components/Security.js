da6dc7bbdfde0c01c2b498147e198e75d503347f
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2018-08-31 18:08:52 +0530

    responsive UI of security and track in investor module

--- src/modules/public/invest/components/Security.js
+++ src/modules/public/invest/components/Security.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Header, Button, Container, Grid, Image } from 'semantic-ui-react';
+import { Header, Button, Container, Grid, Image, Responsive } from 'semantic-ui-react';
+import Aux from 'react-aux';
 import Secure from '../../../../assets/images/secure-horizontal.png';
 
 const Security = () => (
@@ -8,15 +9,17 @@ const Security = () => (
     <Container>
       <Grid padded="vertically">
         <Grid.Row>
-          <Grid.Column floated="right" width={10} className="side-section">
+          <Grid.Column floated="right" computer={10} tablet={10} mobile={16} className="side-section">
             <Header as="h2" className="mb-30">Invest with peace of mind.</Header>
-            <Grid columns={2}>
+            <Grid columns={2} doubling stackable>
               <Grid.Column>
                 <Header as="h5">Your funds stay safe and sound.</Header>
                 <p>The uninvested cash in your account1 is FDIC-insured up to $250,000.</p>
                 <Header as="h5">Keep your information protected.</Header>
                 <p>We safeguard your information with bank-level security measures</p>
-                <Image src={Secure} />
+                <Responsive as={Aux} minWidth={768}>
+                  <Image src={Secure} />
+                </Responsive>
               </Grid.Column>
               <Grid.Column>
                 <Header as="h5">First SEC-registered funding portal in the U.S.</Header>
@@ -28,14 +31,23 @@ const Security = () => (
                 </p>
               </Grid.Column>
             </Grid>
-            <p className="note mt-50 mb-50">
-              1 NextSeed accounts are provided and held at our partner bank, Happy State Bank
-              DBA GoldStar Trust Company (&quot;GoldStar&quot;), which provides FDIC insurance
-              for uninvested cash in NextSeed accounts.
-            </p>
+            <Responsive as={Aux} minWidth={768}>
+              <p className="note mt-50 mb-50">
+                1 NextSeed accounts are provided and held at our partner bank, Happy State Bank
+                DBA GoldStar Trust Company (&quot;GoldStar&quot;), which provides FDIC insurance
+                for uninvested cash in NextSeed accounts.
+              </p>
+            </Responsive>
             <div className="center-align">
               <Button as={Link} to="/invest/track" primary>See Track</Button>
             </div>
+            <Responsive as={Aux} maxWidth={767}>
+              <p className="note mt-50">
+                1 NextSeed accounts are provided and held at our partner bank, Happy State Bank
+                DBA GoldStar Trust Company (&quot;GoldStar&quot;), which provides FDIC insurance
+                for uninvested cash in NextSeed accounts.
+              </p>
+            </Responsive>
           </Grid.Column>
         </Grid.Row>
       </Grid>
