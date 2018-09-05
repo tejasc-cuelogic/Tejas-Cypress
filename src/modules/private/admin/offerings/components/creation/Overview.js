019605b2fcf3e03284aa2bd8d7b9c04623ee98a6
sagar.cuelogic <sagar.cuelogic@nextseed.com>
2018-09-05 12:45:44 +0530

    adding overview page UI

--- src/modules/private/admin/offerings/components/creation/Overview.js
+++ src/modules/private/admin/offerings/components/creation/Overview.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Header, Checkbox } from 'semantic-ui-react';
+// import { FormCheckbox } from '../../../../../../theme/form';
 
 export default class Overview extends Component {
   render() {
     return (
-      <div>
-        In Overview page!
-      </div>
+      <Form>
+        <div className="inner-content-spacer">
+          <Header as="h4">
+            Launch Contingencies
+            <Link to="/" className="link"><small>+ Add Launch Contingency</small></Link>
+          </Header>
+          <div className="featured-section">
+            <Checkbox
+              label="test"
+              // values={
+              //   { label: 'Fashion & Merchandising', value: 'FASHION_AND_APPAREL' }
+              // }
+              name="checkbox1"
+              changed={this.handleTick}
+              defaults
+            />
+          </div>
+        </div>
+      </Form>
     );
   }
 }
