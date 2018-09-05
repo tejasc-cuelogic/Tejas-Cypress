10b9a3a8183d7283f80d6306f96c5ba5ae7c0446
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2018-09-05 18:57:17 +0530

    compare section UI

--- src/modules/public/business/components/Compare.js
+++ src/modules/public/business/components/Compare.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Container, Table, Header, List } from 'semantic-ui-react';
+import { Container, Table, Header, List, Responsive, Accordion, Icon } from 'semantic-ui-react';
 import Aux from 'react-aux';
+
+const isMobile = document.documentElement.clientWidth < 768;
 
 const Compare = () => (
   <Aux>
-    <section className="compare-section">
+    <section className="compare-section edu-center">
       <Container>
         <Header as="h2" textAlign="center" className="mb-30">See how we stack up.</Header>
-        <Table fixed celled definition striped className="mb-50 compare-table">
+        <Responsive minWidth={768} as={Table} celled definition striped className="mb-50 compare-table">
           <Table.Header>
             <Table.Row>
               <Table.HeaderCell />
@@ -17,10 +19,9 @@ const Compare = () => (
               <Table.HeaderCell>Equity Investors</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
-
           <Table.Body>
             <Table.Row>
-              <Table.Cell collapsing>
+              <Table.Cell>
               Amount
               </Table.Cell>
               <Table.Cell>$50k–$1MM</Table.Cell>
@@ -29,7 +30,7 @@ const Compare = () => (
               <Table.Cell>Variable</Table.Cell>
             </Table.Row>
             <Table.Row>
-              <Table.Cell collapsing>
+              <Table.Cell>
               Total Cost APR1
               </Table.Cell>
               <Table.Cell>Term Loans 10–18%<br />Revenue Sharing Notes 15–25%</Table.Cell>
@@ -38,7 +39,7 @@ const Compare = () => (
               <Table.Cell>30%+</Table.Cell>
             </Table.Row>
             <Table.Row>
-              <Table.Cell collapsing>
+              <Table.Cell>
               Fundraising Period
               </Table.Cell>
               <Table.Cell>21+ Days</Table.Cell>
@@ -47,7 +48,7 @@ const Compare = () => (
               <Table.Cell>3–9 Months</Table.Cell>
             </Table.Row>
             <Table.Row>
-              <Table.Cell collapsing>
+              <Table.Cell>
               Security
               </Table.Cell>
               <Table.Cell>Flexible collateral terms</Table.Cell>
@@ -56,7 +57,7 @@ const Compare = () => (
               <Table.Cell>Variable</Table.Cell>
             </Table.Row>
             <Table.Row>
-              <Table.Cell collapsing>
+              <Table.Cell>
               Capital Base
               </Table.Cell>
               <Table.Cell>Community, potential fans<br /> and customers</Table.Cell>
@@ -65,7 +66,7 @@ const Compare = () => (
               <Table.Cell>Typically accredited investors</Table.Cell>
             </Table.Row>
             <Table.Row>
-              <Table.Cell collapsing>
+              <Table.Cell>
               Additional Fees1
               </Table.Cell>
               <Table.Cell>5–10% success fee (all inclusive)</Table.Cell>
@@ -74,7 +75,7 @@ const Compare = () => (
               <Table.Cell>Expensive legal fees</Table.Cell>
             </Table.Row>
             <Table.Row>
-              <Table.Cell collapsing>
+              <Table.Cell>
               Marketing Benefits
               </Table.Cell>
               <Table.Cell>
@@ -88,8 +89,208 @@ const Compare = () => (
               <Table.Cell>None</Table.Cell>
             </Table.Row>
           </Table.Body>
-        </Table>
-        <List horizontal relaxed className="learn-more-list mb-50">
+        </Responsive>
+        <Responsive maxWidth={767} as={Accordion} className="splitted mb-50 compare-section">
+          <Accordion>
+            <Accordion.Title active>
+              Amount
+              <Icon className="ns-chevron-down" />
+            </Accordion.Title>
+            <Accordion.Content active>
+              <Table striped unstackable>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell className="highlight-text"><b>NextSeed</b></Table.HeaderCell>
+                    <Table.HeaderCell className="highlight-text"><b>$50k–$1MM</b></Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Cell>Bank</Table.Cell>
+                    <Table.Cell>$250k+</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Merchant Cash</Table.Cell>
+                    <Table.Cell>$20k–$100k</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Equity Investors</Table.Cell>
+                    <Table.Cell>Variable</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              </Table>
+            </Accordion.Content>
+            <Accordion.Title active>
+              Total cost APR
+              <Icon className="ns-chevron-down" />
+            </Accordion.Title>
+            <Accordion.Content active>
+              <Table striped unstackable>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell className="highlight-text"><b>NextSeed</b></Table.HeaderCell>
+                    <Table.HeaderCell className="highlight-text"><b>Term Loans 10–18%<br />Revenue Sharing 15–25%</b></Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Cell>Bank</Table.Cell>
+                    <Table.Cell>$250k+</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Merchant Cash</Table.Cell>
+                    <Table.Cell>$20k–$100k</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Equity Investors</Table.Cell>
+                    <Table.Cell>Variable</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              </Table>
+            </Accordion.Content>
+            <Accordion.Title active>
+              Funding Period
+              <Icon className="ns-chevron-down" />
+            </Accordion.Title>
+            <Accordion.Content active>
+              <Table striped unstackable>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell className="highlight-text"><b>NextSeed</b></Table.HeaderCell>
+                    <Table.HeaderCell className="highlight-text"><b>21+ days</b></Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Cell>Bank</Table.Cell>
+                    <Table.Cell>2–9 months</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Merchant Cash</Table.Cell>
+                    <Table.Cell>14+ days</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Equity Investors</Table.Cell>
+                    <Table.Cell>3–9 months</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              </Table>
+            </Accordion.Content>
+            <Accordion.Title active>
+              Security
+              <Icon className="ns-chevron-down" />
+            </Accordion.Title>
+            <Accordion.Content active>
+              <Table striped unstackable>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell className="highlight-text"><b>NextSeed</b></Table.HeaderCell>
+                    <Table.HeaderCell className="highlight-text"><b>Flexible collateral terms</b></Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Cell>Bank</Table.Cell>
+                    <Table.Cell>Personal guarantee & assets collateral</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Merchant Cash</Table.Cell>
+                    <Table.Cell>Daily cash withdrawl</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Equity Investors</Table.Cell>
+                    <Table.Cell>Variable</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              </Table>
+            </Accordion.Content>
+            <Accordion.Title active>
+              Capital Base
+              <Icon className="ns-chevron-down" />
+            </Accordion.Title>
+            <Accordion.Content active>
+              <Table striped unstackable>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell className="highlight-text"><b>NextSeed</b></Table.HeaderCell>
+                    <Table.HeaderCell className="highlight-text"><b>Community, fans and customers</b></Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Cell>Bank</Table.Cell>
+                    <Table.Cell>Bank</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Merchant Cash</Table.Cell>
+                    <Table.Cell>Institutional lenders</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Equity Investors</Table.Cell>
+                    <Table.Cell>Typically accredited investors</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              </Table>
+            </Accordion.Content>
+            <Accordion.Title active>
+              Additional Fees
+              <Icon className="ns-chevron-down" />
+            </Accordion.Title>
+            <Accordion.Content active>
+              <Table striped unstackable className="mb-50">
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell className="highlight-text"><b>NextSeed</b></Table.HeaderCell>
+                    <Table.HeaderCell className="highlight-text"><b>5–10%  sucess fee<br />(all inclusive)</b></Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Cell>Bank</Table.Cell>
+                    <Table.Cell>3–8% origination fee</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Merchant Cash</Table.Cell>
+                    <Table.Cell>3–10% origination fee</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Equity Investors</Table.Cell>
+                    <Table.Cell>Expensive legal fees</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              </Table>
+            </Accordion.Content>
+            <Accordion.Title active>
+              Marketing Benefits
+              <Icon className="ns-chevron-down" />
+            </Accordion.Title>
+            <Accordion.Content active>
+              <Table striped unstackable className="mb-50">
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell className="highlight-text"><b>NextSeed</b></Table.HeaderCell>
+                    <Table.HeaderCell className="highlight-text"><b>Smart army of advocates Community events Social media Targeted marketing</b></Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Cell>Bank</Table.Cell>
+                    <Table.Cell>Bank</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Merchant Cash</Table.Cell>
+                    <Table.Cell>Institutional lenders</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Equity Investors</Table.Cell>
+                    <Table.Cell>Typically accredited investors</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              </Table>
+            </Accordion.Content>
+          </Accordion>
+        </Responsive>
+        <List horizontal={!isMobile} relaxed className="learn-more-list mb-50">
           <List.Item>
             <List.Header>Learn more</List.Header>
             <List.Icon className="ns-arrow-right" color="green" />
