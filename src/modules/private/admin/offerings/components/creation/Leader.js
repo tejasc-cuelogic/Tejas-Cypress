c7191635787e3b9b144848f30f478b3182d101c0
Mayura Thorat <mayura.cuelogic@nextseed.com>
2018-09-10 17:49:03 +0530

    done changes for leadership tab

---
+++ src/modules/private/admin/offerings/components/creation/Leader.js
@@ -0,0 +1,226 @@
+import React, { Component } from 'react';
+import Aux from 'react-aux';
+import { inject, observer } from 'mobx-react';
+import { Form, Header, Button, Divider, Confirm, Icon } from 'semantic-ui-react';
+import { withRouter } from 'react-router-dom';
+import { FormCheckbox, FormInput, MaskedInput, FormTextarea, DropZone } from '../../../../../../theme/form';
+
+@inject('offeringCreationStore', 'uiStore')
+@withRouter
+@observer
+export default class Leader extends Component {
+  onFileDrop = (files) => {
+    this.props.offeringCreationStore.setFileUploadData('KEY_TERMS_FRM', 'proFormas', files);
+  }
+  confirmRemoveDoc = (e, name) => {
+    e.preventDefault();
+    this.props.uiStore.setConfirmBox(name);
+  }
+  handleDelCancel = () => {
+    this.props.uiStore.setConfirmBox('');
+  }
+  handleDelDoc = (field) => {
+    this.props.offeringCreationStore.removeUploadedData('KEY_TERMS_FRM', field);
+    this.props.uiStore.setConfirmBox('');
+  }
+  render() {
+    const leaderNumber = this.props.index;
+    const index = leaderNumber || 0;
+    const {
+      LEADERSHIP_FRM,
+      formChangeWithIndex,
+      maskChangeWithIndex,
+    } = this.props.offeringCreationStore;
+    const { confirmBox } = this.props.uiStore;
+    return (
+      <Aux>
+        <Form>
+          <Header as="h4">
+            {`Leader ${index + 1}`}
+            <Button.Group className="pull-right">
+              <Button secondary className="relaxed" disabled={!LEADERSHIP_FRM.meta.isValid} >Save</Button>
+              <Button inverted color="red" content="Delete Leader" />
+            </Button.Group>
+          </Header>
+          <FormCheckbox
+            fielddata={LEADERSHIP_FRM.fields.data[index].includeInOfferingPage}
+            name="includeInOfferingPage"
+            changed={(e, result) => formChangeWithIndex(e, result, 'LEADERSHIP_FRM', index)}
+            defaults
+            containerclassname="ui relaxed list"
+          />
+          <Header as="h4">
+            Personal Info
+          </Header>
+          <Form.Group widths="equal">
+            {
+              ['firstName', 'lastName'].map(field => (
+                <FormInput
+                  name={field}
+                  fielddata={LEADERSHIP_FRM.fields.data[index][field]}
+                  changed={(e, result) => formChangeWithIndex(e, result, 'LEADERSHIP_FRM', index)}
+                />
+              ))
+            }
+          </Form.Group>
+          <Form.Group widths="equal">
+            <FormInput
+              name="emailAddress"
+              fielddata={LEADERSHIP_FRM.fields.data[index].emailAddress}
+              changed={(e, result) => formChangeWithIndex(e, result, 'LEADERSHIP_FRM', index)}
+            />
+            <MaskedInput
+              name="phoneNumber"
+              fielddata={LEADERSHIP_FRM.fields.data[index].phoneNumber}
+              format="###-###-####"
+              changed={(values, name) => maskChangeWithIndex(values, 'LEADERSHIP_FRM', name, index)}
+              phoneNumber
+            />
+          </Form.Group>
+          <Form.Group widths="equal">
+            <MaskedInput
+              name="dob"
+              fielddata={LEADERSHIP_FRM.fields.data[index].dob}
+              format="##/##/####"
+              changed={(values, name) => maskChangeWithIndex(values, 'LEADERSHIP_FRM', name, index)}
+              dateOfBirth
+            />
+            <MaskedInput
+              name="ssn"
+              type="tel"
+              fielddata={LEADERSHIP_FRM.fields.data[index].ssn}
+              ssn
+              changed={(values, name) => maskChangeWithIndex(values, 'LEADERSHIP_FRM', name, index)}
+            />
+            <FormInput
+              name="countryOfCitizanship"
+              fielddata={LEADERSHIP_FRM.fields.data[index].countryOfCitizanship}
+              changed={(e, result) => formChangeWithIndex(e, result, 'LEADERSHIP_FRM', index)}
+            />
+          </Form.Group>
+          <Form.Group widths="equal">
+            <MaskedInput
+              name="percentageOwned"
+              fielddata={LEADERSHIP_FRM.fields.data[index].percentageOwned}
+              percentage
+              changed={(values, name) => maskChangeWithIndex(values, 'LEADERSHIP_FRM', name, index)}
+            />
+            <FormInput
+              name="companyPosition"
+              fielddata={LEADERSHIP_FRM.fields.data[index].companyPosition}
+              changed={(e, result) => formChangeWithIndex(e, result, 'LEADERSHIP_FRM', index)}
+            />
+            <MaskedInput
+              name="startDateOfService"
+              fielddata={LEADERSHIP_FRM.fields.data[index].startDateOfService}
+              format="##-##-####"
+              changed={(values, name) => maskChangeWithIndex(values, 'LEADERSHIP_FRM', name, index)}
+              dateOfBirth
+            />
+          </Form.Group>
+          <Header as="h4">
+            Address
+          </Header>
+          <FormInput
+            name="residentialStreet"
+            fielddata={LEADERSHIP_FRM.fields.data[index].residentialStreet}
+            changed={(e, result) => formChangeWithIndex(e, result, 'LEADERSHIP_FRM', index)}
+          />
+          <Form.Group widths="equal">
+            {
+              ['city', 'state'].map(field => (
+                <FormInput
+                  name={field}
+                  fielddata={LEADERSHIP_FRM.fields.data[index][field]}
+                  changed={(e, result) => formChangeWithIndex(e, result, 'LEADERSHIP_FRM', index)}
+                />
+              ))
+            }
+            <MaskedInput
+              name="zipCode"
+              fielddata={LEADERSHIP_FRM.fields.data[index].zipCode}
+              changed={(values, name) => maskChangeWithIndex(values, 'LEADERSHIP_FRM', name, index)}
+              zipCode
+            />
+          </Form.Group>
+          <Header as="h4">
+            Bio
+          </Header>
+          <FormTextarea
+            name="bio"
+            fielddata={LEADERSHIP_FRM.fields.data[index].bio}
+            changed={(e, result) => formChangeWithIndex(e, result, 'LEADERSHIP_FRM', index)}
+            containerclassname="secondary"
+            hidelabel
+          />
+          <Divider section />
+          <Header as="h4">
+            Website and Social Profiles
+          </Header>
+          {
+            ['website', 'facebook', 'linkedIn', 'twitter'].map(field => (
+              <FormInput
+                name={field}
+                fielddata={LEADERSHIP_FRM.fields.data[index][field]}
+                changed={(e, result) => formChangeWithIndex(e, result, 'LEADERSHIP_FRM', index)}
+              />
+            ))
+          }
+          <Divider section />
+          <Header as="h4">
+            Uploads
+          </Header>
+          <Form.Group widths="equal">
+            {
+              ['headShot', 'heroImage', 'driverLicense'].map(field => (
+                <DropZone
+                  name={field}
+                  fielddata={LEADERSHIP_FRM.fields.data[index][field]}
+                  ondrop={this.onFileDrop}
+                  onremove={this.confirmRemoveDoc}
+                  uploadtitle="Upload a file"
+                />
+              ))
+            }
+          </Form.Group>
+          <Divider section />
+          <Header as="h4">
+            Other Business Information
+          </Header>
+          {
+            ['namesOfOtherEntities', 'namesOfPromoters'].map(field => (
+              <FormTextarea
+                name={field}
+                fielddata={LEADERSHIP_FRM.fields.data[index][field]}
+                changed={(e, result) => formChangeWithIndex(e, result, 'LEADERSHIP_FRM', index)}
+                containerclassname="secondary"
+              />
+            ))
+          }
+          <div className="clearfix mb-20">
+            <Button as="span" className="time-stamp">
+              <Icon className="ns-check-circle" color="green" />
+              Submitted by ISSUER_NAME on 2/3/2018
+            </Button>
+          </div>
+          <div className="clearfix">
+            <Button as="span" className="time-stamp">
+              <Icon className="ns-check-circle" color="green" />
+              Approved by MANAGER_NAME on 2/3/2018
+            </Button>
+          </div>
+        </Form>
+        <Confirm
+          header="Confirm"
+          content="Are you sure you want to remove this file?"
+          open={confirmBox.entity === 'headShot' || confirmBox.entity === 'heroImage' || confirmBox.entity === 'driverLicense'}
+          onCancel={this.handleDelCancel}
+          onConfirm={() => this.handleDelDoc(confirmBox.entity)}
+          size="mini"
+          className="deletion"
+        />
+      </Aux>
+    );
+  }
+}
+
--- src/modules/private/admin/offerings/components/creation/Leadership.js
+++ src/modules/private/admin/offerings/components/creation/Leadership.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
+import { Grid, Button } from 'semantic-ui-react';
+import { Switch, Route } from 'react-router-dom';
+import SecondaryMenu from '../../../../../../theme/layout/SecondaryMenu';
+import Leader from './Leader';
 
+@inject('offeringCreationStore')
+@observer
 export default class Leadership extends Component {
+  componentWillMount() {
+    if (this.props.match.isExist) {
+      this.props.history.push('./leader/1');
+    }
+  }
+  addMore = (e, formName) => {
+    e.preventDefault();
+    this.props.offeringCreationStore.addMore(formName);
+  }
   render() {
+    const { match } = this.props;
+    const { LEADERSHIP_FRM } = this.props.offeringCreationStore;
+    const navItems = [];
+    const formName = 'LEADERSHIP_FRM';
+    LEADERSHIP_FRM.fields.data.map((leader, index) => {
+      navItems.push({ title: `Leader ${index + 1}`, to: `leader/${index + 1}` });
+      return navItems;
+    });
     return (
-      <div>
-        In Leadership page!
+      <div className="inner-content-spacer">
+        <Grid>
+          <Grid.Column widescreen={4} computer={3} tablet={3} mobile={16}>
+            <SecondaryMenu secondary vertical match={match} navItems={navItems} />
+            <Button size="small" color="blue" className="link-button" onClick={e => this.addMore(e, formName)}>+ Add another leader</Button>
+          </Grid.Column>
+          <Grid.Column widescreen={12} computer={13} tablet={13} mobile={16}>
+            <Switch>
+              <Route exact path={match.url} render={props => <Leader {...props} />} />
+              {
+                navItems.map((item, index) => (
+                  <Route exact={false} key={item.to} path={`${match.url}/${item.to}`} render={props => <Leader refLink={match.url} {...props} index={index || 0} />} />
+                ))
+              }
+            </Switch>
+          </Grid.Column>
+        </Grid>
       </div>
     );
   }
