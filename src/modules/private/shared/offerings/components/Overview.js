5a82607fe86f3a11d3ca1694582a4d06216c3a6b
Mayura Thorat <mayura.cuelogic@nextseed.com>
2018-09-11 12:44:48 +0530

    done chnages to show offering url and referral code part in overview to all admin users

--- src/modules/private/shared/offerings/components/Overview.js
+++ src/modules/private/shared/offerings/components/Overview.js
@@ -2,18 +2,16 @@
 import React, { Component } from 'react';
 import { withRouter, Route } from 'react-router-dom';
 import { observer, inject } from 'mobx-react';
-import Aux from 'react-aux';
 import { Form, Header } from 'semantic-ui-react';
 import Contingency from './overview/Contingency';
 import AddNewContingency from './overview/AddNewContingency';
 import { FormInput } from '../../../../../theme/form';
 
 @withRouter
-@inject('offeringCreationStore', 'userStore')
+@inject('offeringCreationStore')
 @observer
 export default class Overview extends Component {
   render() {
-    const { roles } = this.props.userStore.currentUser;
     const {
       LAUNCH_CONTITNGENCIES_FRM,
       CLOSING_CONTITNGENCIES_FRM,
@@ -26,24 +24,20 @@ export default class Overview extends Component {
       <Form>
         <div className="inner-content-spacer">
           <Route exact path={`${match.url}/add-new-contingency`} render={props => <AddNewContingency refLink={match.url} {...props} />} />
-          {(roles && (roles.includes('support') || roles.includes('admin'))) &&
-          <Aux>
-            <Header as="h4">
-            Offering Details
-            </Header>
-            <Form.Group widths={2}>
-              {
-                ['offeringUrl', 'offeringReferralCode'].map(field => (
-                  <FormInput
-                    name={field}
-                    fielddata={OFFERING_DETAILS_FRM.fields[field]}
-                    changed={(e, result) => formChange(e, result, 'OFFERING_DETAILS_FRM')}
-                  />
-                ))
-              }
-            </Form.Group>
-          </Aux>
-          }
+          <Header as="h4">
+          Offering Details
+          </Header>
+          <Form.Group widths={2}>
+            {
+              ['offeringUrl', 'offeringReferralCode'].map(field => (
+                <FormInput
+                  name={field}
+                  fielddata={OFFERING_DETAILS_FRM.fields[field]}
+                  changed={(e, result) => formChange(e, result, 'OFFERING_DETAILS_FRM')}
+                />
+              ))
+            }
+          </Form.Group>
           <Contingency formChangeWithIndex={formChangeWithIndex} form={LAUNCH_CONTITNGENCIES_FRM} formName="LAUNCH_CONTITNGENCIES_FRM" />
           <Contingency formChangeWithIndex={formChangeWithIndex} form={CLOSING_CONTITNGENCIES_FRM} formName="CLOSING_CONTITNGENCIES_FRM" />
         </div>
