986289cb98a24c826886eea3ca893ead6d9ba3a8
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2018-09-12 17:49:01 +0530

    campaign overview, about company and disclosure UI

--- src/modules/public/offering/components/campaignDetails/Disclosures.js
+++ src/modules/public/offering/components/campaignDetails/Disclosures.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Modal } from 'semantic-ui-react';
 
 class Disclosures extends Component {
+  handleClose = () => this.props.history.goBack();
   render() {
     return (
-      <div className="campaign-about-wrapper">
+      <Modal
+        open
+        size="large"
+        onClose={this.handleClose}
+        closeIcon
+      >
         <Header as="h3">Disclosures</Header>
-        <div className="pdf-viewer">
-          <object width="100%" height="100%" data="https://s3.amazonaws.com/dev-cdn.nextseed.qa/welcome-packet/offeringpageignited.pdf" type="application/pdf">failed to load..</object>
-        </div>
-      </div>
+        <Modal.Content>
+          <div className="pdf-viewer">
+            <object width="100%" height="100%" data="https://s3.amazonaws.com/dev-cdn.nextseed.qa/welcome-packet/offeringpageignited.pdf" type="application/pdf">failed to load..</object>
+          </div>
+        </Modal.Content>
+      </Modal>
     );
   }
 }
