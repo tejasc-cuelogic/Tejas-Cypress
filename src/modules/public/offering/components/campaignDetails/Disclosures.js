4437e045c27b3c6aa5612204ed8a2ae8682f4e0f
vishakhanimkar <er.vishakhanimkar@gmail.com>
2018-09-14 15:59:09 +0530

    changed disclosure view

--- src/modules/public/offering/components/campaignDetails/Disclosures.js
+++ src/modules/public/offering/components/campaignDetails/Disclosures.js
@@ -1,23 +1,15 @@
 import React, { Component } from 'react';
-import { Header, Modal } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
 
 class Disclosures extends Component {
-  handleClose = () => this.props.history.goBack();
   render() {
     return (
-      <Modal
-        open
-        size="large"
-        onClose={this.handleClose}
-        closeIcon
-      >
+      <div className="campaign-about-wrapper">
         <Header as="h3">Disclosures</Header>
-        <Modal.Content>
-          <div className="pdf-viewer">
-            <object width="100%" height="100%" data="https://s3.amazonaws.com/dev-cdn.nextseed.qa/welcome-packet/offeringpageignited.pdf" type="application/pdf">failed to load..</object>
-          </div>
-        </Modal.Content>
-      </Modal>
+        <div className="pdf-viewer">
+          <object width="100%" height="100%" data="https://s3.amazonaws.com/dev-cdn.nextseed.qa/welcome-packet/offeringpageignited.pdf" type="application/pdf">failed to load..</object>
+        </div>
+      </div>
     );
   }
 }
