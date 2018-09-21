29009eff9dfb2bb60b54373375726a3e9fdc7661
Mayura Thorat <mayura.cuelogic@nextseed.com>
2018-09-21 18:42:52 +0530

    done changes for bonus rewards

--- src/modules/private/shared/offerings/components/BonusRewardsCreation.js
+++ src/modules/private/shared/offerings/components/BonusRewardsCreation.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import Aux from 'react-aux';
 import { Button } from 'semantic-ui-react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import AddNewTier from './bonusrewards/AddNewTier';
 
 export default class BonusRewardsCreation extends Component {
   render() {
     const { match } = this.props;
     return (
-      <Aux>
+      <div className="inner-content-spacer">
         <Route path={`${match.url}/add-new-tier`} render={props => <AddNewTier refLink={match.url} {...props} />} />
-        <Button floated="right" primary content="Add new rewards tier" />
-      </Aux>
+        <Button as={Link} to={`${match.url}/add-new-tier`} floated="right" primary content="Add new rewards tier" />
+      </div>
     );
   }
 }
