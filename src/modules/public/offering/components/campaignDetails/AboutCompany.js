0a81546758567ac83def4eebce39707b12308166
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2018-09-21 20:26:53 +0530

    removed class

--- src/modules/public/offering/components/campaignDetails/AboutCompany.js
+++ src/modules/public/offering/components/campaignDetails/AboutCompany.js
@@ -168,12 +168,10 @@ class AboutCompany extends Component {
                   </Breadcrumb.Section>
                   <Breadcrumb.Divider icon={{ className: 'ns-chevron-right', color: 'green' }} />
                 </Breadcrumb>
-                <div className="location-analysis">
-                  <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.8980695673813!2d73.87562555088532!3d18.53350778733976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c0f824992459%3A0x4f126e7b4c0ac0f6!2sCuelogic+Technologies!5e0!3m2!1sen!2sin!4v1530687811942"
-                    title="test"
-                  />
-                </div>
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.8980695673813!2d73.87562555088532!3d18.53350778733976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c0f824992459%3A0x4f126e7b4c0ac0f6!2sCuelogic+Technologies!5e0!3m2!1sen!2sin!4v1530687811942"
+                  title="test"
+                />
               </Segment>
             </Grid.Column>
           </Grid.Row>
