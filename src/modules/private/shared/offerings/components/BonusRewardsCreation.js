aa9e6b9e1ca2d9b4477f2e1569af45950795ba58
mahesh <mahesh.auti@cuelogic.com>
2018-09-24 18:15:31 +0530

    done changes on admin offering creation pages

--- src/modules/private/shared/offerings/components/BonusRewardsCreation.js
+++ src/modules/private/shared/offerings/components/BonusRewardsCreation.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Header } from 'semantic-ui-react';
 import { Route, Link } from 'react-router-dom';
 import AddNewTier from './bonusrewards/AddNewTier';
 import AddNewBonusReward from './bonusrewards/addNewBonusRewards';
@@ -7,7 +7,19 @@ import AddNewBonusReward from './bonusrewards/addNewBonusRewards';
 
 const tiersArray = [
   { title: 'Early Birds' },
-  { title: 'Invest $500 or more' },
+  {
+    title: 'Invest $500 or more',
+    bonusRewards: [{
+      title: '5 Class Series + 5 Free Shoe Rentals',
+      description: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. ',
+      expDate: 'May 23, 2018',
+    },
+    {
+      title: 'Opening Night Invitation',
+      description: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper.',
+      expDate: 'May 23, 2018',
+    }],
+  },
   { title: 'Invest $1000 or more' },
   { title: 'Invest $5000 or more' },
 ];
@@ -24,7 +36,20 @@ export default class BonusRewardsCreation extends Component {
         {
           tiersArray.map(data => (
             <div className="featured-section mb-20">
-              <p> {data.title} </p>
+              <Header as="h4"> {data.title} </Header>
+              {data.bonusRewards &&
+              data.bonusRewards.map(item => (
+                <div className="featured-section mb-20">
+                  <Header as="h5">{item.title}</Header>
+                  <p>{item.description}</p>
+                  <p>Exp Date: {item.expDate}</p>
+                  <Button.Group>
+                    <Button inverted size="mini" color="blue" content="Edit" />
+                    <Button type="button" size="mini" color="red" content="Delete" />
+                  </Button.Group>
+                </div>
+              ))
+              }
               <Button size="small" color="blue" as={Link} to={`${match.url}/add-new-bonus-reward`} className="link-button">+ Add next bonus reward</Button>
             </div>
           ))
