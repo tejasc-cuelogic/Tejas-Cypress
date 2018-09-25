69f5aa103dad315d74ee43f5b0e2bb7b80dc72e5
mahesh <mahesh.auti@cuelogic.com>
2018-09-25 11:58:49 +0530

    done changes for bonus rewards creation

--- src/modules/private/shared/offerings/components/BonusRewardsCreation.js
+++ src/modules/private/shared/offerings/components/BonusRewardsCreation.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Button, Header } from 'semantic-ui-react';
+import { Button, Header, Confirm } from 'semantic-ui-react';
+import { inject, observer } from 'mobx-react';
 import { Route, Link } from 'react-router-dom';
 import AddNewTier from './bonusrewards/AddNewTier';
 import AddNewBonusReward from './bonusrewards/addNewBonusRewards';
+import Helper from '../../../../../helper/utility';
 
 
 const tiersArray = [
@@ -23,8 +25,20 @@ const tiersArray = [
   { title: 'Invest $1000 or more' },
   { title: 'Invest $5000 or more' },
 ];
+
+@inject('offeringCreationStore')
+@observer
 export default class BonusRewardsCreation extends Component {
+  toggleConfirmModal = (e, index, formName) => {
+    e.preventDefault();
+    this.props.offeringCreationStore.toggleConfirmModal(index, formName);
+  }
+  removeData = (confirmModalName) => {
+    this.props.offeringCreationStore.removeData(confirmModalName);
+    Helper.toast('Contingency has been deleted successfully.', 'success');
+  }
   render() {
+    const { confirmModal, confirmModalName } = this.props.offeringCreationStore;
     const { match } = this.props;
     return (
       <div className="inner-content-spacer">
@@ -39,14 +53,14 @@ export default class BonusRewardsCreation extends Component {
             <div className="featured-section mb-20">
               <Header as="h4"> {data.title} </Header>
               {data.bonusRewards &&
-              data.bonusRewards.map(item => (
+              data.bonusRewards.map((item, index) => (
                 <div className="featured-section mb-20">
                   <Header as="h5">{item.title}</Header>
                   <p>{item.description}</p>
                   <p>Exp Date: {item.expDate}</p>
                   <Button.Group>
                     <Button inverted size="mini" color="blue" content="Edit" as={Link} to={`${match.url}/edit-new-bonus-reward`} />
-                    <Button type="button" size="mini" color="red" content="Delete" />
+                    <Button type="button" size="mini" color="red" content="Delete" onClick={e => this.toggleConfirmModal(e, index, 'ADD_NEW_BONUS_REWARD_FRM')} />
                   </Button.Group>
                 </div>
               ))
@@ -55,6 +69,15 @@ export default class BonusRewardsCreation extends Component {
             </div>
           ))
         }
+        <Confirm
+          header="Confirm"
+          content="Are you sure you want to remove this contingency?"
+          open={confirmModal}
+          onCancel={this.toggleConfirmModal}
+          onConfirm={() => this.removeData(confirmModalName)}
+          size="mini"
+          className="deletion"
+        />
       </div>
     );
   }
