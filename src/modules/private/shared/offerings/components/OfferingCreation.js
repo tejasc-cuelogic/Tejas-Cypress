5134c5df2e58af1aac182c78cf2303c0dfc6608a
sagar.cuelogic <sagar.cuelogic@nextseed.com>
2018-09-27 13:01:55 +0530

    updating offering creation UI

--- src/modules/private/shared/offerings/components/OfferingCreation.js
+++ src/modules/private/shared/offerings/components/OfferingCreation.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-import Aux from 'react-aux';
+// import Aux from 'react-aux';
 import { observer, inject } from 'mobx-react';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import OfferingModule from './index';
 import SecondaryMenu from '../../../../../theme/layout/SecondaryMenu';
 
 @withRouter
-@inject('offeringsStore', 'navStore')
+@inject('offeringsStore', 'navStore', 'userStore')
 @observer
 export default class OfferingCreation extends Component {
   render() {
     const { match, navStore } = this.props;
+    const { isIssuer } = this.props.userStore;
     let navItems = navStore.specificNavs.subNavigations;
     navItems = navStore.filterByAccess(navItems, 1, ['overview']); // navigation of creation stage
     return (
-      <Aux>
-        <SecondaryMenu force2ary match={match} navItems={navItems} />
+      <div className={isIssuer ? 'ui card fluid' : ''}>
+        <SecondaryMenu force2ary={!isIssuer} match={match} navItems={navItems} />
         <Switch>
           <Route exact path={match.url} component={OfferingModule('overview')} />
           {
@@ -24,7 +25,7 @@ export default class OfferingCreation extends Component {
             ))
           }
         </Switch>
-      </Aux>
+      </div>
     );
   }
 }
