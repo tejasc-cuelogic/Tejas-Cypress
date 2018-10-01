481a02b5db3b3a0ed6259ae40ca0da044b77c1b8
pratik <pratik.cuelogic@nextseed.com>
2018-10-01 19:47:36 +0530

    stage wise load bonus reward

--- src/modules/private/shared/offerings/components/BonusRewards.js
+++ src/modules/private/shared/offerings/components/BonusRewards.js
@@ -1,73 +1,14 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { observer, inject } from 'mobx-react';
-import Aux from 'react-aux';
-import { Header, Form, Grid, Button, Divider } from 'semantic-ui-react';
-import { ByKeyword, DropdownFilter } from '../../../../../theme/form/Filters';
-import RewardList from './rewards/RewardList';
+import Report from './bonusrewards/Report';
+import Creation from './bonusrewards/Creation';
 
-const rewards = [
-  {
-    id: 1, name: 'Anna Maria Bach', amount: 1200, redeemDate: '12-23-2018',
-  },
-  {
-    id: 2, name: 'Evelinne Cyclist', amount: 950, redeemDate: '7-11-2018',
-  },
-  {
-    id: 3, name: 'Bob Brown', amount: 1040, redeemDate: '12-22-2018',
-  },
-];
-
-@inject('offeringCreationStore', 'userStore', 'offeringsStore')
+@inject('offeringsStore')
 @observer
 export default class BonusRewards extends Component {
-  setSearchParam = (e, { name, value }) => {
-    this.props.offeringCreationStore.setInitiateSrch(name, value);
-  }
-  executeSearch = (e) => {
-    if (e.charCode === 13) {
-      this.props.offeringCreationStore.setInitiateSrch('keyword', e.target.value);
-    }
-  }
   render() {
-    const { requestState } = this.props.offeringCreationStore;
-    const { isIssuer } = this.props.userStore;
-    const { offer } = this.props.offeringsStore;
-    return (
-      <Aux>
-        <Form className={offer.stage !== 'CREATION' ? 'search-filters more inner-content-spacer' : ''}>
-          <Grid stackable className="bottom-aligned">
-            <Grid.Row>
-              <ByKeyword
-                executeSearch={this.executeSearch}
-                w={[7]}
-                placeholder="Search by name"
-                more="no"
-                addon={
-                  <Aux>
-                    <Grid.Column width={6}>
-                      <DropdownFilter value={requestState.search.amount} name="Investment amount" change={this.setSearchParam} options={[]} />
-                    </Grid.Column>
-                    <Grid.Column width={3} textAlign="right">
-                      <Button primary floated="right">Manage PIN</Button>
-                    </Grid.Column>
-                  </Aux>
-                }
-              />
-            </Grid.Row>
-          </Grid>
-        </Form>
-        <div className={offer.stage !== 'CREATION' ? 'inner-content-spacer' : ''}>
-          {isIssuer &&
-            <Divider hidden />
-          }
-          <Header as="h4">
-            Bonus rewards
-            <Header.Subheader>For more information about bonus rewards, check out our <Link to="/">Resource Article.</Link></Header.Subheader>
-          </Header>
-          <RewardList listOf="Early bird reward" data={rewards} />
-        </div>
-      </Aux>
-    );
+    const { match, offeringsStore } = this.props;
+    const { offer } = offeringsStore;
+    return offer.stage === 'CREATION' ? <Creation match={match} /> : <Report />;
   }
 }
