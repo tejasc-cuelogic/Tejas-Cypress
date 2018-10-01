714662fd28e0456cb6a334d609777e3892680aa7
sagarng31 <sagarng31@gmail.com>
2018-10-01 15:59:01 +0530

    resolving creation and issuer condition issue

--- src/modules/private/shared/offerings/components/Legal.js
+++ src/modules/private/shared/offerings/components/Legal.js
@@ -44,9 +44,10 @@ export default class Legal extends Component {
       },
     ];
     const { match } = this.props;
+    const { isIssuer } = this.props.userStore;
     const { offer } = this.props.offeringsStore;
     return (
-      <div className={offer.stage !== 'CREATION' ? 'inner-content-spacer' : ''}>
+      <div className={offer.stage !== 'CREATION' && !isIssuer ? 'inner-content-spacer' : ''}>
         <Grid>
           <Grid.Column widescreen={4} computer={3} tablet={3} mobile={16}>
             <SecondaryMenu heading="User Legal Info" secondary vertical match={match} navItems={userLegalInfo} />
