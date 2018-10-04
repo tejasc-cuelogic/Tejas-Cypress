227674619c9c616be5a79c9ba22a9e7966bb348e
Chetan Bura <chetan.cuelogic@nextseed.com>
2018-10-04 12:38:47 +0530

    offering query added more fields

--- src/services/stores/queries/campagin.js
+++ src/services/stores/queries/campagin.js
@@ -39,6 +39,9 @@ export const campaignDetailsQuery = gql`
     id
     stage
     applicationId
+    issuerId
+    offeringUrl
+    referralCode
     selectedOffer {
       structure
     }
@@ -107,21 +110,192 @@ export const campaignDetailsQuery = gql`
     }
     leadership {
       firstName
+      lastName
+      companyPosition
+      uploads {
+        headshot {
+          fileId
+          fileName
+        }
+        heroImage {
+          fileId
+          fileName
+        }
+        license {
+          fileId
+          fileName
+        }
+      }
+      bio
       social {
         twitter
+        website
+        facebook
+        linkedin
       }
+      approved {
+        id
+        by
+        date
+      }
+    }
+    media {
+      heroImage {
+        id
+        url
+        isPublic
+      }
+      tombstoneImage {
+        id
+        url
+        isPublic
+      }
+      location  {
+        id
+        url
+        isPublic
+      }
+      gallery  {
+        id
+        url
+        isPublic
+      }
+      logo  {
+        id
+        url
+        isPublic
+      }
+    }
+    legal {
+      general {
+        websiteUrl
+        useOfProceeds {
+          reachedMinOfferingGoal
+          reachedMaxOfferingGoal
+        }
+        approved {
+          id
+          by
+          date
+        }
+      }
+    }
+    closureSummary {
+      totalInvestorCount
     }
     rewardsTierIds{
       amount
       earlyBirdQuantity
     }
+    comments {
+      id
+      offeringId
+      thread
+      scope
+      comment
+      approved {
+        id
+        by
+        date
+      }
+      updated {
+        id
+        by
+        date
+      }
+      deleted {
+        id
+        by
+        date
+      }
+      actingUserInfo {
+        id
+        userInfo {
+          firstName
+          lastName
+          avatar {
+            url
+            name
+          }
+        }
+      }
+    }
+    updated {
+      id
+      by
+      date
+    }
+    deleted {
+      id
+      by
+      date
+    }
+    updates {
+      id
+      offeringId
+      title
+      content
+      status
+      scope
+      tiers {
+        amount
+        earlyBirdQuantity
+      }
+      isEarlyBirdOnly
+      notificationSent {
+        id
+        by
+        date
+        to
+      }
+      approved {
+        id
+        by
+        date
+      }
+      updated {
+        id
+        by
+        date
+      }
+      deleted {
+        id
+        by
+        date
+      }
+      actingUserInfo {
+        id
+        userInfo {
+          firstName
+          lastName
+          avatar {
+            url
+            name
+          }
+        }
+      }
+    }
+    earlyBirdsCount
     bonusRewards{
       id
+      offeringId
       title
+      rewardStatus
       description
+      expirationDate
       tiers{
         amount
         earlyBirdQuantity
+      }
+      created {
+        id
+        by
+        date
+      }
+      updated {
+        id
+        by
+        date
       }
     }
   }
