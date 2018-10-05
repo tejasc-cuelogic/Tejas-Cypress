6ce832e193017a162846cc977a9e7a9baed4f66a
Chetan Bura <chetan.cuelogic@nextseed.com>
2018-10-05 22:05:19 +0530

    Video modal added

--- src/modules/public/offering/components/campaignDetails/Overview.js
+++ src/modules/public/offering/components/campaignDetails/Overview.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-// import Aux from 'react-aux';
 import { inject, observer } from 'mobx-react';
-import { Route } from 'react-router-dom';
-import { Embed, Grid, Segment } from 'semantic-ui-react';
-import videoPoster from '../../../../../assets/images/636206632.jpg';
+import { Route, Link } from 'react-router-dom';
+import { Grid, Segment, Image } from 'semantic-ui-react';
 import KeyTermsModal from './investmentDetails/KeyTermsModal';
 import AboutTheCompany from './Overview/AboutTheCompany';
 import BonusRewards from './Overview/BonusRewards';
 import KeyTerms from './Overview/KeyTerms';
 import LatestUpdates from './Overview/LatestUpdates';
 import SummaryModal from '../campaignDetails/investmentDetails/SummaryModal';
+import VideoModal from './Overview/VideoModal';
+import videoPoster from '../../../../../assets/images/636206632.jpg';
 
-const nsvideos = {
-  embed: '218642510',
-};
+
 const isTabletBoth = document.documentElement.clientWidth >= 768
 && document.documentElement.clientWidth < 1200;
 const isTabletLand = document.documentElement.clientWidth >= 992
@@ -31,12 +29,7 @@ class Overview extends Component {
             <AboutTheCompany refLink={this.props.refLink} campaign={campaign} />
             <Grid.Column widescreen={9} largeScreen={8} computer={16} tablet={16} className={isTabletLand && 'mt-30'}>
               <Segment padded>
-                <Embed
-                  id={nsvideos.embed}
-                  placeholder={videoPoster}
-                  source="vimeo"
-                  icon="ns-play"
-                />
+                <Image as={Link} to={`${this.props.match.url}/herovideo`} src={videoPoster} />
               </Segment>
             </Grid.Column>
           </Grid.Row>
@@ -54,6 +47,7 @@ class Overview extends Component {
             />
           </Grid.Row>
         </Grid>
+        <Route path={`${this.props.match.url}/herovideo`} render={props => <VideoModal refLink={props.match} {...props} />} />
         <Route path={`${this.props.match.url}/keyterms`} render={props => <KeyTermsModal refLink={props.match} {...props} />} />
         <Route path={`${this.props.match.url}/keyterms/summary`} component={SummaryModal} />
       </div>
