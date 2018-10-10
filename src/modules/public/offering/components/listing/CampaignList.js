dadf16518875e23630f7bf1120b7d114c258da71
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2018-10-10 18:42:57 +0530

    UI changes

--- src/modules/public/offering/components/listing/CampaignList.js
+++ src/modules/public/offering/components/listing/CampaignList.js
@@ -31,60 +31,58 @@ export default class CampaignList extends Component {
           <Container>
             {this.props.heading}
             <Grid doubling columns={3} stackable>
-              <Grid.Row>
-                {loading ? <Spinner loaderMessage="loading.." /> : OfferingList.map(offering => (
-                  <Grid.Column>
-                    <Card className="campaign" fluid as={Link} to={`/offerings/${offering.id}/overview`}>
-                      <Image
-                        centered
-                        src={offering.media.tombstoneImage.url}
-                        alt={`${offering.keyTerms.shorthandBusinessName} poster`}
+              {loading ? <Spinner loaderMessage="loading.." /> : OfferingList.map(offering => (
+                <Grid.Column>
+                  <Card className="campaign" fluid as={Link} to={`/offerings/${offering.id}/overview`}>
+                    <Image
+                      centered
+                      src={offering.media.tombstoneImage.url}
+                      alt={`${offering.keyTerms.shorthandBusinessName} poster`}
+                    />
+                    <Label color="green">NEW</Label> {/* apply attribute basic for successful campaigns */}
+                    <Icon name="heart" /> {/* change name to "heart outline" for unliked campaigns */}
+                    <Card.Content>
+                      <div className="tags mb-10">
+                        {offering && offering.keyTerms && capitalize(offering.keyTerms.industry.split('_').join(' '))}
+                        <span className="pull-right">506(c)</span>
+                      </div>
+                      <Card.Header>{offering.keyTerms.shorthandBusinessName}</Card.Header>
+                      <Card.Meta>
+                        {offering.businessGeneralInfo.address.city}
+                        {offering.businessGeneralInfo.address.state}
+                      </Card.Meta>
+                      <Card.Description
+                        dangerouslySetInnerHTML={{ __html: offering.offering.about.theCompany }}
                       />
-                      <Label color="green">NEW</Label> {/* apply attribute basic for successful campaigns */}
-                      <Icon name="heart" /> {/* change name to "heart outline" for unliked campaigns */}
-                      <Card.Content>
-                        <div className="tags mb-10">
-                          {offering && offering.keyTerms && capitalize(offering.keyTerms.industry.split('_').join(' '))}
-                          <span className="pull-right">506(c)</span>
+                      <p><b>{offering && offering.keyTerms && capitalize(offering.keyTerms.securities.split('_').join(' '))}</b></p>
+                      <List divided horizontal>
+                        <List.Item>Raised $1,000,000</List.Item>
+                        <List.Item>
+                          {(offering && offering.closureSummary &&
+                            offering.closureSummary.totalInvestorCount) || 0} investors
+                        </List.Item>
+                      </List>
+                    </Card.Content>
+                    {this.props.locked === offering.id && (
+                      <Card.Content className="card-hidden">
+                        <div className="lock-image">
+                          <Image src={lockIcon} />
                         </div>
-                        <Card.Header>{offering.keyTerms.shorthandBusinessName}</Card.Header>
-                        <Card.Meta>
-                          {offering.businessGeneralInfo.address.city}
-                          {offering.businessGeneralInfo.address.state}
-                        </Card.Meta>
-                        <Card.Description
-                          dangerouslySetInnerHTML={{ __html: offering.offering.about.theCompany }}
-                        />
-                        <p><b>{offering && offering.keyTerms && capitalize(offering.keyTerms.securities.split('_').join(' '))}</b></p>
-                        <List divided horizontal>
-                          <List.Item>Raised $1,000,000</List.Item>
-                          <List.Item>
-                            {(offering && offering.closureSummary &&
-                              offering.closureSummary.totalInvestorCount) || 0} investors
-                          </List.Item>
-                        </List>
+                        <div className="details">
+                          <div className="tags mb-10">
+                            hidden
+                          </div>
+                          <Card.Header>For NextSeed members only.</Card.Header>
+                          <Card.Meta>
+                            Login or complete your profile to view this offering.
+                          </Card.Meta>
+                        </div>
                       </Card.Content>
-                      {this.props.locked === offering.id && (
-                        <Card.Content className="card-hidden">
-                          <div className="lock-image">
-                            <Image src={lockIcon} />
-                          </div>
-                          <div className="details">
-                            <div className="tags mb-10">
-                              hidden
-                            </div>
-                            <Card.Header>For NextSeed members only.</Card.Header>
-                            <Card.Meta>
-                              Login or complete your profile to view this offering.
-                            </Card.Meta>
-                          </div>
-                        </Card.Content>
-                      )
-                      }
-                    </Card>
-                  </Grid.Column>
-                ))}
-              </Grid.Row>
+                    )
+                    }
+                  </Card>
+                </Grid.Column>
+              ))}
             </Grid>
           </Container>
           {this.state.filters && <div className="overlay" />}
