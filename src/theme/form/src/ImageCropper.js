4e8f465b20e1f13e45582a906273cc145227378b
pratik T <pratik.cuelogic@nextseed.com>
2018-10-11 13:41:48 +0530

    done changes for offering media error handling

--- src/theme/form/src/ImageCropper.js
+++ src/theme/form/src/ImageCropper.js
@@ -34,7 +34,7 @@ export default class ImageCropper extends Component {
     this.setState({ imageType: files[0].type });
     this.props.setData('value', files[0].name);
     this.props.setData('meta', { type: files[0].type });
-    this.props.verifySize(files[0].size);
+    this.props.verifySize(files[0].size, this.props.name);
     this.props.verifyExtension(files[0].type.split('/')[1]);
     const reader = new FileReader();
     reader.onload = () => {
@@ -44,7 +44,7 @@ export default class ImageCropper extends Component {
   }
 
   onImageLoaded = (image) => {
-    this.props.verifyImageDimension(image.width, image.height);
+    this.props.verifyImageDimension(image.width, image.height, this.props.name);
     const minimumWidth = 200;
     const cropWidthPer = (minimumWidth / image.width) * 100;
     this.setState({
