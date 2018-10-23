1646a08ac85c36283b7a44659f4509478ed3d860
Chetan Bura <chetan.cuelogic@nextseed.com>
2018-10-23 17:05:14 +0530

    Portfolio listing done

--- src/modules/private/investor/accountDetails/components/portfolio/PortfolioAllocations.js
+++ src/modules/private/investor/accountDetails/components/portfolio/PortfolioAllocations.js
@@ -3,19 +3,6 @@ import Aux from 'react-aux';
 import { Header, Grid } from 'semantic-ui-react';
 import ChartPie from './ChartPie';
 
-// const INVESTMENT_TYPE = [
-//   { name: 'Term Note', value: 7514 },
-//   { name: 'Rev Share', value: 1299 },
-// ];
-
-// const INDUSTRY = [
-//   { name: 'Food & Breverage', value: 3514 },
-//   { name: 'Fitness', value: 1580 },
-//   { name: 'Bar & Brewery', value: 7430 },
-//   { name: 'Hospitality', value: 1299 },
-//   { name: 'Other', value: 3200 },
-// ];
-
 const COLORS = ['#C782FF', '#28DAC9', '#0681A1', '#86D200', '#D2FF85', '#474747'];
 
 const PortfolioAllocations = ({ pieChart }) => (
