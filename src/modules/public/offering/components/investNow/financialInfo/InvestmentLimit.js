25ec6991ac37d0ecb064ce27604bf0ebb59446b7
sagar.cuelogic <sagar.cuelogic@nextseed.com>
2018-10-25 20:10:24 +0530

    updated UI for update investment amount

--- src/modules/public/offering/components/investNow/financialInfo/InvestmentLimit.js
+++ src/modules/public/offering/components/investNow/financialInfo/InvestmentLimit.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import Aux from 'react-aux';
 import { Link } from 'react-router-dom';
 import { Header, Popup, Icon } from 'semantic-ui-react';
 import Helper from '../../../../../../helper/utility';
 
 const InvestmentLimit = props => (
-  <Header as="h4" textAlign="center">
-    Your investment limit: {Helper.CurrencyFormat(props.getCurrentLimitForAccount || 0)}
-    <Popup
-      wide
-      trigger={<Icon className="ns-help-circle" color="green" />}
-      content="This calculates"
-      position="top center"
-    />
-    <Link to={props.match.url} className="link" onClick={() => props.setStepToBeRendered(4)}><small>Update</small></Link>
-  </Header>
+  <Aux>
+    <Header as="h4" textAlign="center">
+      Your investment limit: {Helper.CurrencyFormat(props.getCurrentLimitForAccount || 0)}
+      <Popup
+        wide
+        trigger={<Icon className="ns-help-circle" color="green" />}
+        content="This calculates"
+        position="top center"
+      />
+      <Link to={props.match.url} className="link" onClick={() => props.setStepToBeRendered(4)}><small>Update</small></Link>
+    </Header>
+    <p>Your investment will be decreased by - <span className="negative-text">$12,000</span></p>
+  </Aux>
 );
 
 export default InvestmentLimit;
