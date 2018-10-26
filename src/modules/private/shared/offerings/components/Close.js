9b3f5f9cdf0c2a0b32378baf95c63a146ea124b1
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2018-10-26 18:56:43 +0530

    admin offering close section UI completed

--- src/modules/private/shared/offerings/components/Close.js
+++ src/modules/private/shared/offerings/components/Close.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
-import { Form, Header } from 'semantic-ui-react';
+import { Form, Header, Divider, Step, Label, Button, Progress, Icon } from 'semantic-ui-react';
 import Contingency from './overview/Contingency';
 import Actions from './overview/Actions';
-import { MaskedInput } from '../../../../../theme/form';
+import { MaskedInput, FormCheckbox } from '../../../../../theme/form';
 
 @inject('offeringCreationStore')
 @observer
@@ -19,9 +19,123 @@ export default class Close extends Component {
       formChangeWithIndex,
     } = this.props.offeringCreationStore;
     const formName = 'OFFERING_CLOSE_FRM';
+    const checkboxesLeft = {
+      value: [],
+      values: [
+        {
+          label: '5/10 split agreed', value: '1',
+        },
+        {
+          label: 'Closing contigencies Sign-Off', value: '2',
+        },
+        {
+          label: 'Escrow funded Propetly', value: '3',
+        },
+      ],
+      error: undefined,
+      rule: 'array',
+    };
     return (
       <Form>
         <div className="inner-content-spacer">
+          <Header as="h4">
+            This campaign <span className="highlight-text">has succeed</span>
+          </Header>
+          <p>
+            Campaign has reached minimum required amount. MobCycle raised <b>$350,000</b>
+            {' '}from <b>227 investors</b> .
+          </p>
+          <Divider section />
+          <Step.Group className="campaign-close">
+            <Step active>
+              <Label circular color="blue">1</Label>
+              <Step.Content>
+                <Step.Title>Fund Escrow</Step.Title>
+              </Step.Content>
+            </Step>
+            <Step>
+              <Label circular color="grey">2</Label>
+              <Step.Content>
+                <Step.Title>Process Notes</Step.Title>
+              </Step.Content>
+            </Step>
+            <Step>
+              <Label circular color="grey">3</Label>
+              <Step.Content>
+                <Step.Title>Finalize envelopes</Step.Title>
+              </Step.Content>
+            </Step>
+            <Step>
+              <Label circular color="grey">4</Label>
+              <Step.Content>
+                <Step.Title>Finalize closure</Step.Title>
+              </Step.Content>
+            </Step>
+          </Step.Group>
+          {/* Step 1 */}
+          <Header as="h4" className="mt-40 mb-30">Start funding escrow</Header>
+          <Button primary>Fund Escrow</Button>
+          <Divider className="doubled" />
+          {/* Step 2 */}
+          <Header as="h4" className="mt-40 mb-30">Confirm</Header>
+          <FormCheckbox
+            fielddata={checkboxesLeft}
+            name="legalConfirmation"
+            changed
+            defaults
+            containerclassname="ui relaxed list"
+          />
+          <Button primary className="mt-20">Process Notes</Button>
+          <Divider className="doubled" />
+          {/* Step 3 */}
+          <Header as="h4" className="mt-40 mb-30">Finalize envelopes</Header>
+          <p>This will finalize all investments in DocuSign.</p>
+          <Button primary className="mt-20">Close Envelopes </Button>
+          <Divider className="doubled" />
+          {/* Step 4 */}
+          <Header as="h4" className="mt-40 mb-30">Envelopes processing
+            <Header.Subheader>
+              It may take a moment, please complete the form below while processing.
+            </Header.Subheader>
+          </Header>
+          <Progress percent={10} size="tiny" color="green" className="campaign-close-progress">
+            34 of 420 investors processed...
+          </Progress>
+          <Divider hidden clearing />
+          <Divider section clearing />
+          <Header as="h4" className="mt-40 mb-30">Finalize closure</Header>
+          <Form>
+            <Form.Group widths={3}>
+              <Form.Input
+                fluid
+                label="Disbursement Date"
+                placeholder="MM/DD/YYYY"
+              />
+              <Form.Input
+                fluid
+                label="Disbursement Amount"
+                placeholder="$ Enter amount"
+              />
+              <Form.Input
+                fluid
+                label="Total Repayment"
+                placeholder="$ Enter amount "
+              />
+            </Form.Group>
+          </Form>
+          <Button.Group className="mt-50">
+            <Button primary>Save draft</Button>
+            <Button color="red">Close offering </Button>
+          </Button.Group>
+          <Divider hidden fitted clearing />
+          <Button.Group>
+            <Button icon color="blue" className="link-button">
+              <Icon className="ns-envelope-line" />
+              Send Test Close Mail
+            </Button>
+            <Button as="span" className="time-stamp">You cannot close the offering if envelopes are still being processed</Button>
+          </Button.Group>
+          <Divider className="doubled" />
           <Contingency
             formChangeWithIndex={formChangeWithIndex}
             form={CLOSING_CONTITNGENCIES_FRM}
