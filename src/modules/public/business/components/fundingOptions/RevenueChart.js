80ccc6ad1b27b6620c095f42c37b8921cee330d6
Vaibhav Pathak <vaibhav.cuelogic@nextseed.com>
2018-11-26 22:03:41 +0530

    partially doen with rechar rerendering issue

--- src/modules/public/business/components/fundingOptions/RevenueChart.js
+++ src/modules/public/business/components/fundingOptions/RevenueChart.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
 import { LineChart, Line, XAxis, Legend, ResponsiveContainer } from 'recharts';
-
-const data = [
-  { name: '0 MOS.', 'Your Payments': 5000, 'Your Revenue': 5000 },
-  { name: '1 MOS.', 'Your Payments': 5000, 'Your Revenue': 3398 },
-  { name: '2 MOS.', 'Your Payments': 5000, 'Your Revenue': 9800 },
-  { name: '3 MOS.', 'Your Payments': 5000, 'Your Revenue': 5908 },
-  { name: '4 MOS.', 'Your Payments': 5000, 'Your Revenue': 6800 },
-  { name: '5 MOS.', 'Your Payments': 5000, 'Your Revenue': 5800 },
-  { name: '6 MOS.', 'Your Payments': 5000, 'Your Revenue': 6300 },
-  { name: '7 MOS.', 'Your Payments': 5000, 'Your Revenue': 5908 },
-  { name: '8 MOS.', 'Your Payments': 5000, 'Your Revenue': 3000 },
-  { name: '9 MOS.', 'Your Payments': 5000, 'Your Revenue': 4100 },
-  { name: '10 MOS.', 'Your Payments': 5000, 'Your Revenue': 5300 },
-  { name: '11 MOS.', 'Your Payments': 5000, 'Your Revenue': 4800 },
-  { name: '12 MOS.', 'Your Payments': 5000, 'Your Revenue': 6300 },
-];
-
+@inject('campaignStore')
+@observer
 export default class RevenueChart extends Component {
+  state = {
+    data: [
+      { name: '0 MOS.', 'Your Payments': 5000, 'Your Revenue': 5000 },
+      { name: '1 MOS.', 'Your Payments': 5000, 'Your Revenue': 3398 },
+      { name: '2 MOS.', 'Your Payments': 5000, 'Your Revenue': 9800 },
+      { name: '3 MOS.', 'Your Payments': 5000, 'Your Revenue': 5908 },
+      { name: '4 MOS.', 'Your Payments': 5000, 'Your Revenue': 6800 },
+      { name: '5 MOS.', 'Your Payments': 5000, 'Your Revenue': 5800 },
+      { name: '6 MOS.', 'Your Payments': 5000, 'Your Revenue': 6300 },
+      { name: '7 MOS.', 'Your Payments': 5000, 'Your Revenue': 5908 },
+      { name: '8 MOS.', 'Your Payments': 5000, 'Your Revenue': 3000 },
+      { name: '9 MOS.', 'Your Payments': 5000, 'Your Revenue': 4100 },
+      { name: '10 MOS.', 'Your Payments': 5000, 'Your Revenue': 5300 },
+      { name: '11 MOS.', 'Your Payments': 5000, 'Your Revenue': 4800 },
+      { name: '12 MOS.', 'Your Payments': 5000, 'Your Revenue': 6300 },
+    ],
+  };
+
+  componentDidMount(prevProps, prevState) {
+    console.log('nextProps==>', prevProps);
+    console.log('nextState==>', prevState);
+    this.props.campaignStore.setFieldValue('isRenderRechart', false);
+  }
   render() {
     const style = {
       top: 0,
@@ -25,7 +34,7 @@ export default class RevenueChart extends Component {
     };
     return (
       <ResponsiveContainer height={220}>
-        <LineChart margin={{ top: 50, left: 20 }} height={100} data={data}>
+        <LineChart margin={{ top: 50, left: 20 }} height={100} data={this.state.data}>
           <Legend layout="vertical" verticalAlign="top" align="right" wrapperStyle={style} />
           <XAxis tickLine={false} axisLine={false} dataKey="name" interval={11} />
           <Line type="monotone" dataKey="Your Revenue" dot="" stroke="#20C86D" strokeWidth={3} />
