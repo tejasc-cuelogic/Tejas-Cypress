d3fb2cadecc0c19e53fb5df5fcbd7990a5ec9a02
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2018-11-29 17:52:13 +0530

    font size updated

--- src/modules/public/offering/components/campaignDetails/Overview/AboutTheCompany.js
+++ src/modules/public/offering/components/campaignDetails/Overview/AboutTheCompany.js
@@ -34,8 +34,8 @@ class AboutTheCompany extends Component {
               <b>Type of Raise: </b>
               {campaign && campaign.keyTerms && campaign.keyTerms.securities ? CAMPAIGN_KEYTERMS_SECURITIES[campaign.keyTerms.securities] : ''}
               <Popup hoverable position="bottom center" trigger={<Icon name="help circle" color="green" />} content={(<span>For every $100 you invest, you are paid a portion of this company&apos;s gross revenue every month until you are paid $190 within 78 months. A 1.0% service fee is deducted from each payment. <a target="blank" href="https://www.nextseed.com/offerings/buffbrew-taproom/#returnsGraphAnchor">See some examples</a>.</span>)} />
+              <b>Industry: </b>
             </p>
-            <b>Industry: </b>
             {campaign && campaign.keyTerms && INDUSTRY_TYPES[campaign.keyTerms.industry]}<br />
             {campaign && campaign.offering && campaign.offering.overview &&
               campaign.offering.overview.highlight ?
