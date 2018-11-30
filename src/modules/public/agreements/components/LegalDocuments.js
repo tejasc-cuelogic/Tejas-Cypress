eed8fb5600b948f60c872173a27fc39f6b487b5d
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2018-11-30 16:00:29 +0530

    updated legal and disclaimer texts

--- src/modules/public/agreements/components/LegalDocuments.js
+++ src/modules/public/agreements/components/LegalDocuments.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import Aux from 'react-aux';
 import { Header, Grid, Segment, Button, Divider } from 'semantic-ui-react';
 
-const isSmallScreen = document.documentElement.clientWidth >= 1024
-&& document.documentElement.clientWidth < 1200;
+// const isSmallScreen = document.documentElement.clientWidth >= 1024
+// && document.documentElement.clientWidth < 1200;
 
 const legalDocs = [
-  { id: 1, docName: (<Aux>CrowdPay Custodial Account Agreement</Aux>), link: 'https://nextseed.box.com/s/6rtk4yx9r71z0iknwk6xdbhtoyxdvwlt' },
-  { id: 2, docName: (<Aux>Substitute IRS Form<br />W-9 Certification</Aux>), link: 'https://nextseed.box.com/s/6rtk4yx9r71z0iknwk6xdbhtoyxdvwlt' },
-  { id: 3, docName: (<Aux>NextSeed Membership Agreements</Aux>), link: 'https://nextseed.box.com/s/6rtk4yx9r71z0iknwk6xdbhtoyxdvwlt' },
-  { id: 4, docName: (<Aux>Lorem Ipsum Dolor Agrements</Aux>), link: 'https://nextseed.box.com/s/6rtk4yx9r71z0iknwk6xdbhtoyxdvwlt' },
+  { id: 1, docName: (<Aux>NextSeed Securities LLC Privacy Policy</Aux>), link: 'https://nextseed.box.com/s/6rtk4yx9r71z0iknwk6xdbhtoyxdvwlt' },
+  { id: 2, docName: (<Aux>NextSeed Securities LLC BCP Disclosure Statement</Aux>), link: 'https://nextseed.box.com/s/6rtk4yx9r71z0iknwk6xdbhtoyxdvwlt' },
+  { id: 3, docName: (<Aux>NextSeed Securities LLC CIP Notice</Aux>), link: 'https://nextseed.box.com/s/6rtk4yx9r71z0iknwk6xdbhtoyxdvwlt' },
+  { id: 4, docName: (<Aux>NextSeed Securities LLC Brokercheck Notice</Aux>), link: 'https://nextseed.box.com/s/6rtk4yx9r71z0iknwk6xdbhtoyxdvwlt' },
+  { id: 5, docName: (<Aux>NextSeed Securities LLC Investor Agreement</Aux>), link: 'https://nextseed.box.com/s/6rtk4yx9r71z0iknwk6xdbhtoyxdvwlt' },
+  { id: 6, docName: (<Aux>NextSeed US LLC Membership Agreement</Aux>), link: 'https://nextseed.box.com/s/6rtk4yx9r71z0iknwk6xdbhtoyxdvwlt' },
 ];
 export default class LegalDocuments extends Component {
   render() {
@@ -17,13 +19,13 @@ export default class LegalDocuments extends Component {
       <Aux>
         <Header as="h2">Legal Documents</Header>
         <Divider hidden />
-        <Grid columns={isSmallScreen ? 3 : 4} stackable doubling>
+        <Grid columns={3} stackable doubling>
           {
             legalDocs.map(l => (
               <Grid.Column key={l.id}>
                 <Segment padded textAlign="center" className="legal-documents">
                   <p>{l.docName}</p>
-                  <Button onClick={() => window.open(`${l.link}`, '_blank')} primary compact>View</Button>
+                  <Button className="relaxed" onClick={() => window.open(`${l.link}`, '_blank')} primary compact>View</Button>
                 </Segment>
               </Grid.Column>
             ))
