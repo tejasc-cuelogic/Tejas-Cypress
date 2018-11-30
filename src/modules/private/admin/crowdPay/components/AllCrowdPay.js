f5c8a03a524d7c9222af1b3acee49610e4a353c9
Chetan Bura <chetan.cuelogic@nextseed.com>
2018-11-30 13:41:49 +0000

    653 crowdpay admin listing

--- src/modules/private/admin/crowdPay/components/AllCrowdPay.js
+++ src/modules/private/admin/crowdPay/components/AllCrowdPay.js
@@ -1,22 +1,57 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
-import { kebabCase } from 'lodash';
+import Aux from 'react-aux';
+import { kebabCase, lowerCase } from 'lodash';
+import { withRouter, Route } from 'react-router-dom';
 import { Card, Table, Icon } from 'semantic-ui-react';
-import { DateTimeFormat, InlineLoader } from './../../../../../theme/shared';
+import { DateTimeFormat, InlineLoader, NsPagination } from './../../../../../theme/shared';
+import { NEXTSEED_BOX_URL } from '../../../../../constants/common';
 import Actions from './Actions';
+import MessageModal from '../components/MessageModal';
 
-@inject('crowdpayStore')
+const types = {
+  review: null,
+  cip: 'INDIVIDUAL',
+  ira: 'IRA',
+  entity: 'ENTITY',
+};
+const statusDetails = {
+  PARTIAL: 'Partial',
+  FULL: 'Full',
+  FROZEN: 'Frozen',
+  CIP_PROCESSING: 'Processing',
+  NS_PROCESSING: 'Processing',
+  GS_PROCESSING: 'Processing',
+  DECLINED: 'Failed',
+  DELETED: 'Deleted',
+};
+
+@inject('crowdpayStore', 'uiStore')
+@withRouter
 @observer
 export default class AllCrowdPay extends Component {
   componentWillMount() {
+    const type = this.props.history.location.pathname === '/app/crowdpay' ? 'review' : this.props.history.location.pathname.includes('cip') ? 'cip' : this.props.history.location.pathname.includes('ira') ? 'ira' : this.props.history.location.pathname.includes('review') ? 'review' : 'entity';
+    const accountType = types[type];
+    this.props.crowdpayStore.setAccountTypes(accountType, type);
+    this.props.crowdpayStore.reset();
     this.props.crowdpayStore.initRequest();
   }
+  paginate = params => this.props.crowdpayStore.pageRequest(params);
   render() {
-    const { crowdpayStore } = this.props;
-    const { accounts, loading } = crowdpayStore;
+    const { crowdpayStore, uiStore } = this.props;
+    const {
+      accounts, loading, count, requestState, crowdPayCtaHandler,
+    } = crowdpayStore;
+    const type = this.props.history.location.pathname === '/app/crowdpay' ? 'review' : this.props.history.location.pathname.includes('cip') ? 'cip' : this.props.history.location.pathname.includes('ira') ? 'ira' : this.props.history.location.pathname.includes('review') ? 'review' : 'entity';
     if (loading) {
       return <InlineLoader />;
     }
+    if (count === 0) {
+      return <InlineLoader text="No data found." />;
+    }
+    const { inProgress } = uiStore;
+    const totalRecords = count || 0;
     return (
       <Card fluid>
         <div className="table-wrapper">
@@ -24,10 +59,22 @@ export default class AllCrowdPay extends Component {
             <Table.Header>
               <Table.Row>
                 <Table.HeaderCell>Name</Table.HeaderCell>
+                {type === 'review' &&
+                  <Table.HeaderCell>Account Type</Table.HeaderCell>
+                }
                 <Table.HeaderCell>Creation date</Table.HeaderCell>
-                <Table.HeaderCell>Status</Table.HeaderCell>
+                {type !== 'review' &&
+                  <Table.HeaderCell>Status</Table.HeaderCell>
+                }
+                {(type !== 'review' && type === 'cip') &&
                 <Table.HeaderCell>Fail Reason</Table.HeaderCell>
+                }
+                {type === 'cip' &&
+                <Table.HeaderCell>GS Processing Date</Table.HeaderCell>
+                }
+                {type !== 'entity' &&
                 <Table.HeaderCell>Documents</Table.HeaderCell>
+                }
                 <Table.HeaderCell textAlign="center" />
               </Table.Row>
             </Table.Header>
@@ -37,24 +84,85 @@ export default class AllCrowdPay extends Component {
                   <Table.Row key={account.id}>
                     <Table.Cell>
                       <p>
-                        <b>{account.name}</b><br />
+                        <b>{account.firstName} {account.lastName}</b><br />
                         {account.email}<br />
-                        {account.phoneNumber}
+                        {account.phone}
                       </p>
                     </Table.Cell>
-                    <Table.Cell><DateTimeFormat fromNow datetime={account.createdAt} /></Table.Cell>
-                    <Table.Cell className={`status ${kebabCase(account.status)}`}>
-                      <Icon className="ns-warning-circle" />{account.status}
+                    {type === 'review' &&
+                    <Table.Cell>
+                      <Icon className={`ns-${lowerCase(account.accountType)}-line`} color="green" size="large" />
                     </Table.Cell>
-                    <Table.Cell>{account.failReason}</Table.Cell>
-                    <Table.Cell>{account.documents}</Table.Cell>
-                    <Actions />
+                    }
+                    <Table.Cell>
+                      {account.created && account.created.date ?
+                        <DateTimeFormat fromNow unix datetime={account.created.date} />
+                        :
+                        <p className="intro-text">N/A</p>
+                      }
+                    </Table.Cell>
+                    {type !== 'review' &&
+                    <Table.Cell className={`status ${kebabCase(account.accountStatus)}`}>
+                      <Icon className="ns-warning-circle" />{statusDetails[account.accountStatus]}
+                    </Table.Cell>
+                    }
+                    {(type !== 'review' && type === 'cip') &&
+                    <Table.Cell>
+                      {account.cip && account.cip.failReason && account.cip.failReason.message ? account.cip.failReason.message : <p className="intro-text">N/A</p>}
+                    </Table.Cell>
+                    }
+                    {type === 'cip' &&
+                    <Table.Cell>
+                      {account.processing && account.processing.gs && account.processing.gs.date ? account.processing.gs.date : <p className="intro-text">N/A</p>}
+                    </Table.Cell>
+                    }
+                    {type === 'ira' ?
+                      <Table.Cell>
+                        {account.legalDetails && account.legalDetails.verificationDocs
+                        && account.legalDetails.verificationDocs.addressProof &&
+                        account.legalDetails.verificationDocs.addressProof.fileHandle ?
+                          <a href={`${NEXTSEED_BOX_URL}folder/${account.legalDetails.verificationDocs.addressProof.fileHandle.boxFolderId}`} className="link" rel="noopener noreferrer" target="_blank" >
+                            View Documents
+                          </a>
+                        :
+                          <p className="intro-text">N/A</p>
+                        }
+                      </Table.Cell>
+                      : type !== 'entity' ?
+                        <Table.Cell>
+                          {account.legalDetails && account.legalDetails.verificationDocs
+                          && account.legalDetails.verificationDocs.addressProof &&
+                          account.legalDetails.verificationDocs.addressProof.fileHandle.boxFileId ?
+                            <Aux>
+                              <a href={`${NEXTSEED_BOX_URL}file/${account.legalDetails.verificationDocs.idProof.fileHandle.boxFileId}`} className="link filename-link" rel="noopener noreferrer" target="_blank" >
+                                {account.legalDetails.verificationDocs.idProof.fileName}
+                              </a>
+                              <a href={`${NEXTSEED_BOX_URL}file/${account.legalDetails.verificationDocs.addressProof.fileHandle.boxFileId}`} className="link filename-link" rel="noopener noreferrer" target="_blank" >
+                                {account.legalDetails.verificationDocs.addressProof.fileName}
+                              </a>
+                            </Aux>
+                          :
+                            <p className="intro-text">N/A</p>
+                          }
+                        </Table.Cell> : null
+                    }
+                    <Actions
+                      inProgress={inProgress}
+                      crowdPayCtaHandler={crowdPayCtaHandler}
+                      refLink={this.props.match.url}
+                      type={type}
+                      account={account}
+                    />
                   </Table.Row>
                 ))
               }
             </Table.Body>
           </Table>
         </div>
+        <Route exact path={`${this.props.match.url}/:action`} render={props => <MessageModal refLink={this.props.match.url} {...props} />} />
+        {totalRecords > 0 &&
+          <NsPagination floated="right" initRequest={this.paginate} meta={{ totalRecords, requestState }} />
+        }
       </Card>
     );
   }
