0ea075fdd49a11a1456298f866469e78d3b1940e
Mayura <mayura.cuelogic@nextseed.com>
2018-12-03 19:23:29 +0530

    done chnages for date of birth validation

--- src/services/constants/user.js
+++ src/services/constants/user.js
@@ -14,7 +14,7 @@ Validator.register(
   'dob', (value, attribute) => {
     return moment(value, 'MM/DD/YYYY').isBefore(moment());
   },
-  'The :attribute should be before Todays Date.',
+  "The :attribute should be less than today's date.",
 );
 
 Validator.register(
