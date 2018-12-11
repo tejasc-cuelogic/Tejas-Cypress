c4bf7859b9313a48abb1334c7645e9dd0195675b
sagar.cuelogic <sagar.cuelogic@nextseed.com>
2018-12-11 20:05:43 +0530

    updated data room UI

--- src/modules/private/shared/offerings/components/legal/DataRoom.js
+++ src/modules/private/shared/offerings/components/legal/DataRoom.js
@@ -1,68 +1,14 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { inject, observer } from 'mobx-react';
 import ReactDragList from 'react-drag-list';
-import { Form, Header, Button, Divider, Table, Icon, Confirm } from 'semantic-ui-react';
+import { Form, Header, Button, Divider, Icon, Confirm } from 'semantic-ui-react';
 import { FormInput, DropZoneConfirm as DropZone } from '../../../../../../theme/form';
 import ButtonGroupType2 from '../ButtonGroupType2';
-
-const dataArray = [
-  {
-    color: '#FF5500',
-    title: 'Senior Product Designer',
-    text: 'Senior Product Designer',
-    documentName: {
-      label: '',
-      value: '',
-      error: undefined,
-      rule: 'required|string',
-      placeHolder: 'Enter here',
-    },
-  },
-  {
-    color: '#5FC296',
-    title: 'Senior Animator',
-    text: 'Senior Animator',
-    documentName: {
-      label: '',
-      value: '',
-      error: undefined,
-      rule: 'required|string',
-      placeHolder: 'Enter here',
-    },
-  },
-  {
-    color: '#2DB7F5',
-    title: 'Visual Designer',
-    text: 'Visual Designer',
-    documentName: {
-      label: '',
-      value: '',
-      error: undefined,
-      rule: 'required|string',
-      placeHolder: 'Enter here',
-    },
-  },
-  {
-    color: '#FFAA00',
-    title: 'Computer Engineer',
-    text: 'Computer Engineer',
-    documentName: {
-      label: '',
-      value: '',
-      error: undefined,
-      rule: 'required|string',
-      placeHolder: 'Enter here',
-    },
-  },
-];
 
 @inject('offeringCreationStore', 'userStore', 'offeringsStore')
 @observer
 export default class DataRoom extends Component {
-  state = {
-    dataSource: dataArray,
-  };
   componentWillMount() {
     if (!this.props.offeringCreationStore.initLoad.includes('DATA_ROOM_FRM')) {
       this.props.offeringCreationStore.setFormData('DATA_ROOM_FRM', 'legal.dataroom');
@@ -119,8 +65,6 @@ export default class DataRoom extends Component {
       removeData,
     } = this.props.offeringCreationStore;
     const formName = 'DATA_ROOM_FRM';
-    console.log(DATA_ROOM_FRM.fields.documents);
-    console.log(offer);
     return (
       <div className={isIssuer || (isIssuer && !match.url.includes('offering-creation')) ? 'ui card fluid form-card' : ''}>
         <Form>
@@ -131,54 +75,60 @@ export default class DataRoom extends Component {
             </Button.Group>
           </Header>
           <Divider hidden />
-          <Table basic compact className="form-table">
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell>Document Name</Table.HeaderCell>
-                <Table.HeaderCell>Document</Table.HeaderCell>
-                <Table.HeaderCell>Actions</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {
-                DATA_ROOM_FRM.fields.documents.length &&
-                DATA_ROOM_FRM.fields.documents.map((document, index) => (
-                  <Table.Row>
-                    <Table.Cell>
-                      <FormInput
-                        displayMode={isReadonly}
-                        name="name"
-                        fielddata={document.name}
-                        size="small"
-                        changed={(e, result) => formArrayChange(e, result, formName, 'documents', index)}
-                        ishidelabel
-                      />
-                    </Table.Cell>
-                    <Table.Cell>
-                      <DropZone
-                        disabled={isReadonly}
-                        size="small"
-                        className="secondary"
-                        name="upload"
-                        fielddata={document.upload}
-                        uploadtitle="Upload"
-                        ondrop={(files, name) => this.onFileDrop(files, name, index)}
-                        onremove={fieldName => this.handleDelDoc(fieldName, index)}
-                      />
-                    </Table.Cell>
-                    <Table.Cell collapsing>
-                      <Button icon circular color={document.accreditedOnly.value ? 'red' : 'green'} className="link-button">
-                        <Icon className={document.accreditedOnly.value ? 'ns-lock' : 'ns-unlock'} onClick={() => this.handleLockUnlock(index)} />
-                      </Button>
-                      <Button icon circular className="link-button">
-                        <Icon className="ns-trash" onClick={e => this.toggleConfirmModal(e, index, formName)} />
-                      </Button>
-                    </Table.Cell>
-                  </Table.Row>
-                ))
-              }
-            </Table.Body>
-          </Table>
+          <div className="ui basic compact table form-table">
+            <div className="row-wrap thead">
+              <div className="balance-half">Document Name</div>
+              <div className="balance-half">Document</div>
+              <div className="date">Updated on</div>
+              <div className="status">is Public?</div>
+              <div className="action">Actions</div>
+            </div>
+            <ReactDragList
+              dataSource={DATA_ROOM_FRM.fields.documents.length && DATA_ROOM_FRM.fields.documents}
+              rowKey="title"
+              row={(document, index) => (
+                <div key={index} className="row-wrap">
+                  <div className="balance-half simple-drag-row-title">
+                    <FormInput
+                      displayMode={isReadonly}
+                      name="name"
+                      fielddata={document.name}
+                      size="small"
+                      changed={(e, result) => formArrayChange(e, result, formName, 'documents', index)}
+                      ishidelabel
+                    />
+                  </div>
+                  <div className="balance-half">
+                    <DropZone
+                      disabled={isReadonly}
+                      size="small"
+                      className="secondary"
+                      name="upload"
+                      fielddata={document.upload}
+                      uploadtitle="Upload"
+                      ondrop={(files, name) => this.onFileDrop(files, name, index)}
+                      onremove={fieldName => this.handleDelDoc(fieldName, index)}
+                    />
+                    {/* <Link to="/" className="link"><Icon className="ns-file" />
+                    disclosure-document.pdf</Link> */}
+                  </div>
+                  <div className="date">12/31/2016</div>
+                  <div className="status">Yes</div>
+                  <div className="action">
+                    <Button icon circular color={document.accreditedOnly.value ? 'red' : 'green'} className="link-button">
+                      <Icon className={document.accreditedOnly.value ? 'ns-lock' : 'ns-unlock'} onClick={() => this.handleLockUnlock(index)} />
+                    </Button>
+                    <Button icon circular className="link-button">
+                      <Icon className="ns-trash" onClick={e => this.toggleConfirmModal(e, index, formName)} />
+                    </Button>
+                  </div>
+                </div>
+              )}
+              handles={false}
+              onUpdate={this.handleUpdate}
+            />
+          </div>
+          <Divider hidden />
           <ButtonGroupType2
             submitted={submitted}
             isManager={isManager}
@@ -195,49 +145,6 @@ export default class DataRoom extends Component {
           size="mini"
           className="deletion"
         />
-        <Divider section />
-        <div className="ui basic compact table form-table">
-          <div className="row-wrap thead">
-            <div className="balance-half">Document Name</div>
-            <div className="balance-half">Document</div>
-            <div className="date">Updated on</div>
-            <div className="status">is Public?</div>
-            <div className="action">Actions</div>
-          </div>
-          <ReactDragList
-            dataSource={this.state.dataSource}
-            rowKey="title"
-            row={(record, index) => (
-              <div key={index} className="row-wrap">
-                <div className="balance-half simple-drag-row-title">
-                  <FormInput
-                    ishidelabel
-                    name="documentName"
-                    fielddata={record.documentName}
-                    size="small"
-                    changed={(e, result) => formArrayChange(e, result, formName, 'documents', index)}
-                  />
-                  {/* {record.title} */}
-                </div>
-                <div className="balance-half">
-                  <Link to="/" className="link"><Icon className="ns-file" />disclosure-document.pdf</Link>
-                </div>
-                <div className="date">88/88/8888</div>
-                <div className="status">Yes</div>
-                <div className="action">
-                  <Button icon circular color="red" className="link-button">
-                    <Icon name="ns-lock" />
-                  </Button>
-                  <Button icon circular className="link-button">
-                    <Icon name="ns-trash" />
-                  </Button>
-                </div>
-              </div>
-            )}
-            handles={false}
-            onUpdate={this.handleUpdate}
-          />
-        </div>
       </div>
     );
   }
