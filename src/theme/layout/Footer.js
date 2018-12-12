6393a31229bff1aea6f12a32de1befd282c5dbf4
pratik <pratik.cuelogic@nextseed.com>
2018-12-12 21:17:16 +0530

    merged

--- src/theme/layout/Footer.js
+++ src/theme/layout/Footer.js
@@ -42,7 +42,6 @@ class Footer extends Component {
           {(OfferFooter.find(item => matchPath(path, { path: item }))) && offeirngDisclaimer &&
             <p className="mb-40 copyright-info">
               <b>`{shorthandBusinessName} Disclaimer:`</b>
-              <span dangerouslySetInnerHTML={{ __html: offeirngDisclaimer }} />
             </p>
           }
           <Grid stackable>
