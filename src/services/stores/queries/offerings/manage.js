cd7bb46ce1c0f61e101433e0f42c9001070ad2b1
Mayura <mayura.cuelogic@nextseed.com>
2018-12-13 16:58:11 +0530

    done changes for keytems new field >> startupPeriod

--- src/services/stores/queries/offerings/manage.js
+++ src/services/stores/queries/offerings/manage.js
@@ -87,6 +87,7 @@ export const getOfferingDetails = gql`
       } 
       }
       keyTerms {
+        startupPeriod
         revSharePercentageDescription
         useOfProceedFootnote
         currentFinancialStatements
@@ -882,8 +883,8 @@ export const getOfferingDetails = gql`
 `;
 
 export const updateOffering = gql`
-mutation _updateOffering($id: String!, $poc: String, $offeringDetails: OfferingInputType!) {
-  updateOffering(id: $id, poc: $poc offeringDetails: $offeringDetails) {
+mutation _updateOffering($id: String!, $issuerId: String, $offeringDetails: OfferingInputType!) {
+  updateOffering(id: $id, issuerId: $issuerId offeringDetails: $offeringDetails) {
     id
   }
 }
