a39bfd7ab1e25edb96ccdbbf5e85126127d2d37e
Mayura <mayura.cuelogic@nextseed.com>
2018-12-19 17:26:14 +0530

    done changes for data room public - dic display conditionally

--- src/modules/public/offering/components/campaignDetails/DataRoom/Disclosure.js
+++ src/modules/public/offering/components/campaignDetails/DataRoom/Disclosure.js
@@ -17,12 +17,15 @@ class Disclosure extends Component {
     getBoxEmbedLink(doc.to, doc.url);
   }
   render() {
-    const { embedUrl, docLoading } = this.props.campaignStore;
+    const { embedUrl, docLoading, getNavItemsForDataRoom } = this.props.campaignStore;
     const { stepInRoute } = this.props.navStore;
-    if (!this.props.userStore.currentUser ||
+    const { docKey } = this.props.match.params;
+    const doc = docKey && docKey !== 'disclosure' ? getNavItemsForDataRoom.find(ele => ele.to === parseInt(docKey, 10)) :
+      getNavItemsForDataRoom[0];
+    if (doc.accreditedOnly && (!this.props.userStore.currentUser ||
       (this.props.userStore.currentUser && this.props.userStore.currentUser.roles &&
       this.props.userStore.currentUser.roles.includes('investor') &&
-      !this.props.accreditationStore.isUserAccreditated)) {
+      !this.props.accreditationStore.isUserAccreditated))) {
       return (
         <div className="updates-modal">
           <div className="no-updates">
