38a384a68248587ff5f8ea515a4fac28bbc5cd9a
Chetan Bura <chetan.cuelogic@nextseed.com>
2019-01-03 11:33:04 +0000

    759-bug-fixes-jan-first-week

--- src/modules/private/shared/offerings/components/comments/MessagesList.js
+++ src/modules/private/shared/offerings/components/comments/MessagesList.js
@@ -12,10 +12,15 @@ const MessagesList = props => (
           key={msg.id}
           className={props.currentMessageId === msg.id ? 'active' : ''}
         >
-          {props.threadUsersList(msg.threadComments).length === 0 && !props.isIssuer &&
+          {props.threadUsersList(msg.threadComments).length === 0 && !props.isIssuer ?
             <Item.Extra>
               <Label size="mini" color="red" horizontal circular>Response Needed</Label>
-            </Item.Extra>
+            </Item.Extra> : null
+          }
+          {props.threadMsgCount(msg.threadComments) ?
+            <Item.Extra>
+              <Label size="mini" color="blue" circular>{props.threadMsgCount(msg.threadComments)}</Label>
+            </Item.Extra> : null
           }
           <div className="ui image">
             {props.threadUsersList(msg.threadComments).length === 1 &&
@@ -29,8 +34,8 @@ const MessagesList = props => (
                 }}
               />
             }
-            {props.threadUsersList(msg.threadComments).length ?
-            props.threadUsersList(msg.threadComments).map(u => (
+            {props.threadUsersList(msg.threadComments).length > 1 ?
+            props.threadUsersList(msg.threadComments).map((u, i) => i < 3 && (
               <UserAvatar
                 size="mini"
                 UserInfo={{
@@ -53,7 +58,7 @@ const MessagesList = props => (
           }
           </div>
           <List.Content>
-            <List.Header as="h5">{props.threadUsersList(msg.threadComments).length ? (props.threadUsersList(msg.threadComments).map(u => (u.createdUserInfo.info && `${u.createdUserInfo.info.firstName}`))).join(', ') : msg.createdUserInfo.info && `${msg.createdUserInfo.info.firstName}`}</List.Header>
+            <List.Header as="h5">{props.threadUsersList(msg.threadComments).length ? `${(props.threadUsersList(msg.threadComments).map((u, i) => i < 3 && (`${get(u, 'createdUserInfo.info.firstName')}`))).join(', ')} ${props.threadUsersList(msg.threadComments).length > 2 ? '...' : ''}` : `${get(msg, 'createdUserInfo.info.firstName')}`}</List.Header>
           </List.Content>
           <List.Content>
             <List.Header><DateTimeFormat format="ll" datetime={msg.created.date} /></List.Header>
