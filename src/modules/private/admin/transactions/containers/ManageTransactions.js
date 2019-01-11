ccfaa0394be2a063759fc7b7f53494ba390fd060
Mayura <mayura.cuelogic@nextseed.com>
2019-01-11 14:27:50 +0530

    done changes for admin transactions ui layout

--- src/modules/private/admin/transactions/containers/ManageTransactions.js
+++ src/modules/private/admin/transactions/containers/ManageTransactions.js
@@ -1,42 +1,29 @@
 import React, { Component } from 'react';
 import { Grid } from 'semantic-ui-react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, withRouter, Switch } from 'react-router-dom';
 import PrivateLayout from '../../../shared/PrivateHOC';
-import SecondaryMenu from '../../../../../theme/layout/SecondaryMenu';
 import AllTransactions from '../components/AllTransactions';
 
 @withRouter
 export default class ManageTransactions extends Component {
+  componentWillMount() {
+    const { match, history } = this.props;
+    if (match.isExact) {
+      history.push(`${match.url}/status-1`);
+    }
+  }
   render() {
     const { match } = this.props;
-    const navItems = [
-      { title: 'Status 1', to: 'status-1', component: 'Status' },
-      { title: 'Status 2', to: 'status-2', component: 'Status' },
-      { title: 'Status 3', to: 'status-3', component: 'Status' },
-      { title: 'Status 4', to: 'status-4', component: 'Status' },
-    ];
     return (
       <PrivateLayout
         {...this.props}
         subNav
       >
         <Grid>
-          <Grid.Column widescreen={4} computer={3} tablet={3} mobile={16}>
-            <div className="sticy-sidebar">
-              <SecondaryMenu secondary match={match} navItems={navItems} />
-            </div>
-          </Grid.Column>
           <Grid.Column widescreen={12} computer={13} tablet={13} mobile={16}>
-            <div className="ui card fluid form-card">
-              <Switch>
-                <Route exact path={match.url} component={AllTransactions} />
-                {
-                  navItems.map(item => (
-                    <Route exact={false} key={item.to} path={`${match.url}/${item.to}`} component={AllTransactions} />
-                  ))
-                }
-              </Switch>
-            </div>
+            <Switch>
+              <Route exact path={`${match.url}/:statusType`} component={AllTransactions} />
+            </Switch>
           </Grid.Column>
         </Grid>
       </PrivateLayout>
--- src/modules/private/admin/transactions/index.js
+++ src/modules/private/admin/transactions/index.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import ManageTransactions from './containers/ManageTransactions';
-import TransactionDetails from './containers/TransactionDetails';
+// import TransactionDetails from './containers/TransactionDetails';
 
 export default class Transactions extends Component {
   render() {
     const { match } = this.props;
     return (
       <Switch>
-        <Route exact path={`${match.url}`} component={ManageTransactions} />
-        <Route exact path={`${match.url}/:id`} component={TransactionDetails} />
+        <Route exact={false} path={`${match.url}`} component={ManageTransactions} />
+        {/* <Route exact path={`${match.url}/:id`} component={TransactionDetails} /> */}
       </Switch>
     );
   }
