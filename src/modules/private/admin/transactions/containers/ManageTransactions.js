705908819b39f78344295549993102618f7dc648
Mayura <mayura.cuelogic@nextseed.com>
2019-01-11 12:39:28 +0530

    done changes for transactions listing

--- src/modules/private/admin/transactions/containers/ManageTransactions.js
+++ src/modules/private/admin/transactions/containers/ManageTransactions.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import PrivateLayout from '../../../shared/PrivateHOC';
-import { ByKeyword as Search } from '../../../../../theme/form/Filters';
+import SecondaryMenu from '../../../../../theme/layout/SecondaryMenu';
 import AllTransactions from '../components/AllTransactions';
 
+@withRouter
 export default class ManageTransactions extends Component {
-  search = (e) => {
-    if (e.charCode === 13 && false) {
-      // search goes here..
-    }
-  }
   render() {
     const { match } = this.props;
+    const navItems = [
+      { title: 'Status 1', to: 'status-1', component: 'Status' },
+      { title: 'Status 2', to: 'status-2', component: 'Status' },
+      { title: 'Status 3', to: 'status-3', component: 'Status' },
+      { title: 'Status 4', to: 'status-4', component: 'Status' },
+    ];
     return (
       <PrivateLayout
         {...this.props}
-        P1={<Search {...this.props} w={[9]} placeholder="Type keyword" executeSearch={this.search} />}
+        subNav
       >
-        <AllTransactions match={match} />
+        <Grid>
+          <Grid.Column widescreen={4} computer={3} tablet={3} mobile={16}>
+            <div className="sticy-sidebar">
+              <SecondaryMenu secondary match={match} navItems={navItems} />
+            </div>
+          </Grid.Column>
+          <Grid.Column widescreen={12} computer={13} tablet={13} mobile={16}>
+            <div className="ui card fluid form-card">
+              <Switch>
+                <Route exact path={match.url} component={AllTransactions} />
+                {
+                  navItems.map(item => (
+                    <Route exact={false} key={item.to} path={`${match.url}/${item.to}`} component={AllTransactions} />
+                  ))
+                }
+              </Switch>
+            </div>
+          </Grid.Column>
+        </Grid>
       </PrivateLayout>
     );
   }
