6b241dced42a688dd3d16fedadca561d5bd598b6
Mayura <mayura.cuelogic@nextseed.com>
2019-01-11 17:44:14 +0530

    done cha gs for filter- store

--- src/services/stores/entities/admin/transactionsStore.js
+++ src/services/stores/entities/admin/transactionsStore.js
@@ -1,4 +1,6 @@
 import { observable, action } from 'mobx';
+import { isArray } from 'lodash';
+import moment from 'moment';
 
 export class TransactionsStore {
   @observable filters = false;
@@ -19,6 +21,39 @@ export class TransactionsStore {
   @action
   toggleSearch = () => {
     this.filters = !this.filters;
+  }
+
+  @action
+  initiateSearch = (srchParams) => {
+    this.requestState.search = srchParams;
+  }
+
+  @action
+  setInitiateSrch = (name, value) => {
+    if (name === 'startDate' || name === 'endDate') {
+      this.requestState.search[name] = value;
+      if (this.requestState.search.startDate !== '' && this.requestState.search.endDate !== '') {
+        const srchParams = { ...this.requestState.search };
+        this.initiateSearch(srchParams);
+      }
+    } else {
+      const srchParams = { ...this.requestState.search };
+      if ((isArray(value) && value.length > 0) || (typeof value === 'string' && value !== '')) {
+        srchParams[name] = value;
+      } else {
+        delete srchParams[name];
+      }
+      this.initiateSearch(srchParams);
+    }
+  }
+
+  @action
+  maskChange = (values, field) => {
+    if (moment(values.formattedValue, 'MM-DD-YYYY', true).isValid()) {
+      const isoDate = field === 'startDate' ? moment(values.formattedValue).toISOString() :
+        moment(values.formattedValue).add(1, 'day').toISOString();
+      this.setInitiateSrch(field, isoDate);
+    }
   }
 }
 
