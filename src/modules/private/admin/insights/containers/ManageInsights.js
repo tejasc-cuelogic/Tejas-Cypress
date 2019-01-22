a5306bf1502ed6c85331f31fbf11aa315f471837
Mayura Thorat <mayura.cuelogic@nextseed.com>
2019-01-22 07:23:34 +0000

    done changes for multistep - individual and establish profile errors

--- src/modules/private/admin/insights/containers/ManageInsights.js
+++ src/modules/private/admin/insights/containers/ManageInsights.js
@@ -1,20 +1,79 @@
 import React, { Component } from 'react';
-import PrivateLayout from '../../../shared/PrivateHOC';
-import { ByKeyword as Search } from '../../../../../theme/form/Filters';
+import { inject, observer } from 'mobx-react';
+import { Grid, Button, Form } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import PrivateLayout from '../../../shared/PrivateLayout';
+import { CATEGORY_VALUES, TAGS, ARTICLE_STATUS_VALUES, AUTHORS } from '../../../../../services/constants/admin/article';
+import { ByKeyword as Search, DropdownFilter, DateRangeFilter } from '../../../../../theme/form/Filters';
 import AllInsights from '../components/AllInsights';
 
+@inject('articleStore')
+@observer
 export default class ManageInsights extends Component {
+  componentWillMount() {
+    this.props.articleStore.getCategoryList();
+  }
+  setSearchParam = (e, { name, value }) =>
+    this.props.articleStore.setInitiateSrch(name, value);
   search = (e) => {
     if (e.charCode === 13 && false) {
       // search goes here..
     }
   }
+  toggleSearch = () => this.props.articleStore.toggleSearch();
   render() {
     const { match } = this.props;
+    const {
+      filters,
+      requestState,
+      maskChange,
+    } = this.props.articleStore;
     return (
       <PrivateLayout
         {...this.props}
-        P1={<Search {...this.props} w={[9]} placeholder="Type keyword" executeSearch={this.search} />}
+        P1={
+          <Search
+            {...this.props}
+            w={[10]}
+            placeholder="Search by keyword or phrase"
+            executeSearch={this.search}
+            addon={
+              <Grid.Column width={3} textAlign="right">
+                <Button color="green" as={Link} floated="right" to={`${match.url}/new`}>
+                Add new article
+                </Button>
+              </Grid.Column>
+            }
+            change={this.setSearchParam}
+            toggleSearch={this.toggleSearch}
+            filters={filters}
+          />
+        }
+        P2={
+          <div className={`more search-filters ${filters ? '' : 'collapsed'}`}>
+            <Form>
+              <Grid stackable columns="equal">
+                <Grid.Row>
+                  <Grid.Column>
+                    <DropdownFilter value={requestState.search.categoryName} change={this.setSearchParam} name="Category" keyName="categoryName" options={CATEGORY_VALUES} />
+                  </Grid.Column>
+                  <Grid.Column>
+                    <DropdownFilter value={requestState.search.tags} change={this.setSearchParam} name="Tags" keyName="tags" options={TAGS} isMultiple />
+                  </Grid.Column>
+                  <Grid.Column>
+                    <DropdownFilter value={requestState.search.status} change={this.setSearchParam} name="Status" keyName="status" options={ARTICLE_STATUS_VALUES} />
+                  </Grid.Column>
+                  <Grid.Column>
+                    <DropdownFilter value={requestState.search.author} change={this.setSearchParam} name="Author" keyName="author" options={AUTHORS} />
+                  </Grid.Column>
+                  <Grid.Column width={4}>
+                    <DateRangeFilter change={maskChange} label="Date Range" name="dateRange" />
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Form>
+          </div>
+        }
       >
         <AllInsights match={match} />
       </PrivateLayout>
--- src/services/stores/entities/admin/articleStore.js
+++ src/services/stores/entities/admin/articleStore.js
@@ -1,30 +1,99 @@
 import { observable, action, computed, toJS } from 'mobx';
+import moment from 'moment';
 import graphql from 'mobx-apollo';
 import map from 'lodash/map';
+import isArray from 'lodash/isArray';
 import mapKeys from 'lodash/mapKeys';
 import mapValues from 'lodash/mapValues';
-import { GqlClient as client } from '../../../../api/publicApi';
-import { allInsightArticles, getArticleDetails, getArticlesByCatId } from '../../queries/insightArticle';
+import { FormValidator as Validator } from '../../../../helper';
+import { GqlClient as client } from '../../../../api/gqlApi';
+import { GqlClient as clientPublic } from '../../../../api/publicApi';
+import { ARTICLES } from '../../../constants/admin/article';
+import { allInsightArticles, getArticleDetails, getArticlesByCatId, getArticleById, createArticle, updateArticle } from '../../queries/insightArticle';
 import { getCategoryList } from '../../queries/categoryArticle';
 
 export class ArticleStore {
     @observable data = [];
     @observable Categories = [];
     @observable article = null;
+    @observable ARTICLE_FRM = Validator.prepareFormObject(ARTICLES);
     @observable featuredData = [];
     @observable featuredCategoryId = 'a25924d6-8136-4514-aee7-1ad8d78bb609';
+    @observable filters = false;
+    @observable requestState = {
+      search: {},
+    };
+    @observable globalAction = '';
 
     @action
-    requestAllArticles = () => {
-      this.data = graphql({ client, query: allInsightArticles });
+    setGlobalAction = (name, globalAction) => {
+      this[name] = globalAction;
+    }
+
+    @action
+    toggleSearch = () => {
+      this.filters = !this.filters;
+    }
+
+    @action
+    initiateSearch = (srchParams) => {
+      this.requestState.search = srchParams;
+    }
+
+    @action
+    setInitiateSrch = (name, value) => {
+      const srchParams = { ...this.requestState.search };
+      if ((isArray(value) && value.length > 0) || (typeof value === 'string' && value !== '')) {
+        srchParams[name] = value;
+      } else {
+        delete srchParams[name];
+      }
+      this.requestState.page = 1;
+      this.initiateSearch(srchParams);
+    }
+
+    @action
+    requestAllArticles = (isPublic = true) => {
+      const apiClient = isPublic ? clientPublic : client;
+      this.data = graphql({ client: apiClient, query: allInsightArticles });
     }
 
     @action
     requestArticlesByCategoryId = (id) => {
-      this.data = graphql({ client, query: getArticlesByCatId, variables: { id } });
+      this.data = graphql({ client: clientPublic, query: getArticlesByCatId, variables: { id } });
     }
 
     @action
+    getArticle = (id, isPublic = true) => {
+      const query = isPublic ? getArticleDetails : getArticleById;
+      const apiClient = isPublic ? clientPublic : client;
+      this.article = graphql({
+        client: apiClient,
+        query,
+        variables: { id },
+        onFetch: (res) => {
+          if (!isPublic) {
+            Object.keys(this.ARTICLE_FRM.fields).map((key) => {
+              this.ARTICLE_FRM.fields[key].value = res.insightsArticle[key];
+              return null;
+            });
+            Validator.validateForm(this.ARTICLE_FRM);
+          }
+        },
+
+      });
+    }
+
+    @action
+    save = (id) => {
+      const data = Validator.ExtractValues(this.ARTICLE_FRM.fields);
+      client
+        .mutate({
+          mutation: id === 'new' ? createArticle : updateArticle,
+          variables: id === 'new' ? { payload: data } :
+            { ...{ payload: data }, id },
+        });
+    }
     featuredRequestArticlesByCategoryId = () => {
       const id = this.featuredCategoryId;
       this.featuredData =
@@ -55,7 +124,12 @@ export class ArticleStore {
 
     @action
     getCategoryList = () => {
-      this.Categories = graphql({ client, query: getCategoryList, variables: { categoryType: 'INSIGHTS' } });
+      this.Categories = graphql({
+        client: clientPublic,
+        query: getCategoryList,
+        variables: { categoryType: 'INSIGHTS' },
+        fetchPolicy: 'network-only',
+      });
     }
 
     @computed get InsightCategories() {
@@ -68,6 +142,41 @@ export class ArticleStore {
 
     @computed get loading() {
       return this.data.loading;
+    }
+    @action
+    reset = () => {
+      this.ARTICLE_FRM = Validator.prepareFormObject(ARTICLES);
+    }
+    @action
+    htmlContentChange = (field, value) => {
+      this.ARTICLE_FRM.fields[field].value = value;
+      Validator.validateForm(this.ARTICLE_FRM);
+    }
+    @action
+    articleChange = (e, result) => {
+      this.ARTICLE_FRM = Validator.onChange(this.ARTICLE_FRM, Validator.pullValues(e, result));
+    };
+    @action
+    maskChange = (values, field) => {
+      if (moment(values.formattedValue, 'MM-DD-YYYY', true).isValid()) {
+        const isoDate = field === 'startDate' ? moment(values.formattedValue).toISOString() :
+          moment(values.formattedValue).add(1, 'day').toISOString();
+        this.setInitiateSrch(field, isoDate);
+      } else {
+        this.setInitiateSrch(field, values.value);
+      }
+    }
+    @computed get categoriesDropdown() {
+      console.log(this.Categories);
+      const categoriesArray = [];
+      if (this.Categories.data && this.Categories.data.categories) {
+        this.Categories.data.categories.map((ele) => {
+          categoriesArray.push({ key: ele.categoryName, value: ele.id, text: ele.categoryName });
+          return categoriesArray;
+        });
+        return categoriesArray;
+      }
+      return null;
     }
 }
 
