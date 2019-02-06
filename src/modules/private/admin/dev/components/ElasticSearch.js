fdbd61c7f57b47fb5a78b624c4b0f4bd9c76c718
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2019-02-06 15:17:40 +0530

    added grid structure and components

--- src/modules/private/admin/dev/components/ElasticSearch.js
+++ src/modules/private/admin/dev/components/ElasticSearch.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { Grid, Card, Button, Divider } from 'semantic-ui-react';
 import { inject, observer } from 'mobx-react';
 import { withRouter } from 'react-router-dom';
+import ActivityHistory from '../../../shared/ActivityHistory';
+import SecondaryMenu from '../../../../../theme/layout/SecondaryMenu';
 
 @inject('accreditationStore')
 @withRouter
@@ -12,8 +15,39 @@ export default class ElasticSearch extends Component {
     // }
   }
   render() {
+    const { match } = this.props;
+    const navItems = [
+      { title: 'Activity History', to: 'activity-history' },
+    ];
     return (
-      <div>test</div>
+      <Grid>
+        <Grid.Column width={4}>
+          <Card fluid>
+            <Card.Content header="Users Index" />
+            <Card.Content>
+              <Card.Description>
+                <Button.Group compact>
+                  <Button size="mini" content="Create" primary />
+                  <Button size="mini" content="Generate" secondary />
+                </Button.Group>
+                <Divider hidden />
+                <Button.Group compact>
+                  <Button size="mini" content="Update" primary />
+                  <Button size="mini" content="Delete" color="red" />
+                </Button.Group>
+              </Card.Description>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+        <Grid.Column width={12}>
+          <div className="sticky-sidebar">
+            <Card fluid>
+              <SecondaryMenu match={match} navItems={navItems} />
+              <ActivityHistory />
+            </Card>
+          </div>
+        </Grid.Column>
+      </Grid>
     );
   }
 }
