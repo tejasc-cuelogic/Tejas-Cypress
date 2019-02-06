8175aefa402cf67af323850207572b9d30192d5e
Chetan Bura <chetan.cuelogic@nextseed.com>
2019-02-06 15:11:29 +0530

    added mutations for ES indices CTA

--- src/services/stores/entities/dev/elasticSearchStore.js
+++ src/services/stores/entities/dev/elasticSearchStore.js
@@ -1,13 +1,35 @@
-import { observable, action, computed, toJS } from 'mobx';
+import { observable, action } from 'mobx';
+import * as elasticSearchQueries from '../../queries/elasticSearch';
+import { GqlClient as client } from '../../../../api/gqlApi';
+import Helper from '../../../../helper/utility';
 
 export class ElasticSearchStore {
-  @observable opt = {};
-  @computed get isIssuer() {
-    const roles = (this.currentUser && toJS(this.currentUser.roles)) || [];
-    return roles.includes('issuer');
+  @observable inProgress = {};
+
+  @action
+  setFieldValue = (field, value) => {
+    this[field] = value;
   }
-  @action setCurrentUser(user) {
-    this.currentUser = user;
+
+  @action
+  updateApplicationStatus = (mutation) => {
+    this.setFieldValue('inProgress', mutation);
+    return new Promise((resolve, reject) => {
+      client
+        .mutate({
+          mutation: elasticSearchQueries[mutation],
+        })
+        .then((result) => {
+          Helper.toast('Application status updated successfully.', 'success');
+          resolve(result);
+          this.setFieldValue('inProgress', false);
+        })
+        .catch((error) => {
+          Helper.toast('Something went wrong, please try again later.', 'error');
+          reject(error);
+          this.setFieldValue('inProgress', false);
+        });
+    });
   }
 }
 
--- src/services/stores/queries/elasticSearch.js
+++ src/services/stores/queries/elasticSearch.js
@@ -1,9 +1,61 @@
 import gql from 'graphql-tag';
 
-export const createUploadEntry = gql`
-  mutation createUploadEntry($applicationId:String, $applicationIssuerId:String, $stepName: stepsEnum!, $userRole: UserRoleEnum!, $fileData: UploadFileMetaInput!, $offeringId: String, $tags: [String]) {
-    createUploadEntry(applicationId: $applicationId, applicationIssuerId: $applicationIssuerId, stepName: $stepName, userRole: $userRole, fileData: $fileData, offeringId: $offeringId, tags: $tags) {
-      preSignedUrl
-      fileId
-    }
+export const userCreateIndices = gql`
+  mutation userCreateIndices() {
+    userCreateIndices()
+  }`;
+
+export const userDeleteIndices = gql`
+  mutation userDeleteIndices() {
+    userDeleteIndices()
+  }`;
+
+export const userPopulateIndex = gql`
+  mutation userPopulateIndex() {
+    userPopulateIndex()
+  }`;
+
+export const crowdPayCreateIndices = gql`
+  mutation crowdPayCreateIndices() {
+    crowdPayCreateIndices()
+  }`;
+
+export const crowdPayDeleteIndices = gql`
+  mutation crowdPayDeleteIndices() {
+    crowdPayDeleteIndices()
+  }`;
+
+export const crowdPayPopulateIndex = gql`
+  mutation crowdPayPopulateIndex() {
+    crowdPayPopulateIndex()
+  }`;
+
+export const linkedBankCreateIndices = gql`
+  mutation linkedBankCreateIndices() {
+    linkedBankCreateIndices()
+  }`;
+
+export const linkedBankDeleteIndices = gql`
+  mutation linkedBankDeleteIndices() {
+    linkedBankDeleteIndices()
+  }`;
+
+export const linkedBankPopulateIndex = gql`
+  mutation linkedBankPopulateIndex() {
+    linkedBankPopulateIndex()
+  }`;
+
+export const accreditationCreateIndices = gql`
+  mutation accreditationCreateIndices() {
+    accreditationCreateIndices()
+  }`;
+
+export const accreditationDeleteIndices = gql`
+  mutation accreditationDeleteIndices() {
+    accreditationDeleteIndices()
+  }`;
+
+export const accreditationPopulateIndex = gql`
+  mutation accreditationPopulateIndex() {
+    accreditationPopulateIndex()
   }`;
