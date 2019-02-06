3c018094f2f2b1ab5d1e30a207742215de353a2b
Rutuja Bhat <rutuja.cuelogic@nextseed.com>
2019-02-06 06:58:55 +0000

    Key Terms issue

--- src/theme/shared/src/Image64.js
+++ src/theme/shared/src/Image64.js
@@ -25,6 +25,23 @@ class Image64 extends React.Component {
       this.setState({ data: emptyImage });
     }
   }
+  componentWillReceiveProps() {
+    const emptyImage = this.props.imgType && this.props.imgType === 'heroImage' ? emptyImage2 : emptyImage1;
+    if (this.props.srcUrl) {
+      this.props.imageStore.getBase64Content(this.props.srcUrl).then((res) => {
+        if (res.includes('data:')) {
+          this.setState({ data: res || emptyImage });
+        } else {
+          this.setState({ data: this.props.srcUrl || emptyImage });
+        }
+      }).catch((err) => {
+        console.log(err);
+        this.setState({ data: emptyImage });
+      });
+    } else {
+      this.setState({ data: emptyImage });
+    }
+  }
   render() {
     return this.props.bg ? (
       <div {...this.props} style={{ backgroundImage: `url(${this.state.data})` }} />
