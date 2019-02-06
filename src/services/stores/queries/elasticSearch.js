271ba37197759a187f7a8df8f0f89253e2e4f23b
Chetan Bura <chetan.cuelogic@nextseed.com>
2019-02-06 15:19:40 +0530

    store added for elastic search mgt

--- src/services/stores/queries/elasticSearch.js
+++ src/services/stores/queries/elasticSearch.js
@@ -1,61 +1,61 @@
 import gql from 'graphql-tag';
 
 export const userCreateIndices = gql`
-  mutation userCreateIndices() {
-    userCreateIndices()
+  mutation userCreateIndices {
+    userCreateIndices
   }`;
 
 export const userDeleteIndices = gql`
-  mutation userDeleteIndices() {
-    userDeleteIndices()
+  mutation userDeleteIndices {
+    userDeleteIndices
   }`;
 
 export const userPopulateIndex = gql`
-  mutation userPopulateIndex() {
-    userPopulateIndex()
+  mutation userPopulateIndex {
+    userPopulateIndex
   }`;
 
 export const crowdPayCreateIndices = gql`
-  mutation crowdPayCreateIndices() {
-    crowdPayCreateIndices()
+  mutation crowdPayCreateIndices {
+    crowdPayCreateIndices
   }`;
 
 export const crowdPayDeleteIndices = gql`
-  mutation crowdPayDeleteIndices() {
-    crowdPayDeleteIndices()
+  mutation crowdPayDeleteIndices {
+    crowdPayDeleteIndices
   }`;
 
 export const crowdPayPopulateIndex = gql`
-  mutation crowdPayPopulateIndex() {
-    crowdPayPopulateIndex()
+  mutation crowdPayPopulateIndex {
+    crowdPayPopulateIndex
   }`;
 
 export const linkedBankCreateIndices = gql`
-  mutation linkedBankCreateIndices() {
-    linkedBankCreateIndices()
+  mutation linkedBankCreateIndices {
+    linkedBankCreateIndices
   }`;
 
 export const linkedBankDeleteIndices = gql`
-  mutation linkedBankDeleteIndices() {
-    linkedBankDeleteIndices()
+  mutation linkedBankDeleteIndices {
+    linkedBankDeleteIndices
   }`;
 
 export const linkedBankPopulateIndex = gql`
-  mutation linkedBankPopulateIndex() {
-    linkedBankPopulateIndex()
+  mutation linkedBankPopulateIndex {
+    linkedBankPopulateIndex
   }`;
 
 export const accreditationCreateIndices = gql`
-  mutation accreditationCreateIndices() {
-    accreditationCreateIndices()
+  mutation accreditationCreateIndices {
+    accreditationCreateIndices
   }`;
 
 export const accreditationDeleteIndices = gql`
-  mutation accreditationDeleteIndices() {
-    accreditationDeleteIndices()
+  mutation accreditationDeleteIndices {
+    accreditationDeleteIndices
   }`;
 
 export const accreditationPopulateIndex = gql`
-  mutation accreditationPopulateIndex() {
-    accreditationPopulateIndex()
+  mutation accreditationPopulateIndex {
+    accreditationPopulateIndex
   }`;
