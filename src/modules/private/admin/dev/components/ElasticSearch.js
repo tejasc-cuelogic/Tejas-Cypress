0dc1ea116f8d75bf328f74c4c668b1832e00f45e
Chetan Bura <chetan.cuelogic@nextseed.com>
2019-02-06 15:38:13 +0530

    card added dynamically for all cta

--- src/modules/private/admin/dev/components/ElasticSearch.js
+++ src/modules/private/admin/dev/components/ElasticSearch.js
@@ -1,62 +1,57 @@
 import React, { Component } from 'react';
-import { Grid, Card, Button, Divider } from 'semantic-ui-react';
+import { Grid, Card, Button } from 'semantic-ui-react';
 import { inject, observer } from 'mobx-react';
 import { withRouter } from 'react-router-dom';
 import ActivityHistory from '../../../shared/ActivityHistory';
 import SecondaryMenu from '../../../../../theme/layout/SecondaryMenu';
 
-// const elasticSearchModules = [
-//   {
-//     module: 'users',
-//     title: 'Users Index',
-//     cta: [
-//       { title: 'Create', color: 'green', mutation: 'userCreateIndices' },
-//       { title: 'Generate', color: 'blue', mutation: 'userPopulateIndex' },
-//       { title: 'Update', color: 'blue', mutation: 'userPopulateIndex' },
-//       { title: 'Delete', color: 'red', mutation: 'userDeleteIndices' },
-//     ],
-//   },
-//   {
-//     module: 'crowdPay',
-//     title: 'CrowdPay Index',
-//     cta: [
-//       { title: 'Create', color: 'green', mutation: 'crowdPayCreateIndices' },
-//       { title: 'Generate', color: 'blue', mutation: 'crowdPayPopulateIndex' },
-//       { title: 'Update', color: 'blue', mutation: 'crowdPayPopulateIndex' },
-//       { title: 'Delete', color: 'red', mutation: 'crowdPayDeleteIndices' },
-//     ],
-//   },
-//   {
-//     module: 'accreditation',
-//     title: 'Accreditation Index',
-//     cta: [
-//       { title: 'Create', color: 'green', mutation: 'accreditationCreateIndices' },
-//       { title: 'Generate', color: 'blue', mutation: 'accreditationPopulateIndex' },
-//       { title: 'Update', color: 'blue', mutation: 'accreditationPopulateIndex' },
-//       { title: 'Delete', color: 'red', mutation: 'accreditationDeleteIndices' },
-//     ],
-//   },
-//   {
-//     module: 'linkedBank',
-//     title: 'LinkedBank Index',
-//     cta: [
-//       { title: 'Create', color: 'green', mutation: 'linkedBankCreateIndices' },
-//       { title: 'Generate', color: 'blue', mutation: 'linkedBankPopulateIndex' },
-//       { title: 'Update', color: 'blue', mutation: 'linkedBankPopulateIndex' },
-//       { title: 'Delete', color: 'red', mutation: 'linkedBankDeleteIndices' },
-//     ],
-//   },
-// ];
+const elasticSearchModules = [
+  {
+    module: 'users',
+    title: 'Users Index',
+    cta: [
+      { title: 'Create', color: 'green', mutation: 'userCreateIndices' },
+      { title: 'Generate', color: 'blue', mutation: 'userPopulateIndex' },
+      { title: 'Update', color: 'blue', mutation: 'userPopulateIndex' },
+      { title: 'Delete', color: 'red', mutation: 'userDeleteIndices' },
+    ],
+  },
+  {
+    module: 'crowdPay',
+    title: 'CrowdPay Index',
+    cta: [
+      { title: 'Create', color: 'green', mutation: 'crowdPayCreateIndices' },
+      { title: 'Generate', color: 'blue', mutation: 'crowdPayPopulateIndex' },
+      { title: 'Update', color: 'blue', mutation: 'crowdPayPopulateIndex' },
+      { title: 'Delete', color: 'red', mutation: 'crowdPayDeleteIndices' },
+    ],
+  },
+  {
+    module: 'accreditation',
+    title: 'Accreditation Index',
+    cta: [
+      { title: 'Create', color: 'green', mutation: 'accreditationCreateIndices' },
+      { title: 'Generate', color: 'blue', mutation: 'accreditationPopulateIndex' },
+      { title: 'Update', color: 'blue', mutation: 'accreditationPopulateIndex' },
+      { title: 'Delete', color: 'red', mutation: 'accreditationDeleteIndices' },
+    ],
+  },
+  {
+    module: 'linkedBank',
+    title: 'LinkedBank Index',
+    cta: [
+      { title: 'Create', color: 'green', mutation: 'linkedBankCreateIndices' },
+      { title: 'Generate', color: 'blue', mutation: 'linkedBankPopulateIndex' },
+      { title: 'Update', color: 'blue', mutation: 'linkedBankPopulateIndex' },
+      { title: 'Delete', color: 'red', mutation: 'linkedBankDeleteIndices' },
+    ],
+  },
+];
 
 @inject('elasticSearchStore')
 @withRouter
 @observer
 export default class ElasticSearch extends Component {
-  componentWillMount() {
-    // if (this.props.match.isExact) {
-    //   this.props.accreditationStore.initRequest();
-    // }
-  }
   render() {
     const { match } = this.props;
     const navItems = [
@@ -65,22 +60,22 @@ export default class ElasticSearch extends Component {
     return (
       <Grid>
         <Grid.Column width={4}>
-          <Card fluid>
-            <Card.Content header="Users Index" />
-            <Card.Content>
-              <Card.Description>
-                <Button.Group compact>
-                  <Button size="mini" content="Create" primary />
-                  <Button size="mini" content="Generate" secondary />
-                </Button.Group>
-                <Divider hidden />
-                <Button.Group compact>
-                  <Button size="mini" content="Update" primary />
-                  <Button size="mini" content="Delete" color="red" />
-                </Button.Group>
-              </Card.Description>
-            </Card.Content>
-          </Card>
+          {elasticSearchModules.map(es => (
+            <Card fluid>
+              <Card.Content header={es.title} />
+              <Card.Content>
+                <Card.Description>
+                  <Button.Group compact>
+                    {es.cta.map(b => (
+                      <Button size="mini" content={b.title} color={b.color} />
+                    ))
+                    }
+                  </Button.Group>
+                </Card.Description>
+              </Card.Content>
+            </Card>
+          ))
+          }
         </Grid.Column>
         <Grid.Column width={12}>
           <div className="sticky-sidebar">
