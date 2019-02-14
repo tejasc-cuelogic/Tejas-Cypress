bff9c783d604278bd33f5bba0c33246a7fd9fe80
Swapnil Bhosale <swapnil.cuelogic@nextseed.com>
2019-02-14 12:47:56 +0000

    Admin Transactions feedback changes  feb 1st week

--- src/theme/form/Filters.js
+++ src/theme/form/Filters.js
@@ -28,14 +28,16 @@ export const DropdownFilter = props => (
 );
 
 export const ByKeyword = ({
-  w, executeSearch, placeholder, fLabel, requestState, toggleSearch, filters, addon, more, change,
+  w, executeSearch, placeholder, fLabel, requestState, toggleSearch, filters, addon,
+  more, enableSearch, change,
 }) => (
   <Aux>
     <Grid.Column widescreen={w[0]} largeScreen={w[0]} computer={w[1]} tablet={w[1]} mobile={w[1]}>
       <Form>
         <Form.Field inverted>
           {fLabel && <label className="invisible">{placeholder}</label>}
-          <Input fluid onChange={change} onKeyPress={executeSearch} inverted icon={{ className: 'ns-search' }} iconPosition="left" placeholder={placeholder} />
+          {!enableSearch &&
+          <Input fluid onChange={change} onKeyPress={executeSearch} inverted icon={{ className: 'ns-search' }} iconPosition="left" placeholder={placeholder} />}
         </Form.Field>
       </Form>
     </Grid.Column>
@@ -62,7 +64,7 @@ export const DateRangeFilter = props => (
           type="text"
           format="##-##-####"
           placeholder="MM-DD-YYYY"
-          onValueChange={values => props.change(values, 'startDate')}
+          onValueChange={values => props.change(values, props.nameStart || 'startDate')}
         />
       </Form.Field>
       <Form.Field>
@@ -70,7 +72,7 @@ export const DateRangeFilter = props => (
           type="text"
           format="##-##-####"
           placeholder="MM-DD-YYYY"
-          onValueChange={values => props.change(values, 'endDate')}
+          onValueChange={values => props.change(values, props.nameEnd || 'endDate')}
         />
       </Form.Field>
     </Form.Group>
@@ -88,7 +90,7 @@ export const AmountRangeFilter = props => (
           thousandSeparator
           placeholder={props.placeHolderMin || 'Min Amount'}
           currency
-          onValueChange={values => props.change(values, 'min')}
+          onValueChange={values => props.change(values, props.nameMin || 'min')}
         />
       </Form.Field>
       <Form.Field>
@@ -98,7 +100,7 @@ export const AmountRangeFilter = props => (
           thousandSeparator
           placeholder={props.placeHolderMax || 'Max Amount'}
           currency
-          onValueChange={values => props.change(values, 'max')}
+          onValueChange={values => props.change(values, props.nameMax || 'max')}
         />
       </Form.Field>
     </Form.Group>
