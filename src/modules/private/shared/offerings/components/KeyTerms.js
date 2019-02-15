a06be65656bc90ec35f01cfc8ebdecbff0c915ed
pratik T <pratik.cuelogic>
2019-02-15 18:34:04 +0530

    done changes for keyterms and media new fields

--- src/modules/private/shared/offerings/components/KeyTerms.js
+++ src/modules/private/shared/offerings/components/KeyTerms.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Header, Form, Divider } from 'semantic-ui-react';
+import { Header, Form, Divider, Icon, Confirm } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import Aux from 'react-aux';
 import { inject, observer } from 'mobx-react';
 import { startsWith } from 'lodash';
-import { BUSINESS_INDUSTRIES, SECURITIES_VALUES, BUSINESS_TYPE_VALUES, REGULATION_VALUES, BD_REGULATION_VALUES, FP_REGULATION_VALUES } from '../../../../../services/constants/admin/offerings';
+import { BUSINESS_INDUSTRIES, SECURITIES_VALUES, BUSINESS_TYPE_VALUES, ROUND_TYPE_VALUES, REGULATION_VALUES, BD_REGULATION_VALUES, FP_REGULATION_VALUES } from '../../../../../services/constants/admin/offerings';
 import { FormInput, MaskedInput, FormDropDown, FormTextarea, FormRadioGroup, DropZoneConfirm as DropZone } from '../../../../../theme/form';
 import ButtonGroupType2 from './ButtonGroupType2';
 import HtmlEditor from '../../../../shared/HtmlEditor';
@@ -11,7 +13,7 @@ import HtmlEditor from '../../../../shared/HtmlEditor';
 @observer
 export default class KeyTerms extends Component {
   componentWillMount() {
-    this.props.offeringCreationStore.setFormData('KEY_TERMS_FRM', 'keyTerms');
+    // this.props.offeringCreationStore.setFormData('KEY_TERMS_FRM', 'keyTerms');
   }
   onProFormasDrop = (files) => {
     this.props.offeringCreationStore.setFileUploadData('KEY_TERMS_FRM', 'uploadProformas', files, '', null, 'KEY_TERMS_PROFORMAS');
@@ -23,10 +25,21 @@ export default class KeyTerms extends Component {
     const { KEY_TERMS_FRM, updateOffering, currentOfferingId } = this.props.offeringCreationStore;
     updateOffering(currentOfferingId, KEY_TERMS_FRM.fields, 'keyTerms', null, true, undefined, isApproved);
   }
+  addMore = (e, formName, arrayName) => {
+    e.preventDefault();
+    this.props.offeringCreationStore.addMore(formName, arrayName);
+  }
+  toggleConfirmModal = (e, index, formName) => {
+    e.preventDefault();
+    this.props.offeringCreationStore.toggleConfirmModal(index, formName);
+  }
   editorChange =
   (field, value, form) => this.props.offeringCreationStore.rtEditorChange(field, value, form);
   render() {
-    const { KEY_TERMS_FRM, formChange, maskChange } = this.props.offeringCreationStore;
+    const {
+      KEY_TERMS_FRM, formChange, maskChange, formArrayChange,
+      confirmModal, confirmModalName, removeData,
+    } = this.props.offeringCreationStore;
     const formName = 'KEY_TERMS_FRM';
     const { offer } = this.props.offeringsStore;
     const access = this.props.userStore.myAccessForModule('OFFERINGS');
@@ -176,6 +189,28 @@ export default class KeyTerms extends Component {
               fielddata={KEY_TERMS_FRM.fields.frequencyOfPayments}
               changed={(e, result) => formChange(e, result, formName)}
             />
+            <FormDropDown
+              containerclassname={isReadonly ? 'display-only' : ''}
+              className={isReadonly ? 'display-only' : ''}
+              disabled={isReadonly}
+              fielddata={KEY_TERMS_FRM.fields.roundType}
+              selection
+              value={KEY_TERMS_FRM.fields.roundType.value}
+              name="roundType"
+              placeholder={isReadonly ? 'N/A' : 'Choose here'}
+              options={ROUND_TYPE_VALUES}
+              onChange={(e, result) => formChange(e, result, formName)}
+            />
+            {['unitPrice', 'premoneyValuation'].map(field => (
+              <MaskedInput
+                displayMode={isReadonly}
+                name={field}
+                fielddata={KEY_TERMS_FRM.fields[field]}
+                changed={(values, name) => maskChange(values, formName, name)}
+                currency
+                prefix="$"
+              />
+            ))}
           </Form.Group>
           <Form.Group widths={2}>
             {['investmentMultipleSummary', 'offeringDisclaimer', 'revShareSummary', 'revSharePercentageDescription'].map(field => (
@@ -189,6 +224,41 @@ export default class KeyTerms extends Component {
                   content={KEY_TERMS_FRM.fields[field].value}
                 />
               </Form.Field>
+            ))}
+            <Header as="h6">
+            Additional Key Terms
+              {!isReadonly &&
+              <Link to={this.props.match.url} className="link" onClick={e => this.addMore(e, formName, 'additionalKeyterms')}><small>+ Add New Term</small></Link>
+              }
+            </Header>
+            {KEY_TERMS_FRM.fields.additionalKeyterms.map((field, index) => (
+              <Aux>
+                <Header as="h6">{`Term ${index + 1}`}
+                  {KEY_TERMS_FRM.fields.additionalKeyterms.length > 1 &&
+                  <Link to={this.props.match.url} className="link" onClick={e => this.toggleConfirmModal(e, index, 'additionalKeyterms')} >
+                    <Icon className="ns-close-circle" color="grey" />
+                  </Link>
+                  }
+                </Header>
+                <div className="featured-section">
+                  <FormInput
+                    displayMode={isReadonly}
+                    name="label"
+                    fielddata={field.label}
+                    changed={(e, result) => formArrayChange(e, result, formName, 'additionalKeyterms', index)}
+                  />
+                  <Form.Field>
+                    <Header as="h6">{field.description.label}</Header>
+                    <HtmlEditor
+                      readOnly={isReadonly}
+                      changed={this.editorChange}
+                      name="description"
+                      form={formName}
+                      content={field.description.value}
+                    />
+                  </Form.Field>
+                </div>
+              </Aux>
             ))}
           </Form.Group>
           <Header as="h4">Legal</Header>
@@ -274,6 +344,15 @@ export default class KeyTerms extends Component {
             updateOffer={this.handleFormSubmit}
           />
         </Form>
+        <Confirm
+          header="Confirm"
+          content="Are you sure you want to remove this Term"
+          open={confirmModal}
+          onCancel={this.toggleConfirmModal}
+          onConfirm={() => removeData('KEY_TERMS_FRM', confirmModalName)}
+          size="mini"
+          className="deletion"
+        />
       </div>
     );
   }
