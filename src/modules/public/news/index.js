4606025852a49b3435d2184af0eea9e3b791088a
sagar.patil@cuelogic.co.in <sagar.patil@cuelogic.co.in>
2019-02-28 18:14:03 +0530

    added news page UI

--- src/modules/public/news/index.js
+++ src/modules/public/news/index.js
@@ -64,15 +64,15 @@ export default class News extends Component {
           <section className="center-align">
             <Logo centered dataSrc="LogoBlack" />
           </section>
-          <Header as="h2" className="center-align">Small business investing, <span className="highlight-text">made easy.</span></Header>
+          <Header as="h2" className="center-align mt-0">Small business investing, <span className="highlight-text">made easy.</span></Header>
           <section>
-            <Grid stackable doubling centered relaxed="very">
+            <Grid stackable doubling centered relaxed="very" className="mb-30">
               <Grid.Row>
                 <Grid.Column width={7}>
-                  <Item.Group relaxed="very">
+                  <Item.Group relaxed="very" className={isMobile && 'horizontal-items'}>
                     {
                     highlights.map(h => (
-                      <Item>
+                      <Item className="mb-40">
                         <div className="ui mini image">
                           <NSImage path={h.icon} />
                         </div>
@@ -86,8 +86,8 @@ export default class News extends Component {
                   </Item.Group>
                 </Grid.Column>
                 <Grid.Column width={6}>
-                  <Segment>
-                    <Header as="h4">Start investing in local businesses you know and trust.</Header>
+                  <Segment padded>
+                    <Header as="h4" className={`${isMobile && 'center-align'} mb-20`}>Start investing in local businesses you know and trust.</Header>
                     <Form>
                       <Form.Field>
                         <label>First Name*</label>
@@ -109,7 +109,7 @@ export default class News extends Component {
                         <label>Verify Password*</label>
                         <input placeholder="" />
                       </Form.Field>
-                      <Button type="submit" primary>Sign Up Free</Button>
+                      <Button type="submit" primary fluid>Sign Up Free</Button>
                     </Form>
                   </Segment>
                 </Grid.Column>
@@ -118,9 +118,9 @@ export default class News extends Component {
           </section>
         </Container>
         <section className="bg-offwhite">
-          <Container>
-            <Header as="h2" className={isMobile ? 'mb-30' : 'mb-80'} textAlign={isMobile ? 'left' : 'center'}>Investing, simplified.</Header>
-            <div className="how-it-works-steps">
+          <Container className="mt-30">
+            <Header as="h2" className={isMobile ? 'mb-30' : 'mb-80'} textAlign="center">Investing, simplified.</Header>
+            <div className="how-it-works-steps mb-30">
               <Grid stackable centered columns={3}>
                 <Grid.Column>
                   <Header as="h5">Explore</Header>
@@ -143,7 +143,7 @@ export default class News extends Component {
           </Container>
         </section>
         <section>
-          <Container textAlign={isMobile ? 'left' : 'center'}>
+          <Container textAlign={isMobile ? 'left' : 'center'} className="mt-30">
             <Header as="h2" className="mb-30">
             Build an investment portfolio{' '}
               <Responsive as={Aux} minWidth={1199}><br /></Responsive>
@@ -151,11 +151,12 @@ export default class News extends Component {
             </Header>
             <p className={isMobile ? 'mb-40' : 'mb-50'}>
             NextSeed offers the opportunity to invest in restaurants, fitness studios,
+              {!isMobile && <br />}
             craft breweries and a variety of growing concepts.
             </p>
           </Container>
           {!isMobile ?
-            <Container>
+            <Container className="mb-30">
               <Grid centered stackable className="vertical-gutter">
                 {businesses.map(b => (
                   <Grid.Column textAlign="center" width={4}>
@@ -169,18 +170,16 @@ export default class News extends Component {
             </Container>
         :
             <Aux>
-              <Container>
+              <Container className="mb-30">
                 <NsCarousel {...settings}>
-                  {businesses.map(row => (
-                    row.map(b => (
-                      <Grid.Row>
-                        <Grid.Column className="center-align">
-                          <NSImage path={b.image} centered />
-                          <Header as="h5">{b.title}</Header>
-                          <p>{b.description}</p>
-                        </Grid.Column>
-                      </Grid.Row>
-                    ))
+                  {businesses.map(b => (
+                    <Grid.Row>
+                      <Grid.Column className="center-align">
+                        <NSImage path={b.image} centered />
+                        <Header as="h5">{b.title}</Header>
+                        <p>{b.description}</p>
+                      </Grid.Column>
+                    </Grid.Row>
                   ))
                 }
                 </NsCarousel>
@@ -190,14 +189,14 @@ export default class News extends Component {
         </section>
         <section className="bg-offwhite">
           <Container>
-            <Grid relaxed={!isTablet && 'very'} stackable centered className="mt-80 mb-80">
+            <Grid relaxed={!isTablet && 'very'} stackable centered className={!isMobile && 'mt-40 mb-40'}>
               <Grid.Row>
                 <Grid.Column width={10} textAlign="center">
                   <Header as="h2">Start investing today</Header>
                   {/* {!authStore.isUserLoggedIn &&
                     <Aux>
                       <Header as="h2" className="mb-30">Register for an account.</Header> */}
-                  <Button.Group vertical={isMobile} className="mb-50">
+                  <Button.Group vertical={isMobile}>
                     <Button as={Link} to="/auth/register-investor" primary>Sign Up Free</Button>
                   </Button.Group>
                   {/* </Aux>
