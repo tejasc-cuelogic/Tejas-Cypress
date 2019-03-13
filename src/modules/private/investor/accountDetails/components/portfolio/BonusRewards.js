3bf3a24744f571acc9ce53a84926540ff8ce34f0
Vaibhav Pathak <vaibhav.cuelogic@nextseed.com>
2019-03-13 16:05:14 +0000

    feat(BonusRewards[Issue [1276](https://chaos.ns.team/nsv2/ns-ops/issues/1276) - portfolio details bouns rewards]): reflects bonus reward and Your Reward section for investors invested offering detail section.

--- src/modules/private/investor/accountDetails/components/portfolio/BonusRewards.js
+++ src/modules/private/investor/accountDetails/components/portfolio/BonusRewards.js
@@ -1,37 +1,34 @@
+/* eslint-disable */
 import React, { Component } from 'react';
-// import { Header } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
 import { Route } from 'react-router-dom';
 import { inject, observer } from 'mobx-react';
+import Aux from 'react-aux';
+import { get } from 'lodash';
 import InvestmentTimeline from './Rewards/InvestmentTimeline';
-// import RewardList from './Rewards/RewardList';
+import RewardList from './Rewards/RewardList';
 import Redeem from '../../../rewardsWallet/components/Redeem';
 
-// const list = [
-//   {
-//     id: 1, title: '$50 Gift Card', description: 'Lorem ipsum dolor.
-// Etiam ullamcorper.', action: 'redeem',
-//   },
-//   {
-//     id: 2, title: 'Invitation to the Launch Party', description:
-// 'Lorem ipsum dolor sit amet enim.', action: 'redeem',
-//   },
-//   {
-//     id: 3, title: 'Cooking Classes', description:
-// 'Etiam ullamcorper.', action: 'voucher', expiry: '02-12-2019',
-//   },
-// ];
-
-// const BonusRewards = ({ match }) => (
-@inject('campaignStore')
+@inject('campaignStore', 'portfolioStore', 'investmentStore')
 @observer
 class BonusRewards extends Component {
   render() {
     const { props } = this;
+    const { getInvestor } = props.portfolioStore;
+    const { investmentBonusRewards } = props.investmentStore
+    const investedAmount = get(getInvestor, 'myInvestment') ? get(getInvestor, 'myInvestment') : 0;
+    const rewardList = investmentBonusRewards(parseFloat(investedAmount.replace(/,/, '')));
+    const metaTitle = 'Check the Updates tab for the latest information on when rewards will be delivered.  Unless otherwise indicated, rewards will be available after the business is open.';
     return (
       <div className="inner-content-spacer">
         <InvestmentTimeline title="Your investment" {...props} />
-        {/* <Header as="h4">Your rewards</Header>
-        <RewardList title="Your investment" match={props.match} list={list} /> */}
+        {rewardList.length > 0 &&
+        <Aux>
+        <Header as="h4">Your rewards</Header>
+        <p className="mb-30">{metaTitle}</p>
+        <RewardList title="Your investment" match={props.match} list={rewardList} />
+        </Aux>
+        }
         <Route exact path={`${props.match.url}/redeem/:id`} component={Redeem} />
       </div>
     );
