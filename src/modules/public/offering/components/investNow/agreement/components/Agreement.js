37de818e4999734eb90d31d6c89a20a69090794e
Vaibhav Pathak <vaibhav.cuelogic@nextseed.com>
2019-03-19 17:02:48 +0000

    feat(1371[Issue [ClientInvestNowFlow](https://chaos.ns.team/nsv2/ns-ops/issues/1371) - client investment workflow]): resolve issues mentioned in issue ticket 1371

--- src/modules/public/offering/components/investNow/agreement/components/Agreement.js
+++ src/modules/public/offering/components/investNow/agreement/components/Agreement.js
@@ -219,20 +219,20 @@ export default class Agreement extends React.Component {
                     ))}
                   </Grid.Row>
                 </Grid>
+                {investmentFlowErrorMessage &&
+                  <Message error className="mt-30">
+                    {investmentFlowErrorMessage}
+                  </Message>
+                }
+                <div className="center-align mt-30">
+                  <Button primary content="Invest" loading={inProgress} onClick={this.submit} />
+                  <Button type="button" color="gray" content="Cancel" onClick={this.handleCancelAgreement} />
+                </div>
+                {this.state.showError &&
+                  !this.props.investmentStore.AGREEMENT_DETAILS_FORM.meta.isValid &&
+                  <Message error className="bottom-error">All boxes must be checked to confirm your investment.</Message>
+                }
               </Form>
-              {investmentFlowErrorMessage &&
-                <Message error className="mt-30">
-                  {investmentFlowErrorMessage}
-                </Message>
-              }
-              <div className="center-align mt-30">
-                <Button primary content="Invest" loading={inProgress} onClick={this.submit} />
-                <Button type="button" color="gray" content="Cancel" onClick={this.handleCancelAgreement} />
-              </div>
-              {this.state.showError &&
-                !this.props.investmentStore.AGREEMENT_DETAILS_FORM.meta.isValid &&
-                <Message error className="bottom-error">All boxes must be checked to confirm your investment.</Message>
-              }
             </div>
           </Modal.Content>
         </Modal>
