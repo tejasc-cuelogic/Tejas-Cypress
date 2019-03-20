48afa7cdc8e7e651761e9c9f3c9fd715c72aecfc
Pratik Tayade <pratik.cuelogic@nextseed.com>
2019-03-20 14:51:22 +0000

    feat(analytics[Issue [1343](https://chaos.ns.team/nsv2/ns-ops/issues/1343) - analytics]): call analytics.identify as soon as user is loggedin

--- src/modules/public/offering/components/investNow/agreement/components/FireworkAnimation.js
+++ src/modules/public/offering/components/investNow/agreement/components/FireworkAnimation.js
@@ -1,4 +1,6 @@
+/* eslint-disable react/require-default-props */
 import React from 'react';
+import PropTypes from 'prop-types';
 import sizeMe from 'react-sizeme';
 import Confetti from 'react-confetti';
 
@@ -6,14 +8,32 @@ const Firework = sizeMe({
   monitorHeight: true,
   monitorWidth: true,
 })(class Firework extends React.PureComponent {
+  static propTypes = {
+    size: PropTypes.shape({
+      width: PropTypes.number,
+      height: PropTypes.number,
+    }),
+  }
   render() {
     return (
       <div className="animation-container">
-        <Confetti {...this.props.size} recycle />
+        <Confetti {...this.props.size} recycle={false} />
       </div>
     );
   }
 });
+// const Firework = sizeMe({
+//   monitorHeight: true,
+//   monitorWidth: true,
+// })(class Firework extends React.PureComponent {
+//   render() {
+//     return (
+//       <div className="animation-container">
+//         <Confetti {...this.props.size} recycle />
+//       </div>
+//     );
+//   }
+// });
 
 export default Firework;
 
