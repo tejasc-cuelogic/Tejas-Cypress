a27d36022fc4a2794174e307ac16d69985e3864a
Bob Dunton <bob@nextseed.com>
2019-03-27 21:20:25 -0400

    initial layout

--- src/modules/private/investor/referrals/components/ReferralsDetails.js
+++ src/modules/private/investor/referrals/components/ReferralsDetails.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import { get } from 'lodash';
-import { Header, Popup, Icon } from 'semantic-ui-react';
+import { Header, Popup, Icon, Container } from 'semantic-ui-react';
 import { SAASQUATCH_TENANT_ALIAS } from '../../../../../constants/common';
 import { InlineLoader } from './../../../../../theme/shared';
 import Helper from '../../../../../helper/utility';
@@ -59,17 +59,74 @@ export default class ReferralsDetails extends Component {
     }
   }
   render() {
+    const codeBoxStyle = {
+      width: '250px',
+      paddingLeft: '25px',
+      paddingRight: '25px',
+      fontFamily: 'Lucida Console", Monaco, monospace',
+      fontWeight: '600',
+      backgroundColor: '#e9edf9',
+    };
     return (
       <div>
         {this.state.loading ?
           <InlineLoader /> :
-          <Header as="h3" textAlign="center">
-            Available Credit{' '}
-            <Popup trigger={<small><Icon name="help circle" color="green" size="small" /></small>} content="Credits can be used for investment purposes only and cannot be withdrawn. Uninvested credits do not bear interest." position="bottom center" />
-            {Helper.MoneyMathDisplayCurrency(this.state.availableCredit)}
-          </Header>
+          <div>
+            <Container textAlign={this.props.isMobile ? 'left' : 'center'}>
+              <Header as="h4">
+                Available Credit{' '}
+                <Popup trigger={<sup><Icon name="help circle" color="green" /></sup>} content="Credits can be used for investment purposes only and cannot be withdrawn. Uninvested credits do not bear interest." position="bottom center" />
+                {Helper.MoneyMathDisplayCurrency(this.state.availableCredit)}
+              </Header>
+            </Container>
+            <br />
+            <Container textAlign={this.props.isMobile ? 'left' : 'center'}>
+              <Header as="h3">
+                Give $20, Get $20!
+              </Header>
+              <Header as="h4">
+                Refer a friend to NextSeed.
+              </Header>
+              <p>
+                When they sign up for an investment account, <b>you both get $20</b> in credit to use on the site.
+              </p>
+              <p className>
+                For a limited time, you can <b>invite as many friends as you&apos;d like and earn as much credit as you want.</b> Share the link below and use these shortcuts to tell your friends. The more the merrier!
+              </p>
+            </Container>
+            <br />
+            <Container textAlign={this.props.isMobile ? 'left' : 'center'}>
+              <div className="ui big action input">
+                <input type="text" style={codeBoxStyle} value="MYCODE" readOnly />
+                <button className="ui teal right labeled icon button">
+                  <i className="copy icon" />
+                    Copy
+                </button>
+              </div>
+            </Container>
+            <br />
+            <Container textAlign={this.props.isMobile ? 'left' : 'center'}>
+              <div className="ui buttons">
+                <button className="ui labeled icon button">
+                  <i className="envelope outline icon" />
+                  Email
+                </button>
+                <button className="ui labeled twitter icon button">
+                  <i className="twitter icon" />
+                  Tweet
+                </button>
+                <button className="ui labeled linkedin icon button">
+                  <i className="facebook messenger icon" />
+                  Messenger
+                </button>
+                <button className="ui labeled facebook icon button">
+                  <i className="facebook f icon" />
+                  Share
+                </button>
+              </div>
+            </Container>
+          </div>
         }
-        <div className="squatchembed" />
       </div>
     );
   }
