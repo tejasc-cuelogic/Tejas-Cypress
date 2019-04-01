ed2f8592e2a37dc2bbff78a87453d25131653706
Vaibhav Pathak <vaibhav.cuelogic@nextseed.com>
2019-04-01 21:33:04 +0530

    mange condition in TNAR and NAR for zeor value. If value occurs as  Zero show N/A else show actual result

--- src/modules/private/investor/accountDetails/containers/Portfolio.js
+++ src/modules/private/investor/accountDetails/containers/Portfolio.js
@@ -5,6 +5,7 @@ import { includes, orderBy, get } from 'lodash';
 import { inject, observer } from 'mobx-react';
 import { Route, Link } from 'react-router-dom';
 import { Header, Card, Button } from 'semantic-ui-react';
+import money from 'money-math';
 import SummaryHeader from '../components/portfolio/SummaryHeader';
 import { DataFormatter } from '../../../../../helper';
 import PortfolioAllocations from '../components/portfolio/PortfolioAllocations';
@@ -73,6 +74,7 @@ export default class Portfolio extends Component {
       return <InlineLoader />;
     }
     const { getInvestorAccounts, getPieChartData } = portfolioStore;
+    const tnarValue = get(getInvestorAccounts, 'tnar');
     const summaryDetails = {
       isAccountFrozen: isUserAccountFrozen,
       accountType: includes(this.props.location.pathname, 'individual') ? 'individual' : includes(this.props.location.pathname, 'ira') ? 'ira' : 'entity',
@@ -88,7 +90,7 @@ export default class Portfolio extends Component {
           title: 'Net Payments', content: getInvestorAccounts && getInvestorAccounts.netPayments, type: 1, info: 'Payments received to date from all prior investments, minus NextSeed fees.',
         },
         {
-          title: 'TNAR', content: getInvestorAccounts && getInvestorAccounts.tnar, type: 1, info: <span>The Total Net Annualized Return (TNAR) approximates the overall financial return on your investment portfolio. See the <Link target="_blank" to="/resources/education-center">Education Center</Link> for a full explanation of how TNAR is calculated.</span>,
+          title: 'TNAR', content: tnarValue && !money.isZero(tnarValue) ? tnarValue : 'N/A', type: 1, info: <span>The Total Net Annualized Return (TNAR) approximates the overall financial return on your investment portfolio. See the <Link target="_blank" to="/resources/education-center">Education Center</Link> for a full explanation of how TNAR is calculated.</span>,
         },
       ],
     };
