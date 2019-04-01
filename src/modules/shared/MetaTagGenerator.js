9ec72e477d5f462c0db6a7d0de8b7727d8646b01
Rutuja Bhat <rutuja.cuelogic>
2019-04-01 16:14:10 +0530

    issue 1452

--- src/modules/shared/MetaTagGenerator.js
+++ src/modules/shared/MetaTagGenerator.js
@@ -7,7 +7,7 @@ const MetaTagGenerator = ({ metaTagsData }) => (
     <link rel="canonical" href={window.location.href} />
     {map(metaTagsData, d => (
       d.type === 'meta' ?
-        <meta name={d.name} content={d.content} />
+        <meta key={new Date().getTime()} name={d.name} content={d.content} />
       : d.type === 'ogTag' ?
         <meta property={d.property} content={d.content} />
       : null
