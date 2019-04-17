c352feeac40c97b2754b609fa304219db90541d1
Rutuja Bhat <rutuja.cuelogic>
2019-04-17 18:55:12 +0530

    resolved issue 1615

--- src/modules/private/shared/offerings/components/BonusRewards.js
+++ src/modules/private/shared/offerings/components/BonusRewards.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
-// import Report from './bonusrewards/Report';
+import Report from './bonusrewards/Report';
 import Creation from './bonusrewards/Creation';
 
 @inject('offeringsStore', 'userStore')
 @observer
 export default class BonusRewards extends Component {
   render() {
-    // const { isIssuer } = this.props.userStore;
-    // const { match, offeringsStore } = this.props;
-    const { match } = this.props;
-    // const { offer } = offeringsStore;
-    // return (offer.stage === 'CREATION' || isIssuer ||
-    // (offer.stage === 'LIVE' &&
-    // `${match.url.split('/')
-    // .slice(-2)[0]}/${match.url.split('/').pop()}` ===
-    // 'offering-creation/bonus-rewards')) ? <Creation match={match} /> : <Report />;
+    const currentPhase = false;
+    /*
+    currentPhase is the temp constant, used to prevent rendering of <Report /> component.
+    And it will be removed in next launch.
+    */
+    const { isIssuer } = this.props.userStore;
+    const { match, offeringsStore } = this.props;
+    const { offer } = offeringsStore;
+    if (currentPhase) {
+      return (offer.stage === 'CREATION' || isIssuer ||
+      (offer.stage === 'LIVE' && `${match.url.split('/').slice(-2)[0]}/${match.url.split('/').pop()}` === 'offering-creation/bonus-rewards')) ? <Creation match={match} /> : <Report />;
+    }
     return (<Creation match={match} />);
   }
 }
