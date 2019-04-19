4cdb0028686f43f8f6f058aa3c6416bbe5850f10
Rutuja Bhat <rutuja.cuelogic@nextseed.com>
2019-04-19 13:47:51 +0000

    feat( dev-utility-data[Issue [1652](https://chaos.ns.team/nsv2/ns-ops/issues/1652) -  dev utility data]):  dev utility data

---
+++ src/modules/private/admin/dev/components/Data.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Grid, Card, Button, Form } from 'semantic-ui-react';
+import { inject, observer } from 'mobx-react';
+import { withRouter } from 'react-router-dom';
+import { FormInput } from '../../../../../theme/form';
+
+@inject('elasticSearchStore', 'uiStore')
+@withRouter
+@observer
+export default class Data extends Component {
+  onSubmit = (e) => {
+    e.preventDefault();
+    const { uiStore, elasticSearchStore } = this.props;
+    uiStore.setProgress();
+    elasticSearchStore.submitStorageDetails().finally(uiStore.setProgress(false));
+  }
+
+  render() {
+    const { elasticSearchStore, uiStore } = this.props;
+    const { STORAGE_DETAILS_SYNC_FRM, storageDetailsChange } = elasticSearchStore;
+    const { inProgress } = uiStore;
+
+    return (
+      <Grid>
+        <Grid.Column>
+          <Card fluid className="elastic-search">
+            <Card.Content header="Box Audit" />
+            <Card.Content>
+              <Card.Description>
+                <Form error onSubmit={this.onSubmit}>
+                  <Form.Group className="bottom-aligned">
+                    <FormInput
+                      key="userId"
+                      type="text"
+                      name="userId"
+                      containerwidth="12"
+                      fielddata={STORAGE_DETAILS_SYNC_FRM.fields.userId}
+                      changed={(e, result) => storageDetailsChange(e, result)}
+                    />
+                    <Form.Field width={4}>
+                      <Button primary fluid content="Sycn Storage Details" disabled={!STORAGE_DETAILS_SYNC_FRM.meta.isValid} loading={inProgress} />
+                    </Form.Field>
+                  </Form.Group>
+                </Form>
+                <Button primary className="mt-30" content="Sync All Investors" />
+              </Card.Description>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
---
+++ src/services/constants/admin/data.js
@@ -0,0 +1,13 @@
+
+export const STORAGE_DETAILS_SYNC = {
+  userId: {
+    value: '',
+    label: 'User Id',
+    error: undefined,
+    rule: 'required',
+    placeHolder: 'Enter here',
+    customErrors: {
+      required: 'This field is required',
+    },
+  },
+};
