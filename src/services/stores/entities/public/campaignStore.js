b2172457e54e329c32b708f30ce9079c3a12dea9
Alan Lee <alan@nextseed.com>
2019-04-22 11:47:15 -0500

    hotfix bug

--- src/services/stores/entities/public/campaignStore.js
+++ src/services/stores/entities/public/campaignStore.js
@@ -99,6 +99,8 @@ export class CampaignStore {
   }
 
   @computed get activeList() {
+    // const activeListArr = this.OfferingList.filter(o => Object.keys(pickBy(STAGES, s => s.publicRef === 'active')).includes(o.stage));
+    // return orderBy(activeListArr, o => get(o, 'keyTerms.shorthandBusinessName').toLowerCase(), ['desc']);
     return this.OfferingList.filter(o => Object.keys(pickBy(STAGES, s => s.publicRef === 'active')).includes(o.stage));
   }
 
