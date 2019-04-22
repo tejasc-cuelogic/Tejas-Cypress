ec19497aa67583a661581b3d046ef2ca4d16ae55
Vaibhav Pathak <vaibhav.cuelogic@nextseed.com>
2019-04-22 20:13:23 +0530

    modify sequence for reseting form

--- src/modules/private/admin/offerings/containers/OfferingDetails.js
+++ src/modules/private/admin/offerings/containers/OfferingDetails.js
@@ -32,9 +32,9 @@ export default class OfferingDetails extends Component {
 
   handleCloseModal = (e) => {
     e.stopPropagation();
+    this.props.offeringCreationStore.resetAffiliatedIssuerForm();
     this.props.offeringCreationStore.resetAllForms();
     this.props.offeringCreationStore.resetOfferingId();
-    this.props.offeringCreationStore.resetAffiliatedIssuerForm();
     this.props.history.push(`${this.props.refLink}/${this.props.match.params.stage}`);
     window.onpopstate = null;
   };
