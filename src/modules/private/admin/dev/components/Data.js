42cd18ff213aa9e96e9d1fa14acadbedc27d30fa
Rutuja Bhat <rutuja.cuelogic>
2019-04-23 17:34:15 +0530

    1652 sync all investors

--- src/modules/private/admin/dev/components/Data.js
+++ src/modules/private/admin/dev/components/Data.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { Grid, Card, Button, Form } from 'semantic-ui-react';
 import { inject, observer } from 'mobx-react';
 import { withRouter } from 'react-router-dom';
-import { FormInput } from '../../../../../theme/form';
+import { get } from 'lodash';
+import Aux from 'react-aux';
+import { FormInput, MaskedInput } from '../../../../../theme/form';
 import { FieldError } from '../../../../../theme/shared';
 
 @inject('elasticSearchStore', 'uiStore')
@@ -16,10 +18,16 @@ export default class Data extends Component {
   onSubmit = () => {
     this.props.elasticSearchStore.submitStorageDetails();
   }
+  bulkFormOnSubmit = () => {
+    this.props.elasticSearchStore.submitStorageDetailsinBulk();
+  }
 
   render() {
     const { elasticSearchStore, uiStore } = this.props;
-    const { STORAGE_DETAILS_SYNC_FRM, storageDetailsChange, boxMsg } = elasticSearchStore;
+    const {
+      STORAGE_DETAILS_SYNC_FRM, bulkStorageDetailsChange,
+      BULK_STORAGE_DETAILS_SYNC_FRM, storageDetailsChange, boxMsg, countValues,
+    } = elasticSearchStore;
     const { inProgress } = uiStore;
 
     return (
@@ -48,7 +56,37 @@ export default class Data extends Component {
                     }
                   </Form.Group>
                 </Form>
-                <Button disabled primary className="mt-30" content="Sync All Investors" />
+
+                <Form error onSubmit={this.bulkFormOnSubmit}>
+                  <Form.Group className="bottom-aligned">
+                    <MaskedInput
+                      key="limit"
+                      name="limit"
+                      allowNegative={false}
+                      label={BULK_STORAGE_DETAILS_SYNC_FRM.fields.limit.label}
+                      number
+                      containerwidth="12"
+                      fielddata={BULK_STORAGE_DETAILS_SYNC_FRM.fields.limit}
+                      changed={(e, result) => bulkStorageDetailsChange(e, result, 'BULK_STORAGE_DETAILS_SYNC_FRM', 'mask')}
+                      disabled={inProgress}
+                    />
+                    <Form.Field width={4}>
+                      <Button primary fluid content="Sync All Investors" disabled={!BULK_STORAGE_DETAILS_SYNC_FRM.meta.isValid || inProgress} loading={inProgress} />
+                    </Form.Field>
+                  </Form.Group>
+                </Form>
+                { countValues &&
+                <Aux>
+                  {get(countValues, 'count') ?
+                    <p className="hightlight-text" > count: {get(countValues, 'count')} </p> : ''
+                  }
+                  {get(countValues, 'createdCount') ?
+                    <p className="hightlight-text" > countValues: {get(countValues, 'createdCount')} </p> : ''
+                  }
+                </Aux>
+                }
+
+                {/* <Button disabled primary className="mt-30" content="Sync All Investors" /> */}
               </Card.Description>
             </Card.Content>
           </Card>
--- src/services/stores/entities/dev/elasticSearchStore.js
+++ src/services/stores/entities/dev/elasticSearchStore.js
@@ -1,17 +1,20 @@
 import { observable, action } from 'mobx';
 import { get } from 'lodash';
 import * as elasticSearchQueries from '../../queries/elasticSearch';
-import { generateInvestorFolderStructure } from '../../queries/data';
+import { generateInvestorFolderStructure, storageDetailsForInvestor } from '../../queries/data';
 import { GqlClient as client } from '../../../../api/gqlApi';
 import Helper from '../../../../helper/utility';
 import { FormValidator as Validator } from '../../../../helper';
-import { STORAGE_DETAILS_SYNC } from '../../../constants/admin/data';
+import { STORAGE_DETAILS_SYNC, BULK_STORAGE_DETAILS_SYNC } from '../../../constants/admin/data';
 import uiStore from '../../../stores/entities/shared/uiStore';
 
 export class ElasticSearchStore {
   @observable STORAGE_DETAILS_SYNC_FRM = Validator.prepareFormObject(STORAGE_DETAILS_SYNC);
+  @observable BULK_STORAGE_DETAILS_SYNC_FRM =
+  Validator.prepareFormObject(BULK_STORAGE_DETAILS_SYNC);
   @observable inProgress = {};
   @observable boxMsg = '';
+  @observable countValues = [];
 
   @action
   setFieldValue = (field, value) => {
@@ -59,6 +62,48 @@ export class ElasticSearchStore {
     Validator.onChange(this.STORAGE_DETAILS_SYNC_FRM, Validator.pullValues(e, res));
     this.setFieldValue('boxMsg', '');
   };
+
+  @action
+  bulkStorageDetailsChange = (values, field, formName, fieldType) => {
+    if (fieldType === 'mask') {
+      this[formName] = Validator.onChange(
+        this[formName],
+        { name: field, value: values.floatValue },
+      );
+    } else {
+      this[formName] =
+      Validator.onChange(this[formName], Validator.pullValues(field, values));
+    }
+  };
+
+  @action
+  submitStorageDetailsinBulk = () => {
+    uiStore.setProgress();
+    this.setFieldValue('countValues', '');
+    const limit = get(this.BULK_STORAGE_DETAILS_SYNC_FRM, 'fields.limit.value') || null;
+    return new Promise((res, rej) => {
+      client
+        .mutate({
+          mutation: storageDetailsForInvestor,
+          variables: { limit },
+        })
+        .then((result) => {
+          if (result.data.storageDetailsForInvestor) {
+            Helper.toast('Box folder Creation has been initiated, please check after some time.', 'success');
+            document.getElementsByName('limit')[0].value = '';
+            this.setFieldValue('countValues', get(result.data, 'storageDetailsForInvestor'));
+          }
+          this.resetForm();
+          uiStore.setProgress(false);
+          res(result);
+        })
+        .catch((error) => {
+          Helper.toast('Something went wrong, please try again later.', 'error');
+          uiStore.setProgress(false);
+          rej(error);
+        });
+    });
+  }
 
   @action
   elasticSearchHandler = (mutation) => {
