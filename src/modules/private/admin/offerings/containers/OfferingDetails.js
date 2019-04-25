a1b0d7c21fa3c09769185b1736e558f755b8e665
Chetan Bura <chetan.cuelogic@nextseed.com>
2019-04-25 15:30:10 +0530

    fix loader for offering listing

--- src/modules/private/admin/offerings/containers/OfferingDetails.js
+++ src/modules/private/admin/offerings/containers/OfferingDetails.js
@@ -45,7 +45,7 @@ export default class OfferingDetails extends Component {
     const { offerLoading, offerOld } = offeringsStore;
     let { offer } = offeringsStore;
     offer = !offerLoading && offerOld.stage ? offerOld : offer;
-    if (offerLoading || (offerLoading && offer && !offer.stage)) {
+    if (!get(offer, 'id') || (offerLoading && offer && !offer.stage)) {
       return <InlineLoader />;
     }
     const isDev = ['localhost', 'develop'].includes(REACT_APP_DEPLOY_ENV);
