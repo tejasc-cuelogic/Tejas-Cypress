d076c123c46c39a18b6d0844127cbdd4ca281fc7
Vaibhav Pathak <vaibhav.cuelogic@nextseed.com>
2019-04-26 12:01:04 +0530

    modify label calculation condtion for NEW label

--- src/modules/private/shared/offerings/components/comments/MessagesList.js
+++ src/modules/private/shared/offerings/components/comments/MessagesList.js
@@ -56,7 +56,7 @@ const MessagesList = props => (
                 avatarUrl: (get(msg, 'createdUserInfo.info.avatar.url') || null),
                 roles: get(msg, 'createdUserInfo.roles') ? get(msg, 'createdUserInfo.roles').map(r => r.scope) : [],
               }}
-            />
+                  />
           }
           </div>
           <List.Content>
