ad43b17754707c2b4e163f9eae5cd23fc4a1348f
Sagar Patil <sagar.cuelogic@nextseed.com>
2019-04-26 16:23:51 +0530

    updated styles and UI for sidebar

--- src/theme/layout/SidebarLeftOverlay.js
+++ src/theme/layout/SidebarLeftOverlay.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import { Link } from 'react-router-dom';
 import Aux from 'react-aux';
-import { Responsive, Sidebar, Menu, Button, Icon, Dimmer, Loader } from 'semantic-ui-react';
+import { Responsive, Sidebar, Menu, Icon, Dimmer, Loader } from 'semantic-ui-react';
 import { Scrollbars } from 'react-custom-scrollbars';
 import NotificationPanel from './NotificationPanel';
 import { SidebarNav } from './SidebarNav';
@@ -47,11 +47,9 @@ const MySidebar = observer(props => (
         <Sidebar
           as={Menu}
           animation={props.desktop ? 'push' : 'overlay'}
-          width="thin"
           visible={
             props.desktop ? props.layoutState.leftPanel : props.layoutState.leftPanelMobile
           }
-          icon
           vertical
           inverted={(props.UserInfo.roles[0] !== 'investor')}
           className={props.UserInfo.roles[0]}
@@ -64,7 +62,7 @@ const MySidebar = observer(props => (
             renderThumbHorizontal={p => <div {...p} className="thumb-horizontal" />}
             renderView={p => <div {...p} className="view" />}
           >
-            <Link to="/">
+            <Link to="/" className="logo-wrapper">
               <Logo
                 className="logo"
                 dataSrc={((props.layoutState.leftPanel) ?
@@ -88,12 +86,12 @@ const MySidebar = observer(props => (
             <SidebarNav handleLogOut={props.handleLogOut} roles={props.UserInfo.roles} {...props} />
           </Scrollbars>
         </Sidebar>
-        {props.desktop &&
+        {/* {props.desktop &&
           <Button onClick={props.toggle} className="item collapseIcon">
             <i className={`angle ${(props.layoutState.leftPanel) ? 'left' : 'right'} icon`} />
             <span>Collapse</span>
           </Button>
-        }
+        } */}
       </Aux>
     ) : <SidebarNav roles={props.UserInfo.roles} onlyMount />
     }
