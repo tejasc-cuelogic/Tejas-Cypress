6ea647e3f34d965edcbed6e3c32e88937f67b348
Chetan Bura <chetan.cuelogic@nextseed.com>
2019-04-29 16:17:06 +0530

    investor list export csv file name updated with businessname

--- src/modules/private/shared/offerings/components/Investors.js
+++ src/modules/private/shared/offerings/components/Investors.js
@@ -2,31 +2,29 @@ import React, { Component } from 'react';
 import Aux from 'react-aux';
 import { observer, inject } from 'mobx-react';
 import { toJS } from 'mobx';
+import { get } from 'lodash';
 import { Form, Grid, Button } from 'semantic-ui-react';
 import { ByKeyword } from '../../../../../theme/form/Filters';
-// import Filters from './investors/Filters';
 import Listing from './investors/Listing';
 import Helper from '../../../../../helper/utility';
 
-@inject('userStore', 'offeringInvestorStore')
+@inject('userStore', 'offeringInvestorStore', 'offeringsStore')
 @observer
 export default class BonusRewards extends Component {
   componentWillMount() {
     this.props.offeringInvestorStore.initRequest(this.props.offeringId);
   }
-  // setSearchParam = (e, { name, value }) => {
-  //   this.props.offeringInvestorStore.setInitiateSrch(name, value);
-  // }
   executeSearch = (e) => {
     this.props.offeringInvestorStore.setInitiateSrch('keyword', e.target.value);
   }
   populateCsvData = () => {
     const { investorLists } = this.props.offeringInvestorStore;
+    const { offer } = this.props.offeringsStore;
     const fields = ['firstName', 'lastName', 'userEmail', 'city', 'state', 'accountType', 'amount', 'autoDraftAmount', 'credit', 'investmentDate', 'investmentsCount', 'referralCode.code', 'referralCode.isValid'];
     const params = {
       fields,
       data: toJS(investorLists),
-      fileName: 'InvestorList',
+      fileName: `${get(offer, 'keyTerms.shorthandBusinessName')}-investors`,
     };
     Helper.downloadCSV(params);
   }
