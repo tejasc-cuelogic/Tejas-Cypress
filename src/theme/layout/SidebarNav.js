90aa46e4c4e50d2b164aab577fd3bd38f1926a47
Sagar Patil <sagar.cuelogic@nextseed.com>
2019-04-30 13:04:19 +0530

    updated add new account button on sidebar

--- src/theme/layout/SidebarNav.js
+++ src/theme/layout/SidebarNav.js
@@ -3,7 +3,7 @@ import { Link, withRouter, matchPath } from 'react-router-dom';
 import { inject, observer } from 'mobx-react';
 import _ from 'lodash';
 import Aux from 'react-aux';
-import { Menu, Icon } from 'semantic-ui-react';
+import { Menu, Icon, Button } from 'semantic-ui-react';
 import { PRIVATE_NAV, PUBLIC_NAV, FOOTER_NAV } from '../../constants/NavigationMeta';
 import { NavItems } from './NavigationItems';
 import { REACT_APP_DEPLOY_ENV } from '../../constants/common';
@@ -53,9 +53,12 @@ export class SidebarNav extends Component {
           !props.signupStatus.finalStatus && props.accForm.fields.accType.values.length !== 0 &&
           props.signupStatus.investorProfileCompleted &&
             // <Button as={Link} to="/app/summary/account-creation">Add New Account</Button>
-            <Menu.Item as={Link} className="add-account" to="/app/summary/account-creation">
-              <Icon name="add" />
-              <span>Add New Account</span>
+            // <Menu.Item as={Link} className="add-account" to="/app/summary/account-creation">
+            //   <Icon name="add" />
+            //   <span>Add New Account</span>
+            // </Menu.Item>
+            <Menu.Item>
+              <Button fluid basic compact as={Link} to="/app/summary/account-creation" content="Open New Account" />
             </Menu.Item>
         }
       </Aux>
