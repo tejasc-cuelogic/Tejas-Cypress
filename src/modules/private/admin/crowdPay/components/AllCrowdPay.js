bc3cb40ce0746370e99d28ebe760e3d95a6cde57
Swapnil bhosale <swapnil.cuelogic@nextseed.com>
2019-05-10 15:20:44 +0530

    get method added for failReason

--- src/modules/private/admin/crowdPay/components/AllCrowdPay.js
+++ src/modules/private/admin/crowdPay/components/AllCrowdPay.js
@@ -140,7 +140,7 @@ export default class AllCrowdPay extends Component {
                     }
                     {(type !== 'review' && type === 'individual') &&
                     <Table.Cell>
-                      {account.cip && account.cip.failReason.length ?
+                      {get(account, 'cip.faiReason') ?
                         <List as="ol">{(account.cip.failReason).map(obj => <List.Item as="li" value="-">{obj.message}</List.Item>)}</List>
                         : <p>-</p>
                       }
