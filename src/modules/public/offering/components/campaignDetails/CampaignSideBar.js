8b15190c36b36e267bb5895b816c772f3550a504
Alan Lee <alan@nextseed.com>
2019-05-11 08:57:24 -0500

    hide days left if its 0 or less, or if offering is closed

--- src/modules/public/offering/components/campaignDetails/CampaignSideBar.js
+++ src/modules/public/offering/components/campaignDetails/CampaignSideBar.js
@@ -81,7 +81,7 @@ export default class CampaignSideBar extends Component {
               </p>
               <div className="offer-stats">
                 <Statistic.Group>
-                  {diff ?
+                  {!isClosed && diff > 0 &&
                     <Statistic size="mini" className="basic">
                       <Statistic.Value>{diff}</Statistic.Value>
                       <Statistic.Label>Days left</Statistic.Label>
