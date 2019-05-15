78d6adf2ed44a233a8c7573b5625c1ae166a355e
Pratik Tayade <pratik.cuelogic@nextseed.com>
2019-05-15 08:27:38 +0000

    Update utility.js

--- src/helper/utility.js
+++ src/helper/utility.js
@@ -212,20 +212,16 @@ export class Utility {
     // get the real base64 content of the file
     const b64Data = block[1].split(',')[1];
     const size = sliceSize || 512;
-
     const byteCharacters = atob(b64Data);
     const byteArrays = [];
 
     for (let offset = 0; offset < byteCharacters.length; offset += size) {
       const slice = byteCharacters.slice(offset, offset + size);
-
       const byteNumbers = new Array(slice.length);
       for (let i = 0; i < slice.length; i += 1) {
         byteNumbers[i] = slice.charCodeAt(i);
       }
-
       const byteArray = new Uint8Array(byteNumbers);
-
       byteArrays.push(byteArray);
     }
 
