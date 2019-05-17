740af10361397b4a4014a3e378c8a9514b31ab81
Swapnil bhosale <swapnil.cuelogic@nextseed.com>
2019-05-17 13:51:15 +0530

    manual and plaid process cypress

--- cypress/integration/investorSignUp/investor_account_spec.js
+++ cypress/integration/investorSignUp/investor_account_spec.js
@@ -6,36 +6,26 @@ describe('Account Creation', () => {
     InvestorFlowProcess();
   });
 
-  // it('should successfully create individual account with manual link bank process', () => {
-  //   registerApiCall('upsertInvestorAccount');
-  //   cy.get('input[name="accType"]').check('0', { force: true });
-  //   cy.get('button.next').click();
-  //   // cy.get('.bank-link:first').click({ force: true });
-  //   // cy.wait(10000);
-  //   // cy.get('.PrivacyInterstitialPane').find('button').contains('Continue').click();
-  //   // cy.get('.Pane__content').within(() => {
-  //   //   cy.get('input[name="username"]').type('user_good');
-  //   //   cy.get('input[name="password"]').type('pass_good');
-  //   //   cy.get('button[type="submit"]').click();
-  //   // });
-  //   // cy.get('.SelectAccountPane__account-list:first').click();
-  //   // cy.get('button').contains('Continue').click();
-  //   cy.get('button.link-button').contains('Or enter it manually').click();
-  //   cy.get('input[name="accountNumber"]').type('0000000008');
-  //   cy.get('input[name="routingNumber"]').type('122105278');
-  //   cy.get('input[name="accountType"]').check('SAVINGS', { force: true });
-  //   cy.get('button.button').contains('Confirm').click();
-  //   cy.wait('@upsertInvestorAccount');
-  //   cy.get('form').within(() => {
-  //     cy.get('input[name="value"]').type('15000');
-  //     cy.get('button').contains('Confirm').click();
-  //     cy.wait('@upsertInvestorAccount');
-  //   });
-  //   cy.wait(3000);
-  //   cy.get('.multistep').within(() => {
-  //     cy.get('button').contains('Create your account').click({ force: true });
-  //   });
-  // });
+  it('should successfully create individual account with manual link bank process', () => {
+    registerApiCall('upsertInvestorAccount');
+    cy.get('input[name="accType"]').check('0', { force: true });
+    cy.get('button.next').click();
+    cy.get('button.link-button').contains('Or enter it manually').click();
+    cy.get('input[name="accountNumber"]').type('0000000008');
+    cy.get('input[name="routingNumber"]').type('122105278');
+    cy.get('input[name="accountType"]').check('SAVINGS', { force: true });
+    cy.get('button.button').contains('Confirm').click();
+    cy.wait('@upsertInvestorAccount');
+    cy.get('form').within(() => {
+      cy.get('input[name="value"]').type('15000');
+      cy.get('button').contains('Confirm').click();
+      cy.wait('@upsertInvestorAccount');
+    });
+    cy.wait(3000);
+    cy.get('.multistep').within(() => {
+      cy.get('.center-align > .button').contains('Create your account').click({ force: true });
+    });
+  });
 
   it('should successfully create individual account with plaid link bank process', () => {
     registerApiCall('upsertInvestorAccount');
@@ -53,7 +43,7 @@ describe('Account Creation', () => {
       stripe.find('input[name="password"]').type('pass_good');
       stripe = cy.wrap($body);
       stripe.find('button[type="submit"]').click();
-      cy.wait(3000);
+      cy.wait(4000);
       stripe = cy.wrap($body);
       stripe.find('div.SelectAccountPane__account-list:first').click();
       stripe = cy.wrap($body);
