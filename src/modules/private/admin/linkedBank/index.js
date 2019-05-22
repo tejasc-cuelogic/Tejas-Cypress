5bbebecf931e499c4d25d6dfe53d03cb7cb9c9e5
Pankaj <pankaj.cuelogic@nextseed.com>
2019-05-22 15:42:54 +0530

    added filter for locked users

--- src/modules/private/admin/linkedBank/index.js
+++ src/modules/private/admin/linkedBank/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
+import Aux from 'react-aux';
 import PrivateLayout from '../../shared/PrivateLayout';
 import AllRequests from './components/AllRequests';
 import { ByKeyword } from '../../../../theme/form/Filters';
+import { FormCheckbox } from '../../../../theme/form';
 
 @inject('bankAccountStore')
 @observer
@@ -31,19 +33,30 @@ export default class CrowdPay extends Component {
     // match
     const {
       requestState,
+      FILTER_FRM,
+      fChange,
     } = this.props.bankAccountStore;
     return (
       <PrivateLayout
         {...this.props}
         P1={
-          <ByKeyword
-            change={this.executeSearch}
-            w={[8]}
-            placeholder="Search by Investor"
-            toggleSearch={this.toggleSearch}
-            requestState={requestState}
-            more="no"
-          />
+          <Aux>
+            <ByKeyword
+              change={this.executeSearch}
+              w={[8]}
+              placeholder="Search by Investor"
+              toggleSearch={this.toggleSearch}
+              requestState={requestState}
+              more="no"
+            />
+            <FormCheckbox
+              fielddata={FILTER_FRM.fields.locked}
+              name="locked"
+              changed={fChange}
+              defaults
+              containerclassname="ui list horizontal"
+            />
+          </Aux>
         }
       >
         <AllRequests {...this.props} />
