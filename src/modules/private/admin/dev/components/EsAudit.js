3502200fe95f439026e6d76c8a7d4588720cf9dd
vishakha Nimkar <vishakha.cuelogic@nextseed.com>
2019-05-28 19:44:08 +0530

    UI design

--- src/modules/private/admin/dev/components/EsAudit.js
+++ src/modules/private/admin/dev/components/EsAudit.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
-import { Modal, Header, Form, Button, Grid } from 'semantic-ui-react';
+import { Modal, Header, Form, Button, Grid, Divider, Card } from 'semantic-ui-react';
 import { FormInput } from '../../../../../theme/form';
 
 @inject('elasticSearchStore')
@@ -25,19 +25,52 @@ export default class EsAudit extends Component {
         </Modal.Header>
         <Modal.Content className="signup-content">
           <Form error onSubmit={this.onSubmit}>
-            <Button floated="right" primary size="large" className="very relaxed" content="Set new password" />
-            <FormInput
-              name="random"
-              fielddata={ES_AUDIT_FRM.fields.random}
-              changed={(e, result) => formChange(e, result, 'ES_AUDIT_FRM')}
-            />
             <Grid>
-              <Grid.Row columns={2}>
+              <Grid.Row className="mb-20">
+                <Grid.Column width={10}>
+                  <FormInput
+                    name="random"
+                    fielddata={ES_AUDIT_FRM.fields.random}
+                    changed={(e, result) => formChange(e, result, 'ES_AUDIT_FRM')}
+                  />
+                </Grid.Column>
+                <Grid.Column width={6} verticalAlign="middle">
+                  <Button primary content="Set new password" />
+                </Grid.Column>
+              </Grid.Row>
+              <Divider section />
+              <Grid.Row columns={2} divided className="mt-20">
                 <Grid.Column>
-                  <p>Mobile</p>
+                  <Header as="h4">
+                    Output:
+                    <Header.Subheader className="mt-10">Document Id: XYZ</Header.Subheader>
+                  </Header>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row columns={2} divided>
+                <Grid.Column>
+                  <Header as="h6">
+                    Count: 1 <span className="ml-10">1 hr ago</span>
+                  </Header>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.
+                  </p>
+                  <section className="bg-offwhite center-align">
+                    <h3 className="grey-header">No data found</h3>
+                  </section>
                 </Grid.Column>
                 <Grid.Column>
-                  <p>Tablet</p>
+                  <Header as="h6">
+                    Count: 1 <span className="ml-10">1 hr ago</span>
+                  </Header>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.
+                  </p>
+                  <section className="bg-offwhite center-align">
+                    <h3 className="grey-header">No data found</h3>
+                  </section>
                 </Grid.Column>
               </Grid.Row>
             </Grid>
