ebe37ed1dd81be2b5e72b619a75f573121709a62
Alan Lee <alan@nextseed.com>
2019-06-03 15:30:06 -0500

    updates pulled from dev/demo

--- src/services/stores/entities/dev/elasticSearchStore.js
+++ src/services/stores/entities/dev/elasticSearchStore.js
@@ -1,21 +1,33 @@
-import { observable, action } from 'mobx';
-import { get } from 'lodash';
+import { observable, action, computed, toJS } from 'mobx';
+import { get, sortBy } from 'lodash';
+import graphql from 'mobx-apollo';
 import * as elasticSearchQueries from '../../queries/elasticSearch';
 import { generateInvestorFolderStructure, storageDetailsForInvestor } from '../../queries/data';
 import { GqlClient as client } from '../../../../api/gqlApi';
 import Helper from '../../../../helper/utility';
 import { FormValidator as Validator } from '../../../../helper';
-import { STORAGE_DETAILS_SYNC, BULK_STORAGE_DETAILS_SYNC } from '../../../constants/admin/data';
+import { STORAGE_DETAILS_SYNC, BULK_STORAGE_DETAILS_SYNC, ES_AUDIT } from '../../../constants/admin/data';
 import uiStore from '../../../stores/entities/shared/uiStore';
 
 export class ElasticSearchStore {
   @observable STORAGE_DETAILS_SYNC_FRM = Validator.prepareFormObject(STORAGE_DETAILS_SYNC);
+  @observable ES_AUDIT_FRM = Validator.prepareFormObject(ES_AUDIT);
   @observable BULK_STORAGE_DETAILS_SYNC_FRM =
-  Validator.prepareFormObject(BULK_STORAGE_DETAILS_SYNC);
+    Validator.prepareFormObject(BULK_STORAGE_DETAILS_SYNC);
   @observable inProgress = {};
   @observable bulkSyncLoader = false;
   @observable boxMsg = '';
   @observable countValues = [];
+  @observable esAudit = null;
+  @observable esAuditOutput = null;
+  @observable swapIndex = null;
+  @observable mutations = {
+    USERS: ['userDeleteIndices', 'userPopulateIndex'],
+    CROWDPAY: ['crowdPayDeleteIndices', 'crowdPayPopulateIndex'],
+    ACCREDITATIONS: ['accreditationDeleteIndices', 'accreditationPopulateIndex'],
+    LINKEDBANK: ['linkedBankDeleteIndices', 'linkedBankPopulateIndex'],
+    OFFERINGS: ['offeringsDeleteIndices', 'offeringsPopulateIndex'],
+  }
 
   @action
   setFieldValue = (field, value) => {
@@ -174,9 +186,17 @@ export class ElasticSearchStore {
   }
 
   @action
+  formChange = (e, result, form) => {
+    this[form] = Validator.onChange(
+      this[form],
+      Validator.pullValues(e, result),
+    );
+  }
+
+  @action
   storageDetailsChange = (e, res) => {
     this.STORAGE_DETAILS_SYNC_FRM =
-    Validator.onChange(this.STORAGE_DETAILS_SYNC_FRM, Validator.pullValues(e, res));
+      Validator.onChange(this.STORAGE_DETAILS_SYNC_FRM, Validator.pullValues(e, res));
     this.setFieldValue('boxMsg', '');
   };
 
@@ -198,7 +218,7 @@ export class ElasticSearchStore {
       }
     } else {
       this[formName] =
-      Validator.onChange(this[formName], Validator.pullValues(field, values));
+        Validator.onChange(this[formName], Validator.pullValues(field, values));
     }
   };
 
@@ -228,27 +248,6 @@ export class ElasticSearchStore {
           Helper.toast('Something went wrong, please try again later.', 'error');
           this.setFieldValue('bulkSyncLoader', false);
           rej(error);
-        });
-    });
-  }
-
-  @action
-  elasticSearchHandler = (mutation) => {
-    this.setFieldValue('inProgress', mutation);
-    return new Promise((resolve, reject) => {
-      client
-        .mutate({
-          mutation: elasticSearchQueries[mutation],
-        })
-        .then((result) => {
-          Helper.toast('Your request is processed successfully.', 'success');
-          resolve(result);
-          this.setFieldValue('inProgress', false);
-        })
-        .catch((error) => {
-          Helper.toast('Something went wrong, please try again later.', 'error');
-          reject(error);
-          this.setFieldValue('inProgress', false);
         });
     });
   }
