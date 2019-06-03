4fbe930c02558c187b7e973e2345337c18bc5ce2
Swapnil bhosale <swapnil.cuelogic@nextseed.com>
2019-06-03 20:02:23 +0530

    cypress investor WIP

--- cypress/integration/investorSignUp/investor_account_spec.js
+++ cypress/integration/investorSignUp/investor_account_spec.js
@@ -26,6 +26,7 @@ describe('Account Creation', () => {
   });
 
   const manualLinkbankProcess = () => {
+    cy.reload();
     cy.get('input[name="accType"]').check('0', { force: true });
     cy.get('button.next').click();
     cy.wait(2000);
@@ -36,21 +37,21 @@ describe('Account Creation', () => {
     cy.get('button.button').contains('Confirm').click();
     cy.wait('@upsertInvestorAccount');
   };
-  const plaidProcess = (progressStep, frameCount) => {
+  const plaidProcess = (progressStep) => {
     cy.get(`.multistep-modal > ol.progtrckr > ${progressStep}`).click({ force: true }).invoke('text').then((step) => {
       cy.log('bank step', step.toUpperCase());
       if (step.toUpperCase() === 'LINK BANK') {
-        if (progressStep === '.progtrckr-done') {
-          cy.get('button.link-button').contains('Or link account directly').click();
-        }
+        // if (progressStep === '.progtrckr-done') {
+        //   cy.get('button.link-button').contains('Or link account directly').click();
+        // }
         cy.get('.bank-link:first').click({ force: true });
         cy.wait(5000);
-        cy.get(`#plaid-link-iframe-${frameCount}`).then(($iframe) => {
+        cy.get('#plaid-link-iframe-1').then(($iframe) => {
           const $body = $iframe.contents().find('body');
           cy.log('body', $iframe.contents().find('body'));
           cy.wait(500);
           let stripe = cy.wrap($body);
-          stripe.find('.Pane__actions > button').click({ force: true });
+          stripe.find('button').contains('Continue').click({ force: true });
           stripe = cy.wrap($body);
           stripe.find('input[name="username"]').type('user_good');
           stripe = cy.wrap($body);
@@ -72,6 +73,7 @@ describe('Account Creation', () => {
     cy.get('form').within(() => {
       cy.get('input[name="value"]').type(amount);
       cy.get('button').contains('Confirm').click();
+      cy.wait('@upsertInvestorAccount');
       cy.wait('@upsertInvestorAccount');
     });
   }
@@ -98,7 +100,7 @@ describe('Account Creation', () => {
           iraAccountCreation();
           break;
         case 'Link bank':
-          plaidProcess('.progtrckr-doing', '2');
+          plaidProcess('.progtrckr-doing');
           cy.wait(1000);
           addFunds('5010');
           cy.wait('@upsertInvestorAccount');
@@ -174,7 +176,7 @@ describe('Account Creation', () => {
           entityAccountCreation();
           break;
         case 'Link bank':
-          plaidProcess('.progtrckr-doing', '3');
+          plaidProcess('.progtrckr-doing');
           cy.wait(1000);
           addFunds('7000');
           cy.wait('@upsertInvestorAccount');
@@ -185,7 +187,7 @@ describe('Account Creation', () => {
           cy.get('div.content').get('button.button').contains('Submit for review').click({ force: true });
           cy.wait('@upsertInvestorAccount');
           cy.wait('@upsertInvestorAccount');
-          if (cy.get('div.mini').length) {
+          if (cy.get('div.mini').get('button.button').contains('Continue').length) {
             cy.get('div.mini').get('button.button').contains('Continue').click({ force: true });
           }
           cy.wait(1000);
@@ -199,24 +201,26 @@ describe('Account Creation', () => {
   });
 
   it('should successfully link bank with plaid process', () => {
-    plaidProcess('.progtrckr-done', '1');
+    plaidProcess('.progtrckr-done');
   });
 
   it('should create individual account successfully', () => {
+    cy.reload();
     addFunds('15000');
     cy.wait(5000);
-    registerApiCall('submitAccount');
     cy.get('div.content').get('button.button').contains('Create your account').click({ force: true });
-    cy.wait('@submitAccount');
-    cy.wait('@submitAccount');
-    cy.get('div.mini').get('button.button').contains('Continue').click({ force: true });
-    // if (cy.get('div.mini').length) {
-    // } else {
-    //   cy.wait(2000);
-    // }
+    cy.wait('@upsertInvestorAccount');
+    cy.wait('@upsertInvestorAccount');
+    if (cy.get('div.mini').length) {
+      cy.get('div.mini').get('button.button').contains('Continue').click({ force: true });
+      cy.wait('@upsertInvestorAccount');
+    } else {
+      cy.wait(2000);
+    }
   });
 
   it('should create IRA account successfully', () => {
+    cy.reload();
     cy.get('.btn-item').contains('Open New Account').click({ force: true });
     cy.get('input[name="accType"]').check('1', { force: true });
     cy.get('button.next').click();
@@ -224,6 +228,7 @@ describe('Account Creation', () => {
   });
 
   it('should create Entity account successfully', () => {
+    cy.reload();
     cy.get('.btn-item').contains('Open New Account').click({ force: true });
     cy.get('input[name="accType"]').check('2', { force: true });
     cy.get('button.next').click();
