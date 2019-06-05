a21351e61a98a470a493f978967a9d029961ad7c
Swapnil bhosale <swapnil.cuelogic@nextseed.com>
2019-06-05 18:54:09 +0530

    SRI disable for reviw env

--- cypress/support/investorSignup/investorFlow.js
+++ cypress/support/investorSignup/investorFlow.js
@@ -6,7 +6,7 @@ import { enterCodeAndConfirm } from '../common';
 import { completeInvestorProfile } from './InvestorProfile';
 
 export const InvestorFlowProcess = () => {
-  cy.visit('https://http://1702-cypress-test-framework-poc.s3-website-us-east-1.amazonaws.com/');
+  cy.visit('https://http://1702-cypress-test-framework-poc.s3-website-us-east-1.amazonaws.com/password-protected');
   cy.wait(10000)
   fillSignUpFormAndProceed();
   enterCodeAndConfirm();
