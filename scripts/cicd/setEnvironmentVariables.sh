1a0fd4fe7992a0756ad14eca7861eba689d42737
pratik <pratik.cuelogic@nextseed.com>
2019-06-07 12:33:47 +0530

    dev predev 01

--- scripts/cicd/setEnvironmentVariables.sh
+++ scripts/cicd/setEnvironmentVariables.sh
@@ -97,7 +97,7 @@ settingEnv()
     REACT_APP_PUBLIC_API_KEY=$(cat Env.txt | awk '/\/ns-client\/'$environment'\/api\/public\/key/ { print $3 }')
     sed -i.bak "s#^\(REACT_APP_PUBLIC_API_KEY=\).*#\1${REACT_APP_PUBLIC_API_KEY}#" .envTEMPLATE
 
-	if [ "$ci_commit_ref" = "develop" ] || [ "$ci_commit_ref" = "qa" ] || [ "$ci_commit_ref" = "demo" ] || [ "$ci_commit_ref" = "master" ]; then
+	if [ "$ci_commit_ref" = "dev" ] || [ "$ci_commit_ref" = "qa" ] || [ "$ci_commit_ref" = "demo" ] || [ "$ci_commit_ref" = "master" ]; then
 		REACT_APP_BUG_SNAG_KEY=$(cat Env.txt | awk '/\/ns-client\/'$environment'\/bugsnag\/apiKey/ { print $3 }')
 		sed -i.bak "s#^\(REACT_APP_BUG_SNAG_KEY=\).*#\1${REACT_APP_BUG_SNAG_KEY}#" .envTEMPLATE
 
