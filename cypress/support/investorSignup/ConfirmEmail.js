73da4033310aeb05bcf7f5b5d37844e645adde13
Swapnil bhosale <swapnil.cuelogic@nextseed.com>
2019-06-14 15:23:26 +0530

    cypress confirm email timeout glitches

--- cypress/support/investorSignup/ConfirmEmail.js
+++ cypress/support/investorSignup/ConfirmEmail.js
@@ -2,9 +2,10 @@ import { registerApiCall } from '../common';
 
 export const confirmEmailAddressScreen = () => {
   cy.wait(8000)
-  registerApiCall('confirmEmail1', '**/v1/p');
-  registerApiCall('confirmEmail2', '**/v1/i');
+  // registerApiCall('confirmEmail1', '**/v1/p');
+  // registerApiCall('confirmEmail2', '**/v1/i');
   cy.contains('Continue').click();
-  cy.wait('@confirmEmail1');
-  cy.wait('@confirmEmail2');
+  cy.wait(2000)
+  // cy.wait('@confirmEmail1');
+  // cy.wait('@confirmEmail2');
 }
