76af149c601bb2ed0e7b5584bcc0d94bdb8d666e
Swapnil bhosale <swapnil.cuelogic@nextseed.com>
2019-06-14 13:32:55 +0530

    timeout confirm email

--- cypress/support/investorSignup/ConfirmEmail.js
+++ cypress/support/investorSignup/ConfirmEmail.js
@@ -1,7 +1,7 @@
 import { registerApiCall } from '../common';
 
 export const confirmEmailAddressScreen = () => {
-  cy.wait(6000)
+  cy.wait(8000)
   registerApiCall('confirmEmail1', '**/v1/p');
   registerApiCall('confirmEmail2', '**/v1/i');
   cy.contains('Continue').click();
