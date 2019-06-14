c6b20df26b5de65657ee9afa5cb937db1476fb2a
Swapnil bhosale <swapnil.cuelogic@nextseed.com>
2019-06-14 21:05:05 +0530

    cypress wait added

--- cypress/support/investorSignup/ConfirmEmail.js
+++ cypress/support/investorSignup/ConfirmEmail.js
@@ -1,7 +1,7 @@
 import { registerApiCall } from '../common';
 
 export const confirmEmailAddressScreen = () => {
-  cy.wait(8000)
+  cy.wait(12000)
   // registerApiCall('confirmEmail1', '**/v1/p');
   // registerApiCall('confirmEmail2', '**/v1/i');
   cy.contains('Continue').click();
