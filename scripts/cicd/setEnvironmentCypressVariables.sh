d22881960aa26d30f7367c2f5037e91ab16a27dc
Chetan Bura <chetan.cuelogic@nextseed.com>
2019-07-01 19:11:10 +0530

    env varible exported

--- scripts/cicd/setEnvironmentCypressVariables.sh
+++ scripts/cicd/setEnvironmentCypressVariables.sh
@@ -18,3 +18,28 @@ done < "$file"
 }
 
 settingEnv
+
+
+# settingEnv()
+# {
+# file="../../.env.local"
+# rm cypress.env.json
+# echo "{" >> cypress.env.json
+# while IFS= read -r line
+# do
+# 	case $(echo $line | awk -F= '{print $1}') in
+# 	REACT_APP_AWS_REGION )
+# 		echo "\"CYPRESS_AWS_REGION"\": "\"$(echo $line | awk -F= '{print $2}')"\", >> cypress.env.json ;;
+# 	REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID)
+# 		echo "\"CYPRESS_AWS_COGNITO_IDENTITY_POOL_ID"\": "\"$(echo $line | awk -F= '{print $2}')"\", >> cypress.env.json ;;
+# 	REACT_APP_AWS_COGNITO_USER_POOL_ID)
+# 		echo "\"CYPRESS_AWS_COGNITO_USER_POOL_ID"\": "\"$(echo $line | awk -F= '{print $2}')"\", >> cypress.env.json ;;
+# 	REACT_APP_AWS_COGNITO_CLIENT_ID)
+# 		echo "\"CYPRESS_AWS_COGNITO_CLIENT_ID"\": "\"$(echo $line | awk -F= '{print $2}')"\", >> cypress.env.json ;;
+# 	esac
+# done < "$file"
+
+# echo "}" >> cypress.env.json
+# }
+
+# settingEnv
