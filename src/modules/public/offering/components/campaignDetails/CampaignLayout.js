0a0363953fddcb18f4536f64c7108a7382fa5309
chetan B <chetan.cuelogic@nextseed.com>
2019-07-29 18:54:40 +0530

    1972 [enhacemnet] added scrolling to mobile nav

--- src/modules/public/offering/components/campaignDetails/CampaignLayout.js
+++ src/modules/public/offering/components/campaignDetails/CampaignLayout.js
@@ -23,6 +23,8 @@ const isTabletLand = document.documentElement.clientWidth >= 992
   && document.documentElement.clientWidth < 1200;
 const topsAsPerWindowheight = window.innerHeight > 1000 ? 500 : 150;
 const isTablet = document.documentElement.clientWidth < 992;
+const isMobile = document.documentElement.clientWidth < 768;
+
 @inject('campaignStore', 'navStore')
 @observer
 class CampaignLayout extends Component {
@@ -61,6 +63,9 @@ class CampaignLayout extends Component {
           && document.getElementById(item.to.slice(1)).getBoundingClientRect().top < topsAsPerWindowheight
           && document.getElementById(item.to.slice(1)).getBoundingClientRect().top > -1) {
           this.props.navStore.setFieldValue('currentActiveHash', item.to);
+          if (isMobile) {
+            document.getElementsByClassName('campaign-mobile-menu-v2')[0].getElementsByClassName('active')[0].scrollIntoView();
+          }
         }
       });
     }
@@ -140,6 +145,7 @@ class CampaignLayout extends Component {
         <>
           {campaignStatus.isRevenueShare ? (<RevenueSharingSummary {...this.props} />) : (<TotalPaymentCalculator {...this.props} />)
           }
+            <Divider hidden section />
           <Comments newLayout showOnlyOne={!this.state.expandComments} />
           <Button fluid={isTablet} onClick={() => this.handleCollapseExpand('expandComments')} className="link-button highlight-text mt-40">
             {this.state.expandUpdate ? 'Collapse' : 'Expand'} All Comments
