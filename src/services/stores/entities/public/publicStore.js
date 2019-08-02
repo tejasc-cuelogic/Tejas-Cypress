4b879a074ca9dffeef03a0e68bcbb1356d9c2a02
Chetan Bura <chetan.cuelogic@nextseed.com>
2019-08-02 16:28:14 +0530

    2036[enhacement] removed unwanted code

--- src/services/stores/entities/public/publicStore.js
+++ src/services/stores/entities/public/publicStore.js
@@ -6,13 +6,6 @@ import { GqlClient as client } from '../../../../api/publicApi';
 export class PublicStore {
   @observable jobListData = {};
 
-  @observable details = 'This is just a hello world page details!';
-
-  @action
-  initRequest = () => {
-    this.data = [{ id: 1, title: 'this is a title', createdAt: '03/03/2018' }];
-  }
-
   @action
   getJobListing = () => new Promise((resolve, reject) => {
     this.jobListData = graphql({
