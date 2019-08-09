f5df672a6d07372c641ea2f34be30cfffe533a40
Swapnil bhosale <swapnil.cuelogic@nextseed.com>
2019-08-09 16:26:46 +0530

    2048 [Enhancement] - fail cased added

--- src/services/stores/entities/user/identityStore.js
+++ src/services/stores/entities/user/identityStore.js
@@ -317,6 +317,7 @@ export class IdentityStore {
         })
         .then((data) => {
           // eslint-disable-next-line no-unused-expressions
+          sasaddsdsd;
           this.setVerifyIdentityResponse(data.data.verifyCIPIdentity);
           // TODO optimize signUpLoading call
           if (data.data.verifyCIPIdentity.passId
