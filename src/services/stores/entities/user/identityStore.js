1e68f36ff2123a31557a320d3990ba2d565a9c24
Swapnil bhosale <swapnil.cuelogic@nextseed.com>
2019-08-09 19:40:51 +0530

    2048 [Enhancement] - Error removed

--- src/services/stores/entities/user/identityStore.js
+++ src/services/stores/entities/user/identityStore.js
@@ -317,7 +317,6 @@ export class IdentityStore {
         })
         .then((data) => {
           // eslint-disable-next-line no-unused-expressions
-          sasaddsdsd;
           this.setVerifyIdentityResponse(data.data.verifyCIPIdentity);
           // TODO optimize signUpLoading call
           if (data.data.verifyCIPIdentity.passId
