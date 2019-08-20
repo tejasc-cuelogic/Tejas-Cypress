7cca18f058e3c0522ed519189fe3a7d221b5ba0b
Vaibhav Pathak <vaibhav.cuelogic@nextseed.com>
2019-08-20 13:49:45 +0530

    2068 [ResolveIssue] resolve issue for accepting accountype during sync accreditation data

--- src/modules/private/admin/dev/components/EsAudit.js
+++ src/modules/private/admin/dev/components/EsAudit.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
-import { capitalize, get } from 'lodash';
+import { capitalize, get, map, isArray } from 'lodash';
 import moment from 'moment';
 import beautify from 'json-beautify';
 import ReactDiffViewer from 'react-diff-viewer';
@@ -15,7 +15,6 @@ export default class EsAudit extends Component {
     const { auditAlias } = this.props.match.params;
     this.props.elasticSearchStore.getESAuditPara(auditAlias);
     this.props.elasticSearchStore.resetESForm();
-    console.log('to test');
   }
 
   onSubmit = () => {
@@ -40,7 +39,8 @@ export default class EsAudit extends Component {
     } = this.props.elasticSearchStore;
     const { auditAlias } = this.props.match.params;
     const userIdExsist = get(esAuditParaOutput, 'index_a.record.userId') ? get(esAuditParaOutput, 'index_a.record.userId') : get(esAuditParaOutput, 'index_b.record.userId') ? get(esAuditParaOutput, 'index_b.record.userId') : '';
-    const accountTypeExsist = get(esAuditParaOutput, 'index_a.record.accountType') ? get(esAuditParaOutput, 'index_a.record.accountType') : get(esAuditParaOutput, 'index_b.record.userId') ? get(esAuditParaOutput, 'index_b.record.userId') : '';
+    const accountTypeObtained = get(esAuditParaOutput, 'index_a.record.accountType') ? get(esAuditParaOutput, 'index_a.record.accountType') : get(esAuditParaOutput, 'index_b.record.userId') ? get(esAuditParaOutput, 'index_b.record.userId') : '';
+    const accountTypeExsist = isArray(accountTypeObtained) && accountTypeObtained.length > 0 ? map(accountTypeObtained, val => val.toUpperCase()) : [];
     return (
       <Modal open closeIcon onClose={this.handleCloseModal} size="large" closeOnDimmerClick={false}>
         <Modal.Header className="center-align signup-header">
@@ -50,49 +50,49 @@ export default class EsAudit extends Component {
           {esAuditParaOutputLoading
             ? <InlineLoader />
             : (
-<Form error>
-              <Form.Group className="bottom-aligned mb-40">
-                <FormInput
-                  containerwidth={10}
-                  name="random"
-                  fielddata={ES_AUDIT_FRM.fields.random}
-                  changed={(e, result) => formChange(e, result, 'ES_AUDIT_FRM')}
-                />
-                <Form.Field width={6}>
-                  <Button type="button" primary onClick={this.onSubmit} content="Submit" />
-                  <Button type="button" primary loading={inProgress === get(esAuditParaOutput, 'index_a.indexName')} onClick={() => this.handleSync({ documentId: ES_AUDIT_FRM.fields.random.value || '', targetIndex: get(esAuditParaOutput, 'index_a.indexName') || '', indexAliasName: get(esAuditParaOutput, 'alias') || '', userId: userIdExsist, accountType: accountTypeExsist })} content="Sync a" />
-                  <Button type="button" primary loading={inProgress === get(esAuditParaOutput, 'index_b.indexName')} onClick={() => this.handleSync({ documentId: ES_AUDIT_FRM.fields.random.value || '', targetIndex: get(esAuditParaOutput, 'index_b.indexName') || '', indexAliasName: get(esAuditParaOutput, 'alias') || '', userId: userIdExsist, accountType: accountTypeExsist })} content="Sync b" />
-                </Form.Field>
-              </Form.Group>
-              <Grid>
-                <Grid.Row columns={2}>
-                  <Grid.Column>
-                    <Header as="h6">
-                      {this.renderTitle(get(esAuditParaOutput, 'index_a.indexName') || '')} : (Count: {get(esAuditParaOutput, 'index_a.count') || 0} <span className="ml-10">{get(esAuditParaOutput, 'index_a.created.date') ? moment(get(esAuditParaOutput, 'index_a.created.date')).fromNow() : ''}</span>)
+              <Form error>
+                <Form.Group className="bottom-aligned mb-40">
+                  <FormInput
+                    containerwidth={10}
+                    name="random"
+                    fielddata={ES_AUDIT_FRM.fields.random}
+                    changed={(e, result) => formChange(e, result, 'ES_AUDIT_FRM')}
+                  />
+                  <Form.Field width={6}>
+                    <Button type="button" primary onClick={this.onSubmit} content="Submit" />
+                    <Button type="button" primary loading={inProgress === get(esAuditParaOutput, 'index_a.indexName')} onClick={() => this.handleSync({ documentId: ES_AUDIT_FRM.fields.random.value || '', targetIndex: get(esAuditParaOutput, 'index_a.indexName') || '', indexAliasName: get(esAuditParaOutput, 'alias') || '', userId: userIdExsist, accountType: accountTypeExsist })} content="Sync a" />
+                    <Button type="button" primary loading={inProgress === get(esAuditParaOutput, 'index_b.indexName')} onClick={() => this.handleSync({ documentId: ES_AUDIT_FRM.fields.random.value || '', targetIndex: get(esAuditParaOutput, 'index_b.indexName') || '', indexAliasName: get(esAuditParaOutput, 'alias') || '', userId: userIdExsist, accountType: accountTypeExsist })} content="Sync b" />
+                  </Form.Field>
+                </Form.Group>
+                <Grid>
+                  <Grid.Row columns={2}>
+                    <Grid.Column>
+                      <Header as="h6">
+                        {this.renderTitle(get(esAuditParaOutput, 'index_a.indexName') || '')} : (Count: {get(esAuditParaOutput, 'index_a.count') || 0} <span className="ml-10">{get(esAuditParaOutput, 'index_a.created.date') ? moment(get(esAuditParaOutput, 'index_a.created.date')).fromNow() : ''}</span>)
                     </Header>
-                    {!get(esAuditParaOutput, 'index_a.record') && !get(esAuditParaOutput, 'index_b.record')
-                    && <InlineLoader text="No Data Found" />
-                    }
-                  </Grid.Column>
-                  <Grid.Column>
-                    <Header as="h6">
-                      {this.renderTitle(get(esAuditParaOutput, 'index_b.indexName') || '')} : (Count: {get(esAuditParaOutput, 'index_b.count') || 0} <span className="ml-10">{get(esAuditParaOutput, 'index_b.created.date') ? moment(get(esAuditParaOutput, 'index_b.created.date')).fromNow() : ''}</span>)
+                      {!get(esAuditParaOutput, 'index_a.record') && !get(esAuditParaOutput, 'index_b.record')
+                        && <InlineLoader text="No Data Found" />
+                      }
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Header as="h6">
+                        {this.renderTitle(get(esAuditParaOutput, 'index_b.indexName') || '')} : (Count: {get(esAuditParaOutput, 'index_b.count') || 0} <span className="ml-10">{get(esAuditParaOutput, 'index_b.created.date') ? moment(get(esAuditParaOutput, 'index_b.created.date')).fromNow() : ''}</span>)
                     </Header>
-                    {!get(esAuditParaOutput, 'index_a.record') && !get(esAuditParaOutput, 'index_b.record')
-                    && <InlineLoader text="No Data Found" />
-                    }
-                  </Grid.Column>
-                </Grid.Row>
-                {(get(esAuditParaOutput, 'index_a.record') || get(esAuditParaOutput, 'index_b.record'))
-                && (
-<ReactDiffViewer
-  oldValue={beautify(get(esAuditParaOutput, 'index_a.record') || '', null, 2)}
-  newValue={beautify(get(esAuditParaOutput, 'index_b.record') || '', null, 2)}
-  splitView
-/>
-                )}
-              </Grid>
-            </Form>
+                      {!get(esAuditParaOutput, 'index_a.record') && !get(esAuditParaOutput, 'index_b.record')
+                        && <InlineLoader text="No Data Found" />
+                      }
+                    </Grid.Column>
+                  </Grid.Row>
+                  {(get(esAuditParaOutput, 'index_a.record') || get(esAuditParaOutput, 'index_b.record'))
+                    && (
+                      <ReactDiffViewer
+                        oldValue={beautify(get(esAuditParaOutput, 'index_a.record') || '', null, 2)}
+                        newValue={beautify(get(esAuditParaOutput, 'index_b.record') || '', null, 2)}
+                        splitView
+                      />
+                    )}
+                </Grid>
+              </Form>
             )
           }
         </Modal.Content>
