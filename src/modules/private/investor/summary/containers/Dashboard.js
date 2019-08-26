3ff6083c074c1becc434deef0572b9896ead16b1
Pratik Tayade <pratik.cuelogic@nextseed.com>
2019-08-26 15:17:24 +0530

    1902 [Enhancement] - removed componentWillMount

--- src/modules/private/investor/summary/containers/Dashboard.js
+++ src/modules/private/investor/summary/containers/Dashboard.js
@@ -54,7 +54,8 @@ const summaryDetails = ({
 @inject('userDetailsStore', 'portfolioStore')
 @observer
 export default class Dashboard extends Component {
-  componentWillMount() {
+  constructor(props) {
+    super(props);
     // TODO change to regex
     if (!Helper.matchRegexWithUrl([/\baccount-creation(?![-])\b/])) {
       this.props.portfolioStore.getSummary();
