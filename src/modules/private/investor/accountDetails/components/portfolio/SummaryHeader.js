bd4cf328923b29d97588caa8797b1550a815e181
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2019-09-03 15:17:42 +0530

    2106 [bugs] - solved all feedback points

--- src/modules/private/investor/accountDetails/components/portfolio/SummaryHeader.js
+++ src/modules/private/investor/accountDetails/components/portfolio/SummaryHeader.js
@@ -37,70 +37,41 @@ const SummaryHeader = props => (
       {props.details.title !== false && !props.details.businessName
         && <SummaryTitle {...props} />
       }
-      {/* {isMobile
-        ? (
-        <Table basic>
-          {
-            props.details.summary.map(row => (
-            <Table.row>
-              <Table.Cell>
-              {row.title}
-              {row.info
-                && (
-                <Popup
-                  trigger={<Icon className="ns-help-circle" />}
-                  content={row.info}
-                  position="top center"
-                  wide
-                  hoverable
-                />
-                )
-              }
-              </Table.Cell>
-              <Table.Cell>
-                {showValue(row)}
-              </Table.Cell>
-            </Table.row>
-            ))
-          }
-        </Table>
-        ) : ( */}
-        <Grid stackable doubling celled columns={props.cols || props.details.summary.length} className="custom-divided">
-          {
-            props.details.summary.map(row => (
-              <Grid.Column key={snakeCase(row.title)}>
-                <Card.Content>
-                  <Statistic size="mini" horizontal={isMobile} className={row.status}>
-                    <Statistic.Label>
-                      <div>
-                        {row.title}
-                        {row.info
-                          && (
-                          <Popup
-                            trigger={<Icon className="ns-help-circle" />}
-                            content={row.info}
-                            position="top center"
-                            wide
-                            hoverable
-                          />
-                          )
-                        }
-                      </div>
-                      {row.title === 'Total Balance'
-                        && isMobile && <Link to={`/app/account-details/${props.details.accountType}/transfer-funds/add`}><b>Deposit funds</b></Link>
+      <Grid relaxed={!isMobile} stackable doubling celled columns={props.cols || props.details.summary.length} className="custom-divided">
+        {
+          props.details.summary.map(row => (
+            <Grid.Column key={snakeCase(row.title)}>
+              <Card.Content>
+                <Statistic size="mini" horizontal={isMobile} className={row.status}>
+                  <Statistic.Label>
+                    <div>
+                      {row.title}
+                      {row.info
+                        && (
+                        <Popup
+                          trigger={<Icon className="ns-help-circle" />}
+                          content={row.info}
+                          position="top center"
+                          wide
+                          hoverable
+                        />
+                        )
                       }
-                    </Statistic.Label>
-                    <Statistic.Value>{showValue(row)}</Statistic.Value>
+                    </div>
                     {row.title === 'Total Balance'
-                      && !isMobile && <Statistic.Label as={Link} className={props.details.isAccountFrozen ? 'disabled' : ''} to={`/app/account-details/${props.details.accountType}/transfer-funds/add`}>Deposit funds</Statistic.Label>
+                      && isMobile && <Link to={`/app/account-details/${props.details.accountType}/transfer-funds/add`}>Deposit funds</Link>
                     }
-                  </Statistic>
-                </Card.Content>
-              </Grid.Column>
-            ))
-          }
-        </Grid>
-        {/* )} */}
+                  </Statistic.Label>
+                  <Statistic.Value>{showValue(row)}</Statistic.Value>
+                </Statistic>
+              </Card.Content>
+              {row.title === 'Total Balance'
+                && !isMobile && <Link className={`${props.details.isAccountFrozen ? 'disabled' : ''} mt-14 ml-18 display-block`} to={`/app/account-details/${props.details.accountType}/transfer-funds/add`}>Deposit funds</Link>
+              }
+            </Grid.Column>
+          ))
+        }
+      </Grid>
     </Card>
   </>
 );
