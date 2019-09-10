172aed4ed289729f5c803819b5842dfb97015411
pankaj.cuelogic <pankaj.cuelogic@nextseed.com>
2019-09-10 17:46:24 +0530

    2057 [Enhancement] Adding missed else

--- src/services/stores/entities/userStore.js
+++ src/services/stores/entities/userStore.js
@@ -180,7 +180,7 @@ export class UserStore {
       data.header = 'You currently have funds remaining in your account';
       data.message = (<p className="mb-20">In order to delete your account, please withdraw all funds and allow 5-7 business days to clear prior to deleting your account. If you have any questions or need assistance, please email us at <a href="mailto:support@nextseed.com">support@nextseed.com</a>.</p>);
       data.isCancelDelete = false;
-    } if (!get(deletedUserMeta, 'validAgreement') && get(deletedUserMeta, 'availableBalance') <= 0 && get(deletedUserMeta, 'totalBalance') === 0) {
+    } else if (!get(deletedUserMeta, 'validAgreement') && get(deletedUserMeta, 'availableBalance') <= 0 && get(deletedUserMeta, 'totalBalance') === 0) {
       data.header = 'Are you sure?';
       data.message = (<><p className="mb-20">We hate to see you go, but if you would like to delete your NextSeed account please confirm your intent by entering the email address associated with your account.</p><p className="mb-20">Please note that any promotional credits you may have <br /> accumulated in your account will be forfeited.</p></>);
       data.isValidForDelete = true;
