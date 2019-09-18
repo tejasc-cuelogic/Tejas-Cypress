6241c09f4d37311ec05d59e68fc2b64b8d47a30c
pankaj.cuelogic <pankaj.cuelogic@nextseed.com>
2019-09-18 13:37:53 +0530

    2136 [Enhancement] Updated header as per action

--- src/modules/private/admin/applications/components/StatusChangeAppModal.js
+++ src/modules/private/admin/applications/components/StatusChangeAppModal.js
@@ -134,7 +134,10 @@ export default class StatusChangeAppModal extends Component {
     return (
       <Modal closeOnEscape={false} closeOnDimmerClick={false} size="mini" open closeIcon onClose={this.handleCloseModal} closeOnRootNodeClick={false}>
         <Modal.Header className="center-align signup-header">
-          <Header as="h3">{params.action === 'REMOVED' ? 'Remove' : capitalize(params.action)} Application?</Header>
+          {params.action === 'PROMOTE' && params.id !== 'in-progress'
+            ? <Header as="h3">Promote PreQual?</Header>
+            : <Header as="h3">{params.action === 'REMOVED' ? 'Remove' : capitalize(params.action)} Application?</Header>
+          }
         </Modal.Header>
         <Modal.Content className="signup-content">
           <Form error>
