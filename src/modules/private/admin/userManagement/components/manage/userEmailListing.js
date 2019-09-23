176de1213afda85a86e38742908efb33a2e1c0b8
Vaibhav Pathak <vaibhav.cuelogic@nextseed.com>
2019-09-23 23:07:33 +0530

    2133 [Enhancment] update UI as requested for listing

--- src/modules/private/admin/userManagement/components/manage/userEmailListing.js
+++ src/modules/private/admin/userManagement/components/manage/userEmailListing.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import { Link } from 'react-router-dom';
-import { Container, Header, Table, Card, Visibility, Item, Modal, Button } from 'semantic-ui-react';
-import { DateTimeFormat, InlineLoader } from '../../../../../../theme/shared';
+import { Container, Header, Table, Card, Visibility, Item, Modal, Button, Icon, Popup } from 'semantic-ui-react';
+import { InlineLoader } from '../../../../../../theme/shared';
 import { DataFormatter } from '../../../../../../helper';
 
 @inject('userDetailsStore')
@@ -58,13 +58,10 @@ export default class UserEmailList extends Component {
                   <Table unstackable striped className="user-list email-list">
                     <Table.Header>
                       <Table.Row>
+                        <Table.HeaderCell>Send Date</Table.HeaderCell>
                         <Table.HeaderCell>From Details</Table.HeaderCell>
                         <Table.HeaderCell>To Details</Table.HeaderCell>
                         <Table.HeaderCell>Subject</Table.HeaderCell>
-                        <Table.HeaderCell>Send Date</Table.HeaderCell>
-                        <Table.HeaderCell>Mergevars</Table.HeaderCell>
-                        <Table.HeaderCell>Attachements</Table.HeaderCell>
-                        <Table.HeaderCell>Body</Table.HeaderCell>
                       </Table.Row>
                     </Table.Header>
                     <Visibility
@@ -76,6 +73,25 @@ export default class UserEmailList extends Component {
                       ) : (
                         userEmals && userEmals.map((resp, idx) => (
                             <Table.Row>
+                              <Table.Cell>
+                                <Popup
+                                  trigger={(<span>{DataFormatter.getDateAsPerTimeZone(resp.requestDate, true, false, false)}</span>)}
+                                  content={(
+                                    <Item>
+                                      {(resp.mergeVars === null) || (resp.mergeVars && resp.mergeVars.length <= 0) ? 'NA'
+                                        : resp.mergeVars.map(mergDetails => (
+                                          <Item.Content>
+                                            <b>Name: </b>{mergDetails.name ? mergDetails.name : 'N/A'}
+                                            <br />
+                                            <b>Content: </b>{mergDetails.content ? mergDetails.content : 'N/A'}
+                                          </Item.Content>
+                                        ))
+                                      }
+                                    </Item>
+                                  )}
+                                  hoverable
+                                />
+                              </Table.Cell>
                               <Table.Cell className="user-status">
                                 <span className="user-name"><b>{resp.fromName ? resp.fromName : ''}</b></span>
                                 {resp.fromEmail ? resp.fromEmail : ''}
@@ -84,35 +100,27 @@ export default class UserEmailList extends Component {
                                 <span className="user-name"><b>{resp.toFirstName ? resp.toFirstName : ''}</b></span>
                                 {resp.toEmail ? resp.toEmail : ''}
                               </Table.Cell>
-                              <Table.Cell collapsing>{resp.subject}</Table.Cell>
-                              <Table.Cell>
-                                <DateTimeFormat isCSTFormat datetime={DataFormatter.getDateAsPerTimeZone(resp.requestDate, true, false, false)} />
+                              <Table.Cell collapsing>
+                                {resp.subject} <span><Link onClick={e => this.handleModel(e, idx)} to="/"> (Body)</Link></span> {' '}
+                                {((resp.attachments === null) || (resp.attachments && resp.attachments.length <= 0)) && (
+<Popup
+  trigger={<Icon className="ns-attachment" color="blue" size="large" />}
+  content={(
+                                    <Item>
+                                      {(resp.attachments === null) || (resp.attachments && resp.attachments.length <= 0) ? 'NA'
+                                        : resp.attachments.map(attach => (
+                                          <Item.Content>
+                                            <b>Name: </b>{attach.name ? attach.name : 'N/A'}
+                                          </Item.Content>
+                                        ))
+                                      }
+                                    </Item>
+                                  )}
+  hoverable
+  position="top center"
+/>
+                                )}
                               </Table.Cell>
-                              <Table.Cell>
-                                <Item>
-                                  {(resp.mergeVars === null) || (resp.mergeVars && resp.mergeVars.length <= 0) ? 'NA'
-                                    : resp.mergeVars.map(mergDetails => (
-                                      <Item.Content>
-                                        <b>Name: </b>{mergDetails.name ? mergDetails.name : 'N/A'}
-                                        <br />
-                                        <b>Content: </b>{mergDetails.content ? mergDetails.content : 'N/A'}
-                                      </Item.Content>
-                                    ))
-                                  }
-                                </Item>
-                              </Table.Cell>
-                              <Table.Cell>
-                                <Item>
-                                  {(resp.attachments === null) || (resp.attachments && resp.attachments.length <= 0) ? 'NA'
-                                    : resp.attachments.map(attach => (
-                                      <Item.Content>
-                                        <b>Content: </b>{attach.content ? attach.content : 'N/A'}
-                                      </Item.Content>
-                                    ))
-                                  }
-                                </Item>
-                              </Table.Cell>
-                              <Table.Cell collapsing><Link onClick={e => this.handleModel(e, idx)} to="/">Show Email Content</Link></Table.Cell>
                             </Table.Row>
                         ))
                       )
