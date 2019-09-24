d84b8762bee8e1179be4b4a4c02b4c891aea8715
pankaj.cuelogic <pankaj.cuelogic@nextseed.com>
2019-09-24 18:42:23 +0530

    2145 [Enhancement] repayments update

--- src/constants/NavigationMeta.js
+++ src/constants/NavigationMeta.js
@@ -415,11 +415,16 @@ export const PRIVATE_NAV = [
   },
   {
     icon: 'money',
-    title: 'Repayments',
+    title: 'Payments',
     capability: 'REPAYMENTS_ANY',
     to: 'repayments',
     path: 'admin/repayments',
     accessibleTo: ['admin', 'manager', 'support'],
+    subNavigations: [
+      { title: 'Issuers', to: 'issuers' },
+      { title: 'Batches', to: 'batches' },
+      { title: 'Util', to: 'util' },
+    ],
   },
   {
     icon: 'dollar',
