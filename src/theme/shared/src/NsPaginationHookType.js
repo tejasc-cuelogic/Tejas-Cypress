5aa05d24908ccb346a1bebc758603e3c1573ebbd
Vaibhav Pathak <vaibhav.cuelogic@nextseed.com>
2019-09-24 21:19:30 +0530

    2132 [Feature] implment pagination using functional component

--- src/theme/shared/src/NsPaginationHookType.js
+++ src/theme/shared/src/NsPaginationHookType.js
@@ -6,19 +6,6 @@ function NsPaginationHookType(props) {
   const [lek, setLek] = useState(props.meta.requestState.lek);
   const [first] = useState(props.meta.requestState.perPage || 10);
   const [currentPageNo, setCurrentPageNo] = useState(props.meta.requestState.page || 1);
-  // const [stateOptions] = useState([5, 10, 15].map(n => ({ key: n, value: n, text: n })));
-
-  // function pageChangeHandler(e) {
-  //   setCurrentPageNo(parseInt(e.target.value, 10));
-  // }
-
-  // function changeRecordsPerPage(e, result) {
-  //   const firstVal = result.value;
-  //   const currentPageNoVal = 1;
-  //   setFirst(firstVal);
-  //   setCurrentPageNo(currentPageNoVal);
-  //   props.initRequest({ first: firstVal, page: currentPageNoVal });
-  // }
 
   function goToPage(currentPageNoVal) {
     setCurrentPageNo(currentPageNoVal);
@@ -29,15 +16,6 @@ function NsPaginationHookType(props) {
   }, [props.meta.requestState.lek]);
   return (
     <Menu pagination text {...props}>
-      {/* {recPerPage
-        && (
-          <Select
-            value={first}
-            options={stateOptions}
-            onChange={changeRecordsPerPage()}
-          />
-        )
-      } */}
       <Menu.Item
         icon
         onClick={() => goToPage(currentPageNo - 1)}
