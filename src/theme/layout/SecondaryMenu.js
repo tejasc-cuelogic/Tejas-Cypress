bd930714c6c771c08bd8e64f2824290875a686e7
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2019-10-04 12:59:46 +0530

    2122 - More menu UI

--- src/theme/layout/SecondaryMenu.js
+++ src/theme/layout/SecondaryMenu.js
@@ -79,6 +79,11 @@ class SecondaryMenu extends Component {
       noinvert, attached, className, stepsStatus, addon, heading,
       force2ary, navCustomClick,
     } = this.props;
+    const options = [
+      { key: 1, text: 'Choice 1', value: 1 },
+      { key: 2, text: 'Choice 2', value: 2 },
+      { key: 3, text: 'Choice 3', value: 3 },
+    ];
     // const mobNavItems = map(navItemList, i => mapKeys(i, (v, k) => iMap[k] || k));
     return (
       <>
@@ -105,6 +110,7 @@ class SecondaryMenu extends Component {
               stepsStatus={stepsStatus}
               navCustomClick={navCustomClick}
             />
+            <Dropdown text="More..." options={options} className="more-items link item" />
             {this.props.rightLabel}
           </Menu>
         </Responsive>
