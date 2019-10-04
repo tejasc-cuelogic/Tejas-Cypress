4862e9db7889f7546d2d2a2662959551fee9e3f6
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2019-10-04 18:23:48 +0530

    1880 [enhancement] - UI issue

--- src/modules/private/investor/accountSetup/containers/accountCreation/ira/FinancialInformation.js
+++ src/modules/private/investor/accountSetup/containers/accountCreation/ira/FinancialInformation.js
@@ -62,6 +62,7 @@ Your net worth and annual income are used to determine your 12-month investment 
                 </span>
               </b>
             </p>
+            <a target="_blank" rel="noopener noreferrer" href={`${window.location.origin}/resources/education-center/investor/investment-limit-calcuator/`} className={`${isMobile ? 'mt-20 mb-20' : ''} link display-block`}>How is this calculated?</a>
           </div>
           {(FIN_INFO_FRM.fields.investmentLimit.value < 5000
             && FIN_INFO_FRM.fields.investmentLimit.value !== '')
@@ -73,7 +74,6 @@ Your net worth and annual income are used to determine your 12-month investment 
             </p>
             )
           }
-          <a target="_blank" rel="noopener noreferrer" href={`${window.location.origin}/resources/education-center/investor/investment-limit-calcuator/`} className={`${isMobile ? 'mt-20 mb-20' : ''} link display-block`}>How is this calculated?</a>
           {isMobile && (
               <Button fluid primary className="relaxed" content="Continue" disabled={!FIN_INFO_FRM.meta.isValid} onClick={this.handleContinueButton} />
           )
