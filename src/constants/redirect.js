98b1a44d1293a9ba5d84d32a9a3f595d514d2dc8
Chetan Bura <chetan.cuelogic@nextseed.com>
2019-10-09 13:31:23 +0530

    2170 [Feature] updated the redirect.js for redirecting to given url for rs code

--- src/constants/redirect.js
+++ src/constants/redirect.js
@@ -14,14 +14,14 @@ const COMMON = [
 ];
 export const PRODUCTION_REDIRECT_META = [
   ...COMMON,
-  { from: 'rh50', live: true, to: '/invest?utm_campaign=saasquatch&rsCode=RH50' },
+  { from: 'rh50', live: true, to: '/invest?utm_campaign=saasquatch&rsCode=RH50', rsCode: 'RH50', rsRedirect: '/offerings/railway-heights' },
   { from: 'get20', live: true, to: '/invest/why-nextseed?utm_source=invite&utm_medium=link&utm_campaign=saasquatch&rsCode=GET20&rsShareMedium=UNKNOWN&rsEngagementMedium=UNKNOWN' },
   { from: 'greenway', live: true, to: '/invest/why-nextseed?utm_source=invite&utm_medium=link&utm_campaign=saasquatch&rsCode=GWAY&rsShareMedium=UNKNOWN&rsEngagementMedium=UNKNOWN' },
 ];
 
 export const DEV_REDIRECT_META = [
   ...COMMON,
-  { from: 'batmansucks', live: true, to: '/invest/why-nextseed?utm_source=invite&utm_medium=link&utm_campaign=saasquatch&rsCode=BATMANSUCKS&rsShareMedium=UNKNOWN&rsEngagementMedium=UNKNOWN' },
+  { from: 'batmansucks', live: true, to: '/invest/why-nextseed?utm_source=invite&utm_medium=link&utm_campaign=saasquatch&rsCode=BATMANSUCKS&rsShareMedium=UNKNOWN&rsEngagementMedium=UNKNOWN', rsCode: 'BATMANSUCKS', rsRedirect: '/offerings/railway-heights' },
 ];
 
 export const REDIRECT_META = ['production', 'prod', 'master'].includes(process.env.REACT_APP_DEPLOY_ENV) ? PRODUCTION_REDIRECT_META : DEV_REDIRECT_META;
