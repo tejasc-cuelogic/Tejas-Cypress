c03b4728195def0629c9d48b206ef7df538dda0e
pankaj.cuelogic <pankaj.cuelogic@nextseed.com>
2019-10-22 18:14:12 +0530

    2189 [Enhancement] Code optimization

--- src/modules/shared/businessApplication/components/prequlification/Experience.js
+++ src/modules/shared/businessApplication/components/prequlification/Experience.js
@@ -1,54 +1,49 @@
 import React from 'react';
 import { MaskedInput } from '../../../../../theme/form';
 
-const Experience = props => (
-  <>
-    <MaskedInput
-      maxlength="2"
-      containerclassname={props.preQualFormDisabled ? 'display-only' : ''}
-      readOnly={props.preQualFormDisabled}
-      key="industryExperience"
-      name="industryExperience"
-      number
-      asterisk="true"
-      tooltip={props.fields.industryExperience.tooltip}
-      value={props.fields.industryExperience.value}
-      label={props.currentApplicationType === 'commercial-real-estate' ? 'How many years of related industry experience does your team have?' : 'How many years of directly relevant industry experience does your team have?'}
-      fielddata={props.fields.industryExperience}
-      changed={props.businessAppEleMaskChange}
-    />
-    <MaskedInput
-      maxlength="3"
-      containerclassname={props.preQualFormDisabled ? 'display-only' : ''}
-      readOnly={props.preQualFormDisabled}
-      key="estimatedCreditScore"
-      name="estimatedCreditScore"
-      number
-      asterisk="true"
-      tooltip={props.fields.estimatedCreditScore.tooltip}
-      value={props.fields.estimatedCreditScore.value}
-      fielddata={props.fields.estimatedCreditScore}
-      changed={props.businessAppEleMaskChange}
-    />
-    {
-      ['totalProjectCost', 'amountNeeded'].map(field => (
-        <MaskedInput
-          hoverable
-          containerclassname={props.preQualFormDisabled ? 'display-only' : ''}
-          readOnly={props.preQualFormDisabled}
-          key={field}
-          prefix="$ "
-          name={field}
-          currency
-          asterisk="true"
-          tooltip={props.fields[field].tooltip}
-          value={props.fields[field].value}
-          fielddata={props.fields[field]}
-          changed={props.businessAppEleMaskChange}
-        />
-      ))
-    }
-  </>
-);
+const Experience = (props) => {
+  const label = props.currentApplicationType === 'commercial-real-estate' ? 'How many years of related industry experience does your team have?' : 'How many years of directly relevant industry experience does your team have?';
+
+  return (
+    <>
+      {
+        ['industryExperience', 'estimatedCreditScore'].map(field => (
+          <MaskedInput
+            maxlength={field === 'estimatedCreditScore' ? 3 : 2}
+            containerclassname={props.preQualFormDisabled ? 'display-only' : ''}
+            readOnly={props.preQualFormDisabled}
+            key={field}
+            name={field}
+            number
+            asterisk="true"
+            label={field === 'industryExperience' ? label : props.fields[field].label}
+            tooltip={props.fields[field].tooltip}
+            value={props.fields[field].value}
+            fielddata={props.fields[field]}
+            changed={props.businessAppEleMaskChange}
+          />
+        ))
+      }
+      {
+        ['totalProjectCost', 'amountNeeded'].map(field => (
+          <MaskedInput
+            hoverable
+            containerclassname={props.preQualFormDisabled ? 'display-only' : ''}
+            readOnly={props.preQualFormDisabled}
+            key={field}
+            prefix="$ "
+            name={field}
+            currency
+            asterisk="true"
+            tooltip={props.fields[field].tooltip}
+            value={props.fields[field].value}
+            fielddata={props.fields[field]}
+            changed={props.businessAppEleMaskChange}
+          />
+        ))
+      }
+    </>
+  );
+};
 
 export default Experience;
