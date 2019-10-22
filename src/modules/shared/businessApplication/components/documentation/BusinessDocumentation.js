ed94b6cf14373acee41dbd48b767f1e22fa2cc1c
pankaj.cuelogic <pankaj.cuelogic@nextseed.com>
2019-10-22 21:04:30 +0530

    2204 [Bug] added *

--- src/modules/shared/businessApplication/components/documentation/BusinessDocumentation.js
+++ src/modules/shared/businessApplication/components/documentation/BusinessDocumentation.js
@@ -67,7 +67,7 @@ export default class BusinessDocumentation extends Component {
                     multiple
                     key={field}
                     name={field}
-                    asterisk={fields[field].rule.includes('required')}
+                    asterisk={field === 'bankStatements' ? 'true' : ''}
                     fielddata={fields[field]}
                     ondrop={(files, fieldName) => businessAppUploadFiles(files, fieldName, 'BUSINESS_DOC_FRM', null, this.props.userStore.isApplicationManager)}
                     onremove={(fieldName, index) => businessAppRemoveFiles(fieldName, 'BUSINESS_DOC_FRM', index)}
