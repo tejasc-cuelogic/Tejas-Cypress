32c29951432bea5d2702df0cd472bc080d9ed0f1
Vaibhav Pathak <vaibhav.cuelogic@nextseed.com>
2019-11-04 19:09:54 +0530

    2197 [enhancement] created reusable component for emails listing

--- src/modules/private/admin/dev/components/email/emailList.js
+++ src/modules/private/admin/dev/components/email/emailList.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Table, Item, Icon, Popup } from 'semantic-ui-react';
+import { Card } from 'semantic-ui-react';
 import { inject, observer } from 'mobx-react';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import moment from 'moment';
 import Filters from './filter';
 import { InlineLoader } from '../../../../../../theme/shared';
 import formHOC from '../../../../../../theme/form/formHOC';
-import { DataFormatter } from '../../../../../../helper';
 import EmailContent from '../../../../shared/EmailContent';
+import EmailsListing from '../../../../shared/EmailsListing';
 
 const metaInfo = {
   store: 'emailStore',
@@ -15,7 +15,7 @@ const metaInfo = {
 };
 
 function EmailList(props) {
-  const [displyRecord, setdisplyRecord] = useState(false);
+  const [displyNoEmails, setdisplyRecord] = useState(false);
   const [showContentModal, toggleModal] = useState(false);
   const [id, setId] = useState(false);
   const [requestDate, setRequestDate] = useState(false);
@@ -85,73 +85,7 @@ function EmailList(props) {
         <Card.Description>
           {loadingArray.includes('getEmailList') ? <InlineLoader />
             : (
-              <div className="table-wrapper">
-                <Table unstackable striped className="application-list">
-                  <Table.Header>
-                    <Table.Row>
-                      <Table.HeaderCell>Send Date</Table.HeaderCell>
-                      <Table.HeaderCell>From Details</Table.HeaderCell>
-                      <Table.HeaderCell>To Details</Table.HeaderCell>
-                      <Table.HeaderCell>Subject</Table.HeaderCell>
-                    </Table.Row>
-                  </Table.Header>
-                  <Table.Body>
-                    {!emailList || (emailList && emailList.length === 0) ? (
-                      <Table.Row><Table.Cell textAlign="center" colSpan="7">{displyRecord && 'No emails to display.'}</Table.Cell></Table.Row>
-                    ) : (
-                      emailList && emailList.map(resp => (
-                          <Table.Row>
-                            <Table.Cell>
-                              <Popup
-                                trigger={(<span>{DataFormatter.getDateAsPerTimeZone(resp.requestDate, true, false, false)}</span>)}
-                                content={(
-                                  <Item>
-                                    {(resp.mergeVars === null) || (resp.mergeVars && resp.mergeVars.length <= 0) ? 'NA'
-                                      : resp.mergeVars.map(mergDetails => (
-                                        <Item.Content>
-                                          <b>{mergDetails.name}:</b>{' '} {mergDetails.content}
-                                        </Item.Content>
-                                      ))
-                                    }
-                                  </Item>
-                                )}
-                                hoverable
-                              />
-                            </Table.Cell>
-                            <Table.Cell className="user-status">
-                              <span className="user-name"><b>{resp.fromName}</b></span>
-                              <p>{resp.fromEmail}</p>
-                            </Table.Cell>
-                            <Table.Cell className="user-status">
-                              <span className="user-name"><b>{resp.toFirstName}</b></span>
-                              <p>{resp.toEmail}</p>
-                            </Table.Cell>
-                            <Table.Cell collapsing>
-                              {resp.subject} <span><Link className="highlight-text" onClick={e => handleModel(e, { recipientId: resp.recipientId, requestDate: resp.requestDate })} to="/">(Body)</Link></span> {' '}
-                              {resp.attachments && resp.attachments.length > 0 && (
-                                <Popup
-                                  trigger={<Icon className="ns-attachment" color="blue" size="large" />}
-                                  content={(
-                                    <Item>
-                                      {resp.attachments.map(attach => (
-                                        <Item.Content>
-                                          <b>Name: </b>{attach.name || 'N/A'}
-                                        </Item.Content>
-                                      ))
-                                      }
-                                    </Item>
-                                  )}
-                                  hoverable
-                                  position="top center"
-                                />
-                              )}
-                            </Table.Cell>
-                          </Table.Row>
-                      ))
-                    )}
-                  </Table.Body>
-                </Table>
-              </div>
+              <EmailsListing emailList={emailList} displyNoEmails={displyNoEmails} handleModel={handleModel} />
             )}
         </Card.Description>
       </Card>
