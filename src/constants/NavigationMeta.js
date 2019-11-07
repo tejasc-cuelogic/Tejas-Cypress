36493c272edcaa21c2ee7fc7ad7dbd7401890c00
Vaibhav Pathak <vaibhav.cuelogic@nextseed.com>
2019-11-07 13:10:23 +0530

    2216 [Enhancement] add seperate new left nav field for request factory

--- src/constants/NavigationMeta.js
+++ src/constants/NavigationMeta.js
@@ -534,6 +534,9 @@ export const PRIVATE_NAV = [
         title: 'Factory', to: 'factory', component: 'Factory', capability: 'FACTORY_ANY',
       },
       {
+        title: 'Request Factory', to: 'request-factory', component: 'RequestFactory', capability: 'REQUEST_FACTORY_ANY',
+      },
+      {
         title: 'Email', to: 'email', component: 'Email', capability: 'EMAIL_ANY',
       },
     ],
