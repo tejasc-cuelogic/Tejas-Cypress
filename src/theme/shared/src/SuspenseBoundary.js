dc9fab1a2066ed7c86383a19afedf7e2fe7cc481
pratik <pratik.cuelogic@nextseed.com>
2019-11-22 19:57:18 +0530

    lazy load crash - always notify - soft/hard fail

--- src/theme/shared/src/SuspenseBoundary.js
+++ src/theme/shared/src/SuspenseBoundary.js
@@ -4,7 +4,7 @@ import { inject, observer } from 'mobx-react';
 import InlineLoader from './InlineLoader';
 import { REACT_APP_DEPLOY_ENV } from '../../../constants/common';
 
-const catchErrorBoundry = !['localhost'].includes(REACT_APP_DEPLOY_ENV);
+const catchErrorBoundry = !['localhosst'].includes(REACT_APP_DEPLOY_ENV);
 
 @inject('authStore')
 @observer
@@ -19,39 +19,44 @@ class SuspenseBoundary extends Component {
   }
 
   componentDidCatch(error, info) {
+    let operationName = 'Lazy loading';
     if (sessionStorage.getItem('crashed') && sessionStorage.getItem('crashed') === window.location.href) {
-      let emailContent = {
-        graphqlError: { operationName: 'Lazy loading' },
-        urlLocation: window.location.href,
-        message: error.stack,
-      };
-      if (window.FS && window.FS.getCurrentSessionURL) {
-        const fullStorySession = window.FS.getCurrentSessionURL(true);
-        emailContent = {
-          ...emailContent,
-          fullStoryUrl: fullStorySession,
-        };
-      }
+      console.log('SuspenseBoundary crashed more than once');
       sessionStorage.removeItem('crashed');
-      if (catchErrorBoundry) {
-        try {
-          const params = {
-            emailContent: JSON.stringify(emailContent),
-          };
-          this.props.authStore.notifyApplicationError(params).then(() => {
-            console.log('Error logging');
-          }).catch(() => {
-            console.log('Error logging');
-          });
-        } catch (e) {
-          console.log('Error logging', e);
-        }
-      } else {
-        console.log(`Error logging for ${REACT_APP_DEPLOY_ENV}, it would have triggered notifyApplicationError otherwise`, error, info);
+      operationName = 'Lazy loading - hard fail';
+    } else {
+      console.log('SuspenseBoundary crashed once');
+      sessionStorage.setItem('crashed', window.location.href);
+      operationName = 'Lazy loading - soft fail';
+    }
+    let emailContent = {
+      graphqlError: { operationName },
+      urlLocation: window.location.href,
+      message: error.stack,
+    };
+    if (window.FS && window.FS.getCurrentSessionURL) {
+      const fullStorySession = window.FS.getCurrentSessionURL(true);
+      emailContent = {
+        ...emailContent,
+        fullStoryUrl: fullStorySession,
+      };
+    }
+
+    if (catchErrorBoundry) {
+      try {
+        const params = {
+          emailContent: JSON.stringify(emailContent),
+        };
+        this.props.authStore.notifyApplicationError(params).then(() => {
+          console.log('Error logging');
+        }).catch(() => {
+          console.log('Error logging');
+        });
+      } catch (e) {
+        console.log('Error logging', e);
       }
     } else {
-      console.log('SuspenseBoundary crashed once, no error reported via email');
-      sessionStorage.setItem('crashed', window.location.href);
+      console.log(`Error logging for ${REACT_APP_DEPLOY_ENV}, it would have triggered notifyApplicationError otherwise`, error, info);
     }
   }
 
