990de153beb5551a1d263c683f938f4c1d28c940
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2019-11-26 19:38:48 +0530

    1871 [enhancement] - resolved figma comment 239 from investor sign up flow

--- src/modules/private/investor/accountSetup/components/confirmModal.js
+++ src/modules/private/investor/accountSetup/components/confirmModal.js
@@ -6,7 +6,7 @@ const isMobile = document.documentElement.clientWidth < 768;
 const ConfirmModal = props => (
   <Modal open={props.open} onClose={props.handleCloseModal} size="mini" closeIcon>
     <Modal.Content className={`${isMobile ? '' : 'center-align'} mt-30`}>
-      <Header as="h3">Thank you for creating an account with NextSeed</Header>
+      <Header as="h3">Thank you for creating a NextSeed account</Header>
       <p className="mt-30 mb-30">
         {props.content}
       </p>
