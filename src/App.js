f5b0d27233d8fb6323e339016651e71ded9e0c9f
pratik <pratik.cuelogic@nextseed.com>
2019-12-19 15:20:20 +0530

    enable private

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './assets/semantic/semantic.min.css';
 import DevPassProtected from './modules/auth/containers/DevPassProtected';
 import { DevBanner, Spinner } from './theme/shared';
 import Layout from './theme/layout/Layout';
-// import Private from './modules/private';
+import Private from './modules/private';
 import Public from './modules/public';
 import SecureGateway from './modules/public/shared/SecureGateway';
 import { authActions, activityActions } from './services/actions';
@@ -274,8 +274,8 @@ class App extends Component {
             <Layout>
               <Switch>
                 <Redirect from="/app/*" to="/dashboard/*" />
-                {/* <Route exact path="/dashboard/*" component={Private} />
-                <Route exact path="/dashboard" component={Private} /> */}
+                <Route exact path="/dashboard/*" component={Private} />
+                <Route exact path="/dashboard" component={Private} />
                 <Route path="/" component={Public} />
               </Switch>
             </Layout>
