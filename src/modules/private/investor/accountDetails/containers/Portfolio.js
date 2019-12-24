1d3680b1c33be1d066518a5dd14a63abbfd228e5
Swapnil bhosale <swapnil.cuelogic@nextseed.com>
2019-12-24 15:34:56 +0530

    2348 [Enhancement] - minor fix for portfolio value

--- src/modules/private/investor/accountDetails/containers/Portfolio.js
+++ src/modules/private/investor/accountDetails/containers/Portfolio.js
@@ -141,7 +141,6 @@ export default class Portfolio extends PureComponent {
       );
     }
     const { getInvestorAccounts, getPieChartData, portfolioError } = portfolioStore;
-    getInvestorAccounts.totalBalance = '-1.5';
     const totalPortfolioBalance = getInvestorAccounts && getInvestorAccounts.totalBalance ? !this.props.isAdmin && money.isNegative(getInvestorAccounts.totalBalance) ? '0.00' : getInvestorAccounts.totalBalance : '0.00';
     const ERROR_MSG = `Sorry, this page is not loading correctly. We've notified the team.<br />
       Please check back again later, and contact us at
