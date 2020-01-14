ba79bcb05cfe5ebcc13ce9f896f35f24784e65b1
Vaibhav Pathak <vaibhav.cuelogic@nextseed.com>
2020-01-14 18:33:32 +0530

    2276[Bug] resolve issue of soft delete functioanlity not working

--- src/services/stores/entities/user/userDetailsStore.js
+++ src/services/stores/entities/user/userDetailsStore.js
@@ -291,16 +291,17 @@ export class UserDetailsStore {
           variables: !isInvestor ? { userId: this.selectedUserId, reason } : {},
         });
       uiStore.removeOneFromProgressArray('deleteProfile');
-      if (get(res, 'data.DeleteInvestorOrIssuerUserOutput.status') || get(res, 'data.adminUserHardDelete.status')) {
+      if (get(res, 'data.deleteInvestorOrIssuerUser.status') || get(res, 'data.adminUserHardDelete.status')) {
         userStore.setFieldValue('confirmDelete', true);
         Helper.toast('User Profile Deleted Successfully!', 'success');
         resolve();
       } else {
-        reject(!isHardDelete ? get(res, 'data.DeleteInvestorOrIssuerUserOutput.message') : get(res, 'data.adminUserHardDelete.message'));
+        reject(!isHardDelete ? get(res, 'data.deleteInvestorOrIssuerUser.message') : get(res, 'data.adminUserHardDelete.message'));
       }
     } catch (error) {
       uiStore.removeOneFromProgressArray('deleteProfile');
       Helper.toast('Something went wrong, please try again in sometime', 'error');
+      reject(error.message);
     }
   });
 
