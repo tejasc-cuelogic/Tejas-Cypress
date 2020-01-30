961e1bdfc112f0910772d2c50114f485e2c735f5
Swapnil bhosale <swapnil.cuelogic@nextseed.com>
2020-01-30 20:04:40 +0530

    2570 [Bug] - Campaign details query update

--- src/services/stores/queries/campagin.js
+++ src/services/stores/queries/campagin.js
@@ -132,11 +132,54 @@ export const campaignDetailsQuery = gql`
     getOfferingDetailsBySlug (offeringSlug: $id, isValid: $isValid) {
     id
     isInvestedInOffering
+    isAvailablePublicly
     watchListStatus
     stage
     offeringSlug
     issuerId
     regulation
+    updates {
+      id
+      offeringId
+      title
+      content
+      status
+      scope
+      tiers
+      notificationSent {
+        by
+        date
+        to
+      }
+      approved {
+        by
+        date
+      }
+      updated {
+        by
+        date
+      }
+      deleted {
+        by
+        date
+      }
+      actingUserInfo {
+        id
+        info {
+          firstName
+          lastName
+          avatar {
+            url
+            name
+          }
+        }
+      }
+    }
+    updated {
+      id
+      by
+      date
+    }
     created {
       id
     }
@@ -146,6 +189,9 @@ export const campaignDetailsQuery = gql`
         url
         isPublic
       }
+      industry
+      locationRiskFactors
+      isTX
       priceCopy
       discount
       valuationCap
@@ -190,12 +236,16 @@ export const campaignDetailsQuery = gql`
     }
     bonusRewards {
       id
+      offeringId
+      rewardStatus
+      expirationDate
       title
       description
       earlyBirdQuantity
       tiers
       created {
         by
+        id
         date
       }
     }
@@ -233,6 +283,7 @@ export const campaignDetailsQuery = gql`
         targetDate
         edgarLink
         expectedOpsDate
+        terminationDate
       }
       misc {
         additionalBonusRewardsContent
@@ -305,6 +356,9 @@ export const campaignDetailsQuery = gql`
         }
       }
     }
+    selectedOffer {
+      structure
+    }
     closureSummary {
       processingDate
       hardCloseDate
@@ -315,11 +369,14 @@ export const campaignDetailsQuery = gql`
       totalInvestorCount
       repayment {
         count
+        completeDate
       }
       keyTerms {
         multiple
         priceCalculation
         revSharePercentage
+        interestRate
+        businessOpenDate
         supplementalAgreements {
           documents {
             name
