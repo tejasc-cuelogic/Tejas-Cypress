61ea4ad7df3da712a09735269456fec19877b385
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2020-02-03 18:34:12 +0530

    2561 [enhancement] - reverted account types screen design

--- src/modules/private/investor/accountSetup/components/accountCreation/AccountTypes.js
+++ src/modules/private/investor/accountSetup/components/accountCreation/AccountTypes.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Modal, Grid, Header, Form } from 'semantic-ui-react';
-import { FormArrowButton } from '../../../../../../theme/form';
+import { Modal, Grid, Button, Header, Form } from 'semantic-ui-react';
+import { FormArrowButton, FormRadioGroup } from '../../../../../../theme/form';
 import AccTypeDescription from './AccTypeDescription';
 
 const isMobile = document.documentElement.clientWidth < 768;
@@ -15,45 +15,76 @@ const AccountTypes = ({
     closeIcon={!isMobile}
     onClose={close}
     closeOnDimmerClick={false}
-    dimmer="inverted"
+    dimmer={isMobile && 'inverted'}
     centered={!isMobile}
     className={isMobile ? 'multistep-modal bg-white' : ''}
-    basic
-    size="large"
+    basic={isMobile}
   >
+    <Modal.Header className={isMobile ? '' : 'center-align signup-header'}>
+      {!isMobile && (
+      <Header as="h3">Which type of investment account would you like to open?</Header>
+      )}
+      {isMobile && (
+        <>
+          <Button
+            icon={{ className: 'ns-chevron-left' }}
+            className="multistep__btn prev"
+            onClick={close}
+          />
+          <Button
+            icon={{ className: 'ns-close-light' }}
+            className="link-button pull-right multistep__btn"
+            onClick={close}
+          />
+        </>
+      )}
+    </Modal.Header>
     <Modal.Content className="signup-content">
+      {isMobile && (
+      <Header as="h3">Which type of investment account would you like to open?</Header>
+      )}
       <Grid
+        textAlign="center"
         onKeyPress={(e) => {
           if (e.key === 'Enter') {
             renderAccType();
           }
         }}
         className={isMobile && 'mt-30'}
-        centered
       >
-        <Grid.Column width="8">
-          {!isMobile && (
-          <Header as="h4">Which type of investment account would you like to open?</Header>
-          )}
-          <Form error className={isMobile ? '' : 'account-type-tab'}>
-            <FormArrowButton
+        <Form error className={isMobile ? '' : 'account-type-tab'}>
+          {!isMobile
+            ? (
+            <FormRadioGroup
               fielddata={form.fields.accType}
               name="accType"
               changed={handleAccTypeChange}
-              action={renderAccType}
+              containerclassname="button-radio center-align"
             />
-          </Form>
-          {isMobile
-            ? (
-              <p className="grey-header mobile-bottom-notes">
-                NextSeed accounts are provided and held at our partner bank, Happy State Bank DBA GoldStar Trust Company ({'"'}GoldStar{'"'}), which provides FDIC insurance for up to $250,000 of uninvested cash in NextSeed accounts.
-              </p>
             )
             : (
-            <AccTypeDescription accTypes={form.fields.accType} />
+              <FormArrowButton
+                fielddata={form.fields.accType}
+                name="accType"
+                changed={handleAccTypeChange}
+                action={renderAccType}
+              />
+            )
+          }
+        </Form>
+        {isMobile
+          ? (
+            <p className="grey-header mobile-bottom-notes">
+              NextSeed accounts are provided and held at our partner bank, Happy State Bank DBA GoldStar Trust Company ({'"'}GoldStar{'"'}), which provides FDIC insurance for up to $250,000 of uninvested cash in NextSeed accounts.
+            </p>
+          )
+          : (
+          <AccTypeDescription accTypes={form.fields.accType} />
           )}
-        </Grid.Column>
       </Grid>
+      {!isMobile
+      && (<Button circular icon={{ className: 'ns-arrow-right' }} className="multistep__btn next active" onClick={() => renderAccType()} />)
+      }
     </Modal.Content>
   </Modal>
 );
