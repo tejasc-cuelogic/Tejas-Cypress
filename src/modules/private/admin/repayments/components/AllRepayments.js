0d0dae6669e385a5981ea03af33daaeb6e074cc6
Chetan Bura <chetan.cuelogic@nextseed.com>
2020-02-17 18:21:03 +0530

    2605 [Feature] updated the client formula columns

--- src/modules/private/admin/repayments/components/AllRepayments.js
+++ src/modules/private/admin/repayments/components/AllRepayments.js
@@ -208,7 +208,7 @@ export default class AllRepayments extends PureComponent {
       this.props.paymentStore.initRequest();
     } else {
       this.props.paymentStore.setFieldValue('data', []);
-      this.props.paymentStore.setFieldValue('apiHit', true);
+      this.props.paymentStore.setFieldValue('apiHit', false);
     }
   }
 
