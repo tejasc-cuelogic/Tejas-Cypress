ccf8f27379deffe4783abbbeee71aa2ea26f2a4e
chris.humphrey <chris.humphrey@nextseed.com>
2020-02-17 21:40:15 -0600

    adding tooltip design to investment multiple on desktop

--- src/modules/public/offering/components/campaignDetails/investmentDetails/KeytermsDetails.js
+++ src/modules/public/offering/components/campaignDetails/investmentDetails/KeytermsDetails.js
@@ -171,7 +171,7 @@ class KeyTermsDetails extends Component {
                   ? (<PopUpModal customTrigger={<span className="popup-label">Investment Multiple</span>} content={<>This is the multiple of your original investment that the Issuer has agreed to pay back prior to maturity. The Issuer pays a portion of their gross revenues every month until the Investment Multiple is achieved. <a target="_blank" href="/resources/education-center/investor/how-revenue-sharing-notes-work">Learn more</a></>} />)
                   : (
                     <Popup
-                      trigger={<Icon name="help circle" color="green" />}
+                      customTrigger={<span className="popup-label">Investment Multiple</span>}
                       content={<>This is the multiple of your original investment that the Issuer has agreed to pay back prior to maturity. The Issuer pays a portion of their gross revenues every month until the Investment Multiple is achieved. <a target="_blank" href="/resources/education-center/investor/how-revenue-sharing-notes-work">Learn more</a></>}
                       position="top center"
                       hoverable
