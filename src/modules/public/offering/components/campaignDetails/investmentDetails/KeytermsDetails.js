bc8583589682a6caab2817470e1b742707639e69
chris.humphrey <chris.humphrey@nextseed.com>
2020-02-17 21:25:54 -0600

    testing new tooltip design on investment multiple

--- src/modules/public/offering/components/campaignDetails/investmentDetails/KeytermsDetails.js
+++ src/modules/public/offering/components/campaignDetails/investmentDetails/KeytermsDetails.js
@@ -166,8 +166,7 @@ class KeyTermsDetails extends Component {
             <KeyTermsFieldHoc
               data={keyTerms}
               field="investmentMultiple"
-              title="Investment Multiple"
-              titleAddon={
+              title={
                 isMobile
                   ? (<PopUpModal customTrigger={<span className="popup-label">Investment Multiple</span>} content={<>This is the multiple of your original investment that the Issuer has agreed to pay back prior to maturity. The Issuer pays a portion of their gross revenues every month until the Investment Multiple is achieved. <a target="_blank" href="/resources/education-center/investor/how-revenue-sharing-notes-work">Learn more</a></>} />)
                   : (
