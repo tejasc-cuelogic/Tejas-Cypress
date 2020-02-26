fbffe1aab14af8d5e32a7b078b8954803e7acb55
chris.humphrey <chris.humphrey@nextseed.com>
2020-02-26 11:06:50 -0600

    adding tooltip design to investNow flow

--- src/modules/public/offering/components/investNow/financialInfo/InvestmentLimit.js
+++ src/modules/public/offering/components/investNow/financialInfo/InvestmentLimit.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Header, Popup, Icon } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
 import Helper from '../../../../../../helper/utility';
+import { PopUpModal } from '../../../../../../theme/shared';
+
+const isMobile = document.documentElement.clientWidth < 768;
 
 const InvestmentLimit = props => (
   <>
     <Header as={props.changeInvest ? 'h6' : 'h4'} textAlign={props.changeInvest ? '' : 'center'}>
-      Your investment limit: {' '}
+      Your
       {Helper.MoneyMathDisplayCurrency(props.getCurrentLimitForAccount || 0, false)}
-      <Popup
+      <PopUpModal
         wide
-        trigger={<Icon className="ns-help-circle" color="green" />}
+        customTrigger={<span className="popup-label">{' '}investment limit</span>}
         content={(
           <span>
             Under Regulation Crowdfunding, you have a limit as to how much you may invest
             in Reg CF offerings over a 12-month period. This limit is calculated based on your
             annual income and net worth. <Link to={`${props.refLink}/investment-details/#total-payment-calculator`}>Click here</Link> for how this is calculated. If you believe
-            your limit is innacurate, please update your <Link to="/dashboard/account-settings/profile-data">Investor Profile</Link>
+            your limit is inaccurate, please update your <Link to="/dashboard/account-settings/profile-data">Investor Profile</Link>
           </span>
         )}
         position="top center"
+        showOnlyPopup={!isMobile}
         hoverable
       />
+      :{' '}
+      {Helper.MoneyMathDisplayCurrency(currentInvestmentLimit || 0, false)}
       <Link to={props.changeInvest ? 'change-investment-limit' : `${props.match.url}/change-investment-limit`} className="link"><small>Update</small></Link>
     </Header>
     {props.changeInvest
