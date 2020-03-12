06d8e7e86f170b96adf74659abed1b65c93eb766
Chetan Bura <chetan.cuelogic@nextseed.com>
2020-03-12 19:16:11 +0530

    2649 [Feature] updated ui for toc preview

--- src/modules/private/shared/offerings/components/offering/toc/InvestNowTocPreview.js
+++ src/modules/private/shared/offerings/components/offering/toc/InvestNowTocPreview.js
@@ -2,8 +2,9 @@ import React, { useEffect } from 'react';
 import { inject, observer } from 'mobx-react';
 import { withRouter } from 'react-router-dom';
 import { get } from 'lodash';
-import { Header, Button, Grid, Form, Modal } from 'semantic-ui-react';
+import { Header, Button, Grid, Form } from 'semantic-ui-react';
 import { FormCheckbox } from '../../../../../../../theme/form';
+import { NsModal } from '../../../../../../../theme/shared';
 
 function InvestNowTocPreview(props) {
   useEffect(() => {
@@ -18,40 +19,50 @@ function InvestNowTocPreview(props) {
   const businessName = get(offer, 'keyTerms.shorthandBusinessName');
   const index = agreementPage;
   return (
-    <Modal open closeIcon onClose={handleClosePreview} size="small" closeOnDimmerClick={false}>
-      <Modal.Header className="center-align signup-header">
-        <Header as="h3" className="mb-40">
-          Let&#39;s confirm your investment.<br />You are investing
-            <span className="positive-text"> XXXX.00</span> in {businessName}.
-          {AGREEMENT_DETAILS_FORM.fields.page[index].title.value
-          && (
-          <Header.Subheader>
-            {AGREEMENT_DETAILS_FORM.fields.page[index].title.value}
-          </Header.Subheader>
-          )}
-        </Header>
-      </Modal.Header>
-      <Modal.Content className="signup-content">
-        <Form>
-          <Grid stackable>
-            <Grid.Row>
-              <Grid.Column width={16}>
-                <FormCheckbox
-                  defaults
-                  fielddata={AGREEMENT_DETAILS_FORM.fields.page[index].toc}
-                  name="toc"
-                  containerclassname="ui list agreement-list very relaxed"
-                  changed={(e, res) => setCheckbox(e, res, 'page', index)}
-                />
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-          <div className="mt-30">
-            <Button primary content="Invest" disabled={!isAgreementFormValid} onClick={callbackFun} />
+    <NsModal
+      useMountNode
+      open
+      closeOnRootNodeClick={false}
+      closeOnDimmerClick={false}
+      onClose={handleClosePreview}
+      headerLogo
+      borderedHeader
+    >
+      <Grid centered stackable className="mt-0">
+        <Grid.Column width="10" className="pt-0">
+          <div style={{ display: 'block' }}>
+            <Header as="h3" className="mb-40">
+              Let&#39;s confirm your investment.<br />You are investing
+              <span className="positive-text"> XXXX.00</span> in {businessName}.
+                {AGREEMENT_DETAILS_FORM.fields.page[index].title.value
+                && (
+                  <Header.Subheader>
+                    {AGREEMENT_DETAILS_FORM.fields.page[index].title.value}
+                  </Header.Subheader>
+                )}
+            </Header>
+            <Form>
+              <Grid stackable>
+                <Grid.Row>
+                  <Grid.Column width={16}>
+                    <FormCheckbox
+                      defaults
+                      fielddata={AGREEMENT_DETAILS_FORM.fields.page[index].toc}
+                      name="toc"
+                      containerclassname="ui list agreement-list very relaxed"
+                      changed={(e, res) => setCheckbox(e, res, 'page', index)}
+                    />
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+              <div className="mt-30">
+                <Button primary content="Invest" disabled={!isAgreementFormValid} onClick={callbackFun} />
+              </div>
+            </Form>
           </div>
-        </Form>
-      </Modal.Content>
-    </Modal>
+        </Grid.Column>
+      </Grid>
+    </NsModal>
   );
 }
 
