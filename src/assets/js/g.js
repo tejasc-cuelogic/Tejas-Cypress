f622f569552828a4d93ae9403c1dcede7a849a18
Chetan Bura <chetan.cuelogic@nextseed.com>
2020-03-12 13:05:56 +0530

    2644 [Enhnacment] dynamically added goolgle auto complete js

--- src/assets/js/g.js
+++ src/assets/js/g.js
@@ -2,9 +2,6 @@
 var head = document.getElementsByTagName('head')[0];
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.onload = function() {
-    callFunctionFromScript();
-}
 script.src = 'https://maps.googleapis.com/maps/api/js?key='+__SEGMENT_WRITE_KEY__+'&libraries=places';
 head.appendChild(script);
 }();
