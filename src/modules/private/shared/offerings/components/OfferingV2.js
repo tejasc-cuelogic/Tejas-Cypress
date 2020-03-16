09e9dd0ecb908ce04b4c1f7ed23665410940fc2d
Chetan Bura <chetan.cuelogic@nextseed.com>
2020-03-16 14:54:22 +0530

    2386 [Feature] updated the offering tab for template 2

--- src/modules/private/shared/offerings/components/OfferingV2.js
+++ src/modules/private/shared/offerings/components/OfferingV2.js
@@ -1,30 +1,43 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
+import { get } from 'lodash';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import { lazyRetry } from '../../../../../theme/shared';
 import SecondaryMenu from '../../../../../theme/layout/SecondaryMenu';
 
 const getModule = component => lazyRetry(() => import(`./${component}`));
 
-@inject('userStore', 'uiStore', 'offeringCreationStore')
+@inject('userStore', 'offeringsStore')
 @withRouter
 @observer
 export default class OfferingV2 extends Component {
   constructor(props) {
     super(props);
+    const { offer } = props.offeringsStore;
+    const nav = (!!get(offer, 'investNow.page[0]') || get(offer, 'stage') === 'CREATION') ? 'invest-now-toc' : get(offer, 'stage') === 'CREATION' ? 'launch' : 'close';
     if (this.props.match.isExact) {
-      this.props.history.push(`${this.props.match.url}/tombstone`);
+      this.props.history.push(`${this.props.match.url}/${nav}`);
     }
   }
 
   render() {
-    const { isIssuer } = this.props.userStore;
-    const navItems = [
-      { title: 'Invest Now', to: 'tombstone', component: 'offering/InvestNowToc' },
-      { title: 'Launch', to: 'Launch', component: 'offering/OfferingLaunch' },
-      { title: 'Close', to: 'close', component: 'Close' },
-    ];
-    const { match } = this.props;
+    const { offeringsStore, match, userStore } = this.props;
+    const { isIssuer } = userStore;
+    const { offer } = offeringsStore;
+    const showInvestNowToc = !!get(offer, 'investNow.page[0]') || get(offer, 'stage') === 'CREATION';
+    const navItems = [];
+    if (showInvestNowToc) {
+      navItems.push({ title: 'Invest Now', to: 'invest-now-toc', component: 'offering/InvestNowToc' });
+    }
+    navItems.push({ title: 'Launch', to: 'launch', component: 'offering/OfferingLaunch' });
+    if (get(offer, 'stage') === 'LIVE') {
+      navItems.push({ title: 'Close', to: 'close', component: 'Close' });
+    }
+    // const navItems = [
+    //   showInvestNowToc && { title: 'Invest Now', to: 'tombstone', component: 'offering/InvestNowToc' },
+    //   { title: 'Launch', to: 'Launch', component: 'offering/OfferingLaunch' },
+    //   { title: 'Close', to: 'close', component: 'Close' },
+    // ];
     return (
       <div className={isIssuer ? 'ui card fluid' : ''}>
         <SecondaryMenu force2ary={!isIssuer} match={match} navItems={navItems} />
