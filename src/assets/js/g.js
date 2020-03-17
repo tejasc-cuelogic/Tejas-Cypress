80218dbae9c6e3fc5a58577c7a789370ffa26239
Kalim Shaikh <pratik.cuelogic@nextseed.com>
2020-03-17 14:53:47 +0530

    removed g.js as it no longer requried

--- src/assets/js/g.js
+++
@@ -1,7 +0,0 @@
-!function(){
-var head = document.getElementsByTagName('head')[0];
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://maps.googleapis.com/maps/api/js?key='+__SEGMENT_WRITE_KEY__+'&libraries=places';
-head.appendChild(script);
-}();
