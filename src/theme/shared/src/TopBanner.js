cc425ba73cd4850eb42f46ed2335ef37215743d7
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2020-03-24 19:33:02 +0530

    2712 [enhancement] - banner changes for mobile view

--- src/theme/shared/src/TopBanner.js
+++ src/theme/shared/src/TopBanner.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Button, Icon } from 'semantic-ui-react';
 
 const TopBanner = props => (
-  <div className={(props.leftMenu) ? 'sticky-message abcd' : 'sticky-message'}>
+  <div className={(props.leftMenu) ? 'sticky-message hide' : 'sticky-message'}>
     <b>
       <a target="_blank" rel="noopener noreferrer" href="https://www.nextseed.com/insights/businesses-affected-by-coronavirus">
         Click here
       </a>
     </b>
     {' '}to donate to the LIFE Fund.  Resources for small businesses impacted by the Coronavirus crisis can be{' '}
-    <a href="https://www.nextseed.com/insights/businesses-affected-by-coronavirus">found here</a>.
+    <b><a href="https://www.nextseed.com/insights/businesses-affected-by-coronavirus">found here</a></b>.
     <Button
       icon
       onClick={props.toggle}
