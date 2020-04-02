4d66fe5a04e76d47b64fdb3853c26187f9f89d8f
vaibhav.cuelogic <vaibhav.cuelogic@nextseed.com>
2020-04-02 17:45:23 +0530

    2386[Feature] remove unwnted AgreemtnTemplate use case condition from portfolio js file as well as make campaignStatus isInvestNowToc as boolean value holder

--- src/services/stores/entities/public/campaignStore.js
+++ src/services/stores/entities/public/campaignStore.js
@@ -382,7 +382,7 @@ export class CampaignStore {
     campaignStatus.isRealEstate = ((this.offerStructure === CAMPAIGN_KEYTERMS_SECURITIES_ENUM.EQUITY && get(campaign, 'keyTerms.equityClass') === 'LLC_MEMBERSHIP_UNITS'));
     campaignStatus.isPreferredEquity = ((this.offerStructure === CAMPAIGN_KEYTERMS_SECURITIES_ENUM.EQUITY && (get(campaign, 'keyTerms.equityClass') === 'PREFERRED' || (this.inInvestmentFlow && ['CLASS_A_SHARES', 'CLASS_B_SHARES', 'PARALLEL_CLASS_SHARES'].includes(get(campaign, 'keyTerms.equityClass'))))));
     campaignStatus.doneComputing = (get(this.details, 'data.getOfferingDetailsBySlug') && !isEmpty(this.details.data.getOfferingDetailsBySlug.keyTerms)) || false;
-    campaignStatus.isAgreementTemplate = (get(campaign, 'investNow.template') && get(campaign, 'investNow.template') === 2);
+    campaignStatus.isAgreementTemplate = !!(get(campaign, 'investNow.template') && get(campaign, 'investNow.template') === 2);
     campaignStatus.investNowToc = get(campaign, 'investNow.page');
     return campaignStatus;
   }
