14166947ec53049c0bc88eb50ba42300b99b0523
Chetan Bura <chetan.cuelogic@nextseed.com>
2020-04-06 16:19:25 +0000

    Update NavigationMeta.js Added watchlist for offering template 2

--- src/constants/NavigationMeta.js
+++ src/constants/NavigationMeta.js
@@ -287,7 +287,7 @@ export const PRIVATE_NAV = [
         title: 'Offering Creation', to: 'offering-creation', accessFor: [2, 3, 4], accessibleTo: ['admin', 'manager', 'support'], template: 1,
       },
       {
-        title: 'Watch List', to: 'watch-list', accessFor: [2, 3, 4], accessibleTo: ['admin', 'manager', 'support'], template: 1,
+        title: 'Watch List', to: 'watch-list', accessFor: [2, 3, 4], accessibleTo: ['admin', 'manager', 'support'],
       },
       {
         title: 'Activity History', to: 'activity-history', accessFor: [1, 2, 3, 4], accessibleTo: ['admin', 'manager', 'support'],
