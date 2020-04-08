a07128bea23e4a16edf40c07afd0c66c32f875b4
Kalimahmed Shaikh <kalim.cuelogic@nextseed.com>
2020-04-08 14:57:04 +0000

    Revert "Merge branch..."
    This reverts merge request !3221

--- src/constants/account.js
+++ src/constants/account.js
@@ -650,13 +650,12 @@ export const FINANCES = {
     label: 'Net Worth',
     error: undefined,
     showError: true,
-    rule: 'required|min:1|max:2147483647',
+    rule: 'required|min:1',
     placeHolder: 'Enter here',
     fieldType: 'integer',
     customErrors: {
       required: 'required',
       min: 'Please enter a valid amount',
-      max: 'Please enter a valid amount to deposit.',
     },
   },
   annualIncomeCurrentYear: {
