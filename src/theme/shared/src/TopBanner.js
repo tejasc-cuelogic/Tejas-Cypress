56480f62f605697fd3c26e8add27e9a3e7d2faef
Tarek Al Beb <tarekalbeb@Tareks-MacBook-Air.local>
2020-04-10 10:21:02 -0500

    added target to a

--- src/theme/shared/src/TopBanner.js
+++ src/theme/shared/src/TopBanner.js
@@ -6,7 +6,7 @@ const TopBanner = props => (
     Small businesses affected by COVID-19 can{' '}<b><a href="https://www.nextseed.com/insights/community-bridge-notes">apply to raise working capital</a></b>
     {' '}and view helpful resources{' '}<b><a href="https://www.nextseed.com/insights/businesses-affected-by-coronavirus">here</a></b><br />
     Support local restaurants and healthcare workers with a{' '}
-    <b><a href="https://charity.gofundme.com/o/en/campaign/life-fund">donation to the LIFE Fund</a></b>
+    <b><a target="_blank" rel="noopener noreferrer" href="https://charity.gofundme.com/o/en/campaign/life-fund">donation to the LIFE Fund</a></b>
     <Button
       icon
       onClick={props.toggle}
