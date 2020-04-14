3e2085990ca3f2ded79c221e31d6341c6b9293e4
vaibhav.cuelogic <vaibhav.cuelogic@nextseed.com>
2020-04-14 21:46:45 +0530

    2756[Feature] adding new section as Generate Document

--- src/modules/private/shared/offerings/components/offering/InvestNowDocusignGenerations.js
+++ src/modules/private/shared/offerings/components/offering/InvestNowDocusignGenerations.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
 import { withRouter } from 'react-router-dom';
+import { Card } from 'semantic-ui-react';
 // import { get } from 'lodash';
-// import BusinessApplicationMapping from './applicationMapping/businessApplicationMapping';
+import GenerateDocuments from './investNowDocusign/generateDocuments';
 import { InlineLoader } from '../../../../../../theme/shared';
 import DataRoom from '../legal/DataRoom';
 
 
-@inject('businessAppReviewStore', 'businessAppStore', 'nsUiStore', 'offeringCreationStore')
+@inject('businessAppReviewStore', 'businessAppStore', 'nsUiStore', 'offeringCreationStore', 'factoryStore')
 @withRouter
 @observer
 class InvestNowDocusignGenerations extends Component {
+  constructor(props) {
+    super(props);
+    this.props.factoryStore.fetchPluginsForFileFactory();
+  }
+
   render() {
     const { loadingArray } = this.props.nsUiStore;
     const { setFormDataForBusinessUploadDocuments } = this.props.offeringCreationStore;
@@ -21,12 +27,21 @@ class InvestNowDocusignGenerations extends Component {
         ? <InlineLoader />
         : (
           <>
-            <DataRoom
-              header="Documents"
-              referenceFrom="BUSINESS_APPLICATION"
-              documentUpload="AGREEMENTS"
-              isReadOnlyFlag={businessAppReadOnly}
-            />
+            <GenerateDocuments />
+            <Card fluid className="elastic-search">
+              <Card.Content header="Upload Documents" />
+              <Card.Content>
+                <Card.Description>
+                  <DataRoom
+                    header="Documents"
+                    isHeaderVisible={false}
+                    referenceFrom="BUSINESS_APPLICATION"
+                    documentUpload="AGREEMENTS"
+                    isReadOnlyFlag={businessAppReadOnly}
+                  />
+                </Card.Description>
+              </Card.Content>
+            </Card>
           </>
         )
     );
---
+++ src/modules/private/shared/offerings/components/offering/investNowDocusign/generateDocuments.js
@@ -0,0 +1,69 @@
+import React, { useEffect } from 'react';
+import { observer } from 'mobx-react';
+import { withRouter } from 'react-router-dom';
+import { Card, Grid, Divider, Header, Form, Button } from 'semantic-ui-react';
+import formHOC from '../../../../../../../theme/form/formHOC';
+import DynamicFormInput from './dynamicFormInput';
+
+const metaInfo = {
+  store: 'factoryStore',
+  form: 'FILEFACTORY_FRM',
+};
+/* constructor(props) {
+  super(props);
+  this.props.businessAppReviewStore.setFormData('APPLICATION_MAPPED_OFFERING_FORM');
+}
+
+componentWillUnmount() {
+  this.props.businessAppReviewStore.resetBusinessApplicationMappingForm('APPLICATION_MAPPED_OFFERING_FORM');
+} */
+function GenerateDocuments(props) {
+  useEffect(() => {
+    props.factoryStore.resetForm('FILEFACTORY_FRM');
+    props.factoryStore.setFieldValue('inProgress', false, 'fileFactory');
+    props.factoryStore.setFieldValue('DYNAMCI_PAYLOAD_FRM', {}, 'FILEFACTORY');
+  }, []);
+
+  const onSubmit = () => {
+    props.factoryStore.fileFactoryPluginTrigger();
+  };
+
+  const { factoryStore, smartElement } = props;
+  const {
+    FILEFACTORY_FRM, formChangeForPlugin, inProgress, DYNAMCI_PAYLOAD_FRM, currentPluginSelected,
+  } = factoryStore;
+  return (
+    <>
+      <Card fluid className="elastic-search">
+        <Card.Content header="Generate Document" />
+        <Card.Content>
+          <Card.Description>
+            <Form onSubmit={FILEFACTORY_FRM.meta.isValid && onSubmit}>
+              <Form.Group>
+                <Grid className="full-width mlr-0" stackable>
+                  <Grid.Column width={8}>
+                    {smartElement.FormDropDown('method', {
+                      onChange: (e, result) => formChangeForPlugin(e, result, 'FILEFACTORY_FRM'),
+                      containerclassname: 'dropdown-field mlr-0',
+                      placeholder: 'Choose here',
+                      options: FILEFACTORY_FRM.fields.method.values,
+                      className: 'mb-80',
+                    })}
+                    <Divider section hidden />
+                    <Button className="mt-80 ml-10" primary content="Generate" disabled={inProgress.fileFactory || !FILEFACTORY_FRM.meta.isValid || !DYNAMCI_PAYLOAD_FRM.FILEFACTORY.meta.isValid} loading={inProgress.fileFactory} />
+                  </Grid.Column>
+                  <Grid.Column width={8}>
+                    <Header as="h5">Payload</Header>
+                    <DynamicFormInput {...props} formPayload={DYNAMCI_PAYLOAD_FRM.FILEFACTORY} formObj={{ parentForm: 'DYNAMCI_PAYLOAD_FRM', childForm: 'FILEFACTORY' }} selectedPlugin={currentPluginSelected} />
+                  </Grid.Column>
+                </Grid>
+              </Form.Group>
+            </Form>
+          </Card.Description>
+        </Card.Content>
+      </Card>
+    </>
+  );
+}
+
+export default (withRouter(formHOC(observer(GenerateDocuments), metaInfo)));
