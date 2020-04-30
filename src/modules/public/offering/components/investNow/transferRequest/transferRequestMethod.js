276e7a783d5798308c7117964c5eee7faa2714f0
vishakha.cuelogic <vishakha.cuelogic@nextseed.com>
2020-04-30 16:35:05 +0530

    added grid for wire instructions

--- src/modules/public/offering/components/investNow/transferRequest/transferRequestMethod.js
+++ src/modules/public/offering/components/investNow/transferRequest/transferRequestMethod.js
@@ -1,12 +1,49 @@
 import React from 'react';
-import { Table } from 'semantic-ui-react';
-import Helper from '../../../../../../helper/utility';
+import { Grid } from 'semantic-ui-react';
+// import Helper from '../../../../../../helper/utility';
 
 function TransferRequestMethod(props) {
-  const { isPreferredEquity, getTransferRequestAmount, transferMethod, accountDetailsMeta } = props;
+  // const { isPreferredEquity, getTransferRequestAmount, transferMethod, accountDetailsMeta } = props;
+  console.log(props);
   return (
     <>
-      <Table basic="very" className="confirm-transfer-table mt-30" compact>
+    <Grid columns="equal">
+      <Grid.Row className="mt-60">
+        <Grid.Column>
+          <p><b>Wire Amount</b></p>
+        </Grid.Column>
+        <Grid.Column>
+          <p className="right-align"><b>$ 20,000</b></p>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row className="mt-60">
+        <Grid.Column>
+          <p><b>Wire Instructions:</b></p>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column>
+          <p>Please print these instructions for your records
+          A copy will also been sent to you via email after you confirm.
+          </p>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column width={5}>
+          <p className="mb-0">Happy State Bank</p>
+          <p className="mb-0">100 E. Main Street</p>
+          <p className="mb-0">Happy, Texas  79042</p>
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <p className="mb-0">Routing Number: 111310870</p>
+          <p className="mb-0">Account Number: 6002000773</p>
+          <p className="mb-0">Memo - For further credit to: Goldstar Trust Operating</p>
+          <p className="mb-0">FBO: $</p>
+       </Grid.Column>
+      </Grid.Row>
+    </Grid>
+
+      {/* <Table basic="very" className="confirm-transfer-table mt-30" compact>
         <Table.Body>
           <Table.Row>
             <Table.HeaderCell colspan="6">{transferMethod === 'ACH' ? 'Transfer Request:' : 'Wire Amount:'} </Table.HeaderCell>
@@ -36,9 +73,9 @@ function TransferRequestMethod(props) {
                 </Table.Row>
                 <Table.Row>
                   <Table.Cell>
-                    Happy State Bank
-                    100 E. Main Street
-                    Happy, Texas  79042
+                    <p className="mb-0">Happy State Bank</p>
+                    <p className="mb-0">100 E. Main Street</p>
+                    <p className="mb-0">Happy, Texas 79042</p>
                   </Table.Cell>
                   <Table.Cell collapsing></Table.Cell>
                   <Table.Cell>
@@ -52,8 +89,8 @@ function TransferRequestMethod(props) {
             )
           }
         </Table.Body>
-      </Table>
-    </>
+      </Table> */}
+      </>
   );
 }
 
