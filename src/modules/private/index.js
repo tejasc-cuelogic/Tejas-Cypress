65c7802ceaf87eb4f8f3bc0a8cadf08166d21365
vaibhav.cuelogic <vaibhav.cuelogic@nextseed.com>
2020-04-30 15:02:13 +0530

    modify refurl in private index js file if user directly access after login urls without lgoin case

--- src/modules/private/index.js
+++ src/modules/private/index.js
@@ -31,7 +31,8 @@ export default class Private extends React.Component {
     const { userStore, referralsStore, userDetailsStore } = this.props;
     if (!this.props.authStore.isUserLoggedIn) {
       this.props.uiStore.setRedirectURL(this.props.location.pathname);
-      this.props.uiStore.setAuthRef(this.props.location.pathname);
+      // this.props.uiStore.setAuthRef(this.props.location.pathname);
+      this.props.uiStore.setAuthRef('/');
       this.props.history.push('/login');
     } else if (userStore.isInvestor && get(userDetailsStore, 'signupStatus.activeAccounts') && get(userDetailsStore, 'signupStatus.activeAccounts').length) {
       referralsStore.getUserReferralDetails(false, false);
