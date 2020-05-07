6f1983a34c0443b1c040278f95ef002ae7a74df4
Chetan Bura <chetan.cuelogic@nextseed.com>
2020-05-07 14:17:15 +0530

    collection card update for preview

--- src/modules/private/admin/collections/components/TombstonePreview.js
+++ src/modules/private/admin/collections/components/TombstonePreview.js
@@ -16,25 +16,24 @@ export default class TombstonePreview extends Component {
     const collection = TOMBSTONE_FRM.fields;
     return (
       <div className="bg-offwhite pt-30 pb-30">
-        <section className="pt-30 pb-30">
-          {get(collection, 'bgImage.value')
-            && <Image64 bg reRender originalImg className="collection-bg-image ui-container" srcUrl={get(collection, 'bgImage.preSignedUrl')} />
-          }
-          <Container>
-            <Grid style={{ backgroundColor: get(collection, 'bgColor.value') }} className="p-64 collection-box">
-              <Grid.Column widescreen={4} computer={4} tablet={16} mobile={16}>
-                {/* {get(collection, 'text.value')}
-                {get(collection, 'color.value')} */}
-                <Image64 reRender srcUrl={get(collection, 'image.preSignedUrl')} />
-              </Grid.Column>
-              <Grid.Column widescreen={11} computer={11} tablet={16} mobile={16}>
-                <Header as="h3">{get(collection, 'title.value')}</Header>
-                <HtmlEditor readOnly content={get(collection, 'description.value')} />
-                <Button inverted color="white" className="mt-30 mb-30">Explore</Button>
-              </Grid.Column>
-            </Grid>
-          </Container>
-        </section>
+        <Container className="offerings-container pt-30 pb-30">
+          <Grid style={{ backgroundColor: get(collection, 'bgColor.value') }} className="p-36 collection-box">
+            <Grid.Column widescreen={4} computer={4} tablet={16} mobile={16} className="zi-9">
+              <Image64 srcUrl={get(collection, 'image.preSignedUrl')} />
+              <div style={{ backgroundColor: get(collection, 'tag.color.value') }} className="ns_flgs_box">
+                <p>{get(collection, 'tag.text.value')}</p>
+              </div>
+            </Grid.Column>
+            <Grid.Column widescreen={11} computer={11} tablet={16} mobile={16} className="zi-9">
+              <Header as="h3">{get(collection, 'title.value')}</Header>
+              <HtmlEditor readOnly content={get(collection, 'description.value')} />
+              <Button inverted color="white" className="mt-30 mb-30">Explore</Button>
+            </Grid.Column>
+            {get(collection, 'bgImage.value')
+              && <Image64 bg originalImg className="collection-bg-image" srcUrl={get(collection, 'bgImage.value')} />
+            }
+          </Grid>
+        </Container>
       </div>
     );
   }
