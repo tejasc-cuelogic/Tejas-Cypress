6674ed8c234a51c8e62ee6172e609f04e47969c9
vishakha nimkar <vishakha.nimkar@cuelogic.com>
2020-05-12 17:47:30 +0530

    added css and class for button

--- src/modules/private/admin/collections/components/TombstonePreview.js
+++ src/modules/private/admin/collections/components/TombstonePreview.js
@@ -37,7 +37,7 @@ export default class TombstonePreview extends Component {
             <Grid.Column widescreen={12} computer={11} tablet={16} mobile={16} className="zi-9 collection-content">
               <Header style={{ color: get(collection, 'descriptionColor.value') }} as="h3">{get(collection, 'title.value')}</Header>
               <p style={{ color: get(collection, 'descriptionColor.value') }}><HtmlEditor readOnly content={get(collection, 'description.value')} /></p>
-              <Button inverted onMouseLeave={this.toggleHover} onMouseEnter={this.toggleHover} style={{ color: isHovered ? get(collection, 'bgColor.value') : get(collection, 'descriptionColor.value'), backgroundColor: isHovered ? get(collection, 'descriptionColor.value') : '', borderColor: get(collection, 'marketing.tombstone.descriptionColor') }} className="mt-20 mb-30 collectionExplore">Explore</Button>
+              <Button inverted onMouseLeave={this.toggleHover} onMouseEnter={this.toggleHover} style={{ color: isHovered ? get(collection, 'bgColor.value') : get(collection, 'descriptionColor.value'), backgroundColor: isHovered ? get(collection, 'descriptionColor.value') : '', borderColor: get(collection, 'descriptionColor.value') }} className="mt-20 mb-30 collectionExplore">Explore</Button>
             </Grid.Column>
             {get(collection, 'bgImage.preSignedUrl')
               && <Image64 bg reRender originalImg className="collection-bg-image" srcUrl={get(collection, 'bgImage.preSignedUrl')} />
