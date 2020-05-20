9f4801ba0f488f2eaa037d0e051b1c87f58c4419
vishakha nimkar <vishakha.nimkar@cuelogic.com>
2020-05-20 18:45:09 +0530

    2817 [enhancement] added css for offering page

--- src/modules/public/offering/containers/Offering.js
+++ src/modules/public/offering/containers/Offering.js
@@ -17,7 +17,7 @@ const LoadMoreBtn = ({ action, param, isMobile }) => (
 );
 
 const NsCapital = ({ nsCapitalMeta, isMobile, isTablet }) => (
-  <section key={nsCapitalMeta.title} className={`${isMobile || isTablet ? 'pt-20 pb-50' : 'pt-100 pb-100'} bg-white`}>
+  <section key={nsCapitalMeta.title} className={`${isMobile || isTablet ? 'pt-0 pb-50' : 'pt-40 pb-100'} bg-white`}>
     <Container>
       <Grid>
       <Grid.Column widescreen={7} computer={7} tablet={16} mobile={16} floated={!isMobile ? 'left' : ''}>
