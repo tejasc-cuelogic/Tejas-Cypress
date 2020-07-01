1abb3d5b8dcfa4b1d22876714d76775281d0b7a0
tarekbeb <tarek.albeb@nextseed.com>
2020-07-01 01:02:56 -0500

    fixed url

--- src/modules/private/investor/accountSetup/containers/establishProfile/ConfirmCancelModal.js
+++ src/modules/private/investor/accountSetup/containers/establishProfile/ConfirmCancelModal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
 import { inject, observer } from 'mobx-react';
+// import { Button } from 'semantic-ui-react';
 import { NsInterstitial } from '../../../../../../theme/shared';
 import NSImage from '../../../../../shared/NSImage';
 
@@ -11,40 +12,14 @@ export default class ConfirmCancelModal extends React.Component {
   render() {
     const { responsiveVars } = this.props.uiStore;
     const { isMobile } = responsiveVars;
-    // const offerings = [
-    //   {
-    //     title: 'Bravery Chef Hall',
-    //     location: 'Houston, TX',
-    //     description: 'The next evolution of the food hall, Bravery Cheff Hall in Houston will feature five chef-driven concepts with an immersive dining experience and extensive beverage progam.',
-    //     meta1: 'Raised $1,000,000 from 539 investors',
-    //     meta2: 'Funded in May 2018',
-    //     offeredBy: 'Offered by NextSeed Securities LLC',
-    //     imageUrl: `${!isMobile ? 'interstitial/bravery.png' : 'interstitial/braveryMobile.png'}`,
-    //   }, {
-    //     title: 'Citizen Pilates',
-    //     location: 'Houston, TX',
-    //     description: 'Boutique Reformer Pilates studio revered for its positive and inclusive envrionment',
-    //     meta1: 'Raised $100,000 from 74 investors',
-    //     meta2: 'Funded in October 2016',
-    //     offeredBy: 'Offered by NextSeed Securities LLC',
-    //     imageUrl: `${!isMobile ? 'interstitial/citizens.png' : 'interstitial/citizensMobile.png'}`,
-    //   }, {
-    //     title: 'Buffbrew Taproom',
-    //     location: 'Houston, TX',
-    //     description: 'Houston brewery is expanding its facilities and launching the new Buffbrew Taproom, complete with a full-service kitchen, event space and over 40 beers on tap.',
-    //     meta1: 'Raised $1,000,000 from 583 investors',
-    //     meta2: 'Funded in March 2018',
-    //     offeredBy: 'Offered by NextSeed Securities LLC',
-    //     imageUrl: `${!isMobile ? 'interstitial/buffbrew.png' : 'interstitial/buffbrewMobile.png'}`,
-    //   },
-    // ];
+    // className="link-button" color="green" to={Link} content="complete your account setup"
     const interstitialSteps = {
       label: 'Profile Interstitial',
       key: 'profileInterstitial',
       steps: [
         {
           stepToBeRendered: 1,
-          header: 'Let’s have a look around your new account',
+          header: <>Let’s have a look around<br />your new account</>,
           content: <>Once you’ve made your first investment on NextSeed, use your Dashboard to find information related to your investment portfolio — including payments, updates, returns and more.</>,
           component: <NSImage path={`${!isMobile ? 'interstitial/portfolio.png' : 'interstitial/portfolioMobile.png'}`} />,
           imageFooter: '*Sample Portfolio',
@@ -59,9 +34,8 @@ export default class ConfirmCancelModal extends React.Component {
         }, {
           stepToBeRendered: 3,
           header: 'Time to explore your investment opportunities!',
-          content: <>Now that you know the basics of your new NextSeed account, you can go ahead and <Link to="/dashboard/setup">complete your account setup</Link>, or start exploring investment opportunities.</>,
+          content: <>Now that you know the basics of your new NextSeed account, you can go ahead and <Link onClick={() => this.props.history.push('/init-dashboard')}>complete your account setup</Link>, or start exploring investment opportunities.</>,
           note: <div className={`protipWrap ${isMobile ? 'dblock' : 'dblock'}`}><strong>Pro tip:</strong> Click the ‘Follow’ button on any campaign to receive regular campaign updates and alerts so that you never miss a chance to invest.</div>,
-          // component: <NsOfferingsCards offerings={offerings} isMobile={isMobile} />,
           component: <NSImage path={`${!isMobile ? 'interstitial/offerings.png' : 'interstitial/offeringsMobile.png'}`} />,
           button: 'Explore Offerings',
         },
@@ -72,7 +46,6 @@ export default class ConfirmCancelModal extends React.Component {
         closeOnDimmerClick={false}
         open
         interstitialSteps={interstitialSteps}
-        // offerings={offerings}
         endUrl="/offerings"
         url="/dashboard/setup"
         size="large"
