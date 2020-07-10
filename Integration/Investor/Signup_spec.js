4e9fbcc11f027887f5f76628b773c12c74111133
Swapnil <swapnil.cuelogic@nextseed.com>
2020-07-10 13:44:13 +0530

    first commit

---
+++ Integration/Investor/Signup_spec.js
@@ -0,0 +1,52 @@
+import {
+    
+  gotoSignupscreen,
+
+  fillsignupform,
+
+  otpcode,
+
+  identity
+  
+} from './Utility/basicSignup';
+
+
+
+
+
+describe('Investor Sign up page launch', () => {
+
+it ('visits signup page', () => {
+
+cy.visit('https://dev.nextseed.qa/');
+
+
+console.log("This is home screen");
+
+gotoSignupscreen();  
+
+})
+
+
+it('should enter basic data', () => {
+
+   fillsignupform();
+ })
+
+
+ it ('Enter valid OTP',() => {
+
+otpcode();
+
+})
+
+
+it('It should enter identity verification data', () => {
+
+  identity();
+
+})
+
+
+});
+
