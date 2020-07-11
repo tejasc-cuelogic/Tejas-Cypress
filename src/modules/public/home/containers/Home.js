b348f2b2f53f63e6c3616edba1003930e72788d0
tarekbeb <tarek.albeb@nextseed.com>
2020-07-10 21:03:34 -0500

    added covid banner to home page

--- src/modules/public/home/containers/Home.js
+++ src/modules/public/home/containers/Home.js
@@ -10,7 +10,7 @@ import CampaignList from '../../offering/components/listing/CampaignList';
 import SubscribeForNewsletter from '../../shared/components/SubscribeForNewsletter';
 import NSImage from '../../../shared/NSImage';
 
-@inject('campaignStore', 'uiStore', 'userStore')
+@inject('campaignStore', 'uiStore', 'userStore', 'publicStore')
 @observer
 class Home extends Component {
   constructor(props) {
@@ -24,34 +24,33 @@ class Home extends Component {
   }
 
   render() {
-    const {
-      active, loading,
-    } = this.props.campaignStore;
-    const { responsiveVars } = this.props.uiStore;
-    const { userStore } = this.props;
+    const { active, loading } = this.props.campaignStore;
+    const { isMobile } = this.props.uiStore.responsiveVars;
+    const { isIssuer } = this.props.userStore;
+    const { setShowButton, getRedirectUrl } = this.props.publicStore;
+    setShowButton();
+    getRedirectUrl();
     return (
       <>
         <Banner />
         <Responsive as={React.Fragment} fireOnMount onUpdate={this.handleOnUpdate}>
-          <HowItWorksSummary isMobile={responsiveVars.isMobile} />
+          <HowItWorksSummary isMobile={isMobile} />
         </Responsive>
-        {/* <HowItWorks />
-        <Divider fitted as={Container} /> */}
         <CampaignList
           loading={loading}
           explore
           campaigns={active.slice(0, 6)}
           heading={(
             <>
-              <Header as="h2" textAlign={responsiveVars.isMobile ? '' : 'center'} className={responsiveVars.isMobile ? 'mt-20' : 'mt-50'}>Diversify your portfolio with high-growth businesses</Header>
-              <p className={`${responsiveVars.isMobile ? 'mb-50' : 'mb-80 center-align'} neutral-text`}>
+              <Header as="h2" textAlign={isMobile ? '' : 'center'} className={isMobile ? 'mt-20' : 'mt-50'}>Diversify your portfolio with high-growth businesses</Header>
+              <p className={`${isMobile ? 'mb-50' : 'mb-80 center-align'} neutral-text`}>
               These are just a few of the pre-vetted opportunities available in a growing number of industry categories.
               </p>
             </>
           )}
           loadMoreButton={(
-            <div className={`${responsiveVars.isMobile ? 'mb-20' : 'mb-50'} mt-50 center-align`}>
-              <Button fluid={responsiveVars.isMobile} primary basic content="View All Investment Opportunities" onClick={this.handleExploreBtn} />
+            <div className={`${isMobile ? 'mb-20' : 'mb-50'} mt-50 center-align`}>
+              <Button fluid={isMobile} primary basic content="View All Investment Opportunities" onClick={this.handleExploreBtn} />
             </div>
           )}
         />
@@ -59,34 +58,34 @@ class Home extends Component {
         <FeaturedOn />
         <Divider as={Container} fitted />
         <section>
-          <Container className={responsiveVars.isMobile ? 'mb-20 mt-20' : 'mt-50 mb-50'}>
-            <Grid columns={2} stackable className={responsiveVars.isMobile ? '' : 'mb-80'}>
+          <Container className={isMobile ? 'mb-20 mt-20' : 'mt-50 mb-50'}>
+            <Grid columns={2} stackable className={isMobile ? '' : 'mb-80'}>
               <Grid.Column>
-                <Header as="h2" className={responsiveVars.isMobile ? 'mb-20' : 'mb-30'}>Looking to raise capital <Responsive minWidth={768} as="br" />for your business?</Header>
-                <p className={`${responsiveVars.isMobile ? 'mb-30' : 'mb-60'} neutral-text`}>
+                <Header as="h2" className={isMobile ? 'mb-20' : 'mb-30'}>Looking to raise capital <Responsive minWidth={768} as="br" />for your business?</Header>
+                <p className={`${isMobile ? 'mb-30' : 'mb-60'} neutral-text`}>
                   Whether expanding or opening a brand-new concept, <Responsive minWidth={992} as="br" />
                   we make it easy to raise money from thousands of local investors.
                 </p>
-                {!userStore.isIssuer && !responsiveVars.isMobile
+                {!isIssuer && !isMobile
                   && <Button as={Link} to="/business-application" className="relaxed" primary>Apply Online</Button>}
-                {responsiveVars.isMobile && <NSImage path="home.jpg" />}
+                {isMobile && <NSImage path="home.jpg" />}
               </Grid.Column>
               <Grid.Column>
-                {!responsiveVars.isMobile && <NSImage path="home.jpg" />}
-                {!userStore.isIssuer && responsiveVars.isMobile
+                {!isMobile && <NSImage path="home.jpg" />}
+                {!isIssuer && isMobile
                 && <Button as={Link} to="/business-application" primary fluid className="mb-20 mt-10 relaxed">Apply Online</Button>}
               </Grid.Column>
             </Grid>
             <Divider section />
             <Grid id="news-letter" columns={2} stackable doubling>
               <Grid.Column>
-                <Header as="h2" className={responsiveVars.isMobile ? 'mt-0 mb-10' : 'mt-80 mb-20'}>Join our newsletter</Header>
-                <p className={`${responsiveVars.isMobile ? 'mb-10' : ''} neutral-text`}>
+                <Header as="h2" className={isMobile ? 'mt-0 mb-10' : 'mt-80 mb-20'}>Join our newsletter</Header>
+                <p className={`${isMobile ? 'mb-10' : ''} neutral-text`}>
                   Sign up to stay informed about new investment<Responsive minWidth={768} as="br" /> opportunities, updates and events.
                 </p>
               </Grid.Column>
               <Grid.Column verticalAlign="middle">
-                <SubscribeForNewsletter className={`${responsiveVars.isMobile ? 'mt-0' : 'mt-80'} public-form`} />
+                <SubscribeForNewsletter className={`${isMobile ? 'mt-0' : 'mt-80'} public-form`} />
               </Grid.Column>
             </Grid>
           </Container>
--- src/services/stores/entities/public/publicStore.js
+++ src/services/stores/entities/public/publicStore.js
@@ -1,10 +1,33 @@
 import { observable, computed, action, toJS } from 'mobx';
 import graphql from 'mobx-apollo';
+import { get } from 'lodash';
 import { getJobListing } from '../../queries/public';
+import { authStore, userStore, userDetailsStore, navStore } from '../../index';
 import { GqlClient as client } from '../../../../api/publicApi';
 
 export class PublicStore {
   @observable jobListData = {};
+
+  @observable showButton = true;
+
+  @observable redirectUrl = '';
+
+  @action
+  setShowButton = () => {
+    const { isInvestor } = userStore;
+    const { isUserLoggedIn } = authStore;
+    this.showButton = (!isUserLoggedIn || (isUserLoggedIn && isInvestor));
+  }
+
+  @action
+  getRedirectUrl = () => {
+    const { signupStatus, pendingStep } = userDetailsStore;
+    const { isUserLoggedIn } = authStore;
+    const { isInvestor } = userStore;
+    const { stepInRoute } = navStore;
+    const isFullInvestor = isInvestor && get(signupStatus, 'activeAccounts') && get(signupStatus, 'activeAccounts').length;
+    this.redirectUrl = isUserLoggedIn ? (isFullInvestor ? '/offerings' : pendingStep) : `${get(stepInRoute, 'to')}`;
+  }
 
   @action
   getJobListing = () => new Promise((resolve, reject) => {
