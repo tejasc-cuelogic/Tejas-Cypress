05365507a8fd14c30be1381085a426a86f681696
Swapnil Bhosale <swapnil.cuelogic@nextseed.com>
2020-07-13 20:00:12 +0530

    2896 [Enhancmenet] - unnecessary files delete

--- src/modules/private/investor/accountSetup/containers/accountCreation/ira/Beneficiary.js
+++
@@ -1,9 +0,0 @@
-// import React, { Component } from 'react';
-
-// @inject('uiStore', 'iraAccountStore')
-// @observer
-// export default class Beneficiaries extends Component {
-//   render() {
-
-//   }
-// };
