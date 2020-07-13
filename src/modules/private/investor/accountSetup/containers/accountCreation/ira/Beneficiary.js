713f360f1807589862d0076727b1d6f0309c88f2
Swapnil Bhosale <swapnil.cuelogic@nextseed.com>
2020-07-13 19:06:43 +0530

    2896 [ENhancement] - predev simulation done

---
+++ src/modules/private/investor/accountSetup/containers/accountCreation/ira/Beneficiary.js
@@ -0,0 +1,9 @@
+// import React, { Component } from 'react';
+
+// @inject('uiStore', 'iraAccountStore')
+// @observer
+// export default class Beneficiaries extends Component {
+//   render() {
+
+//   }
+// };
